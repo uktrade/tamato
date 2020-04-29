e994933010331119364649de2e2f2103a5b2aa89
Andy Driver <andy@pagezero.net>
2020-04-29 13:27:08 +0100

    Update footnote and footnote type ID validation
    To match TARIC schema
    
    Footnote Type IDs may be 2 or 3 upper-case letters, or 2 digits.
    
    Footnote IDs may be 3 or 5 digits.
    
    This change adds model field validation, which is checked automatically when submitting a ModelForm, but should be checked manually when used programmatically.

--- footnotes/models.py
+++ footnotes/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 
 from common.models import ValidityMixin
+from footnotes import validators
 
 
 class FootnoteType(ValidityMixin):
-    footnote_type_id = models.CharField(unique=True, max_length=2)
+    footnote_type_id = models.CharField(
+        unique=True, max_length=3, validators=[validators.valid_footnote_type_id]
+    )
     description = models.CharField(max_length=500)
 
 
 class Footnote(ValidityMixin):
-    footnote_id = models.CharField(unique=True, max_length=5)
+    footnote_id = models.CharField(
+        unique=True, max_length=5, validators=[validators.valid_footnote_id]
+    )
     footnote_type = models.ForeignKey(FootnoteType, on_delete=models.PROTECT)
     description = models.CharField(max_length=500)
 
