b73b279158eb33445482328a3e5a948525fee055
Andy Driver <andy@pagezero.net>
2020-05-18 18:00:30 +0100

    Ensure Footnote and Footnote Type business rules
    There are a number of TARIC processing rules pertaining to Footnotes (FO1 - FO16) and
    Footnote Types (FOT1 - FOT3).
    
    This change adds database constraints and model validation to ensure the relevant
    processing rules are applied.

--- footnotes/models.py
+++ footnotes/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from common.models import ValidityMixin
@@ -13,10 +14,25 @@ class FootnoteType(ValidityMixin):
 
 class Footnote(ValidityMixin):
     footnote_id = models.CharField(
-        unique=True, max_length=5, validators=[validators.valid_footnote_id]
+        max_length=5, validators=[validators.valid_footnote_id]
     )
     footnote_type = models.ForeignKey(FootnoteType, on_delete=models.PROTECT)
     description = models.CharField(max_length=500)
 
     class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["footnote_id", "footnote_type"], name="FO2",
+            ),
+        ]
         ordering = ["footnote_type__footnote_type_id", "footnote_id"]
+
+    def clean(self):
+        # FO17
+        if not (
+            self.valid_between.lower in self.footnote_type.valid_between
+            and self.valid_between.upper in self.footnote_type.valid_between
+        ):
+            raise ValidationError(
+                "The validity period of the footnote type must span the validity period of the footnote."
+            )
