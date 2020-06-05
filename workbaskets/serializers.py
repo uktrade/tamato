b6ed077a9241a5e00e307cfe0ec073ec6453d235
Andy Driver <andy@pagezero.net>
2020-06-05 09:59:26 +0100

    Simplify WorkBaskets ready for TrackedModels
    The data model for version tracking models removes the need for the WorkBasketItem model.
    
    It has been decided that the approval process for WorkBaskets does not need to handle a submit/rejection/resubmit/approval cycle, removing the need for the ApprovalDecision model.
    
    WorkBaskets are assigned a Transaction ID when they are submitted for approval. This is represented by the Transaction model, which has a One-to-One relation with WorkBasket.

--- workbaskets/serializers.py
+++ workbaskets/serializers.py
@@ -5,37 +5,10 @@ from common.serializers import UserSerializer
 from workbaskets import models
 
 
-class ApprovalDecisionSerializer(serializers.ModelSerializer):
-    approver = UserSerializer()
-
-    class Meta:
-        model = models.ApprovalDecision
-        fields = ["approver", "decision", "created_at"]
-
-
-class WorkBasketItemSerializer(serializers.ModelSerializer):
-    approval = ApprovalDecisionSerializer()
-    author = UserSerializer()
-
-    class Meta:
-        model = models.WorkBasketItem
-        fields = [
-            "id",
-            "content_type",
-            "object_id",
-            "diff",
-            "draft",
-            "errors",
-            "author",
-            "approval",
-        ]
-
-
 class WorkBasketSerializer(serializers.ModelSerializer):
-    approval = ApprovalDecisionSerializer()
+    approver = UserSerializer()
     author = UserSerializer()
-    items = WorkBasketItemSerializer(many=True, read_only=True)
 
     class Meta:
         model = models.WorkBasket
-        fields = ["id", "title", "reason", "author", "approval", "status", "items"]
+        fields = ["id", "title", "reason", "author", "approver", "status"]
--- workbaskets/views.py
+++ workbaskets/views.py
@@ -4,8 +4,6 @@ from rest_framework import renderers
 from rest_framework import viewsets
 
 from workbaskets.models import WorkBasket
-from workbaskets.models import WorkBasketItem
-from workbaskets.serializers import WorkBasketItemSerializer
 from workbaskets.serializers import WorkBasketSerializer
 
 
@@ -34,24 +32,16 @@ class WorkBasketUIViewSet(WorkBasketViewSet):
         )
 
     def retrieve(self, request, *args, **kwargs):
-        items = self.get_object().items.prefetch_related("existing_record").all()
-        groups = dict()
-        for item in items:
-            group_name = item.existing_record.__class__._meta.verbose_name_plural
-            groups.setdefault(group_name, []).append(item.existing_record)
-        groups = sorted(list(groups.items()), key=lambda tup: tup[0])
+        # XXX needs updating to use TrackedModel
+        # items = self.get_object().items.prefetch_related("existing_record").all()
+        # groups = dict()
+        # for item in items:
+        #     group_name = item.existing_record.__class__._meta.verbose_name_plural
+        #     groups.setdefault(group_name, []).append(item.existing_record)
+        # groups = sorted(list(groups.items()), key=lambda tup: tup[0])
+        groups = []
         return render(
             request,
             "workbaskets/detail.jinja",
             context={"workbasket": self.get_object(), "workbasketitem_groups": groups},
         )
-
-
-class WorkBasketItemViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    API endpoint that allows workbasket items to be viewed.
-    """
-
-    queryset = WorkBasketItem.objects.all()
-    serializer_class = WorkBasketItemSerializer
-    permission_classes = [permissions.IsAuthenticated]
