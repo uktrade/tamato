0fef120cc4c46d166ea8ca628da3bd710b316e8e
Andy Driver <andy@pagezero.net>
2020-06-26 11:56:27 +0100

    Make compatible with tamato-taric-import
    The TAMATO TARIC importer script has a dependency on TAMATO's serializers and models to
    validate and modify the TAMATO DB.
    
    This change adds a `setup.py` to allow TAMATO to be added as a pip dependency
    
    It also adds preliminary serializers for Commodity and Measure models, to allow the
    importer to deserialize these models.
    
    Finally, the ValiditySerializerMixin is modified to handle TARIC date format strings
    (YYYY-MM-DD) as well as ISO 8601 format (the default), for convenience.

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+import setuptools
+
+setup(
+    name="tamato",
+    version="0.0.1",
+    description="UK Tariff Management Tool",
+    maintainer="Department for International Trade",
+    maintainer_email="webops@digital.trade.gov.uk",
+    url="https://github.com/uktrade/tamato",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "dj-database-url",
+        "django",
+        "django-dotenv",
+        "django-extra-fields",
+        "django-filter",
+        "django-fsm",
+        "django-health-check",
+        "django-polymorphic",
+        "django-treebeard",
+        "django-webpack-loader",
+        "django_extensions",
+        "djangorestframework",
+        "gunicorn",
+        "jinja2",
+        "psycopg2-binary",
+        "sentry-sdk",
+        "werkzeug",
+        "whitenoise",
+    ],
+    dependency_links=[
+        "https://github.com/alphagov/govuk-frontend-jinja/tarball/master#egg=govuk-frontend-jinja",
+    ],
+)
