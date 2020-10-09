788c995df5f2efe5d6860eb4fcaeb2355cdaa1af
Simon Worthington <simon.worthington@digital.trade.gov.uk>
2020-10-09 16:52:07 +0100

    Update indents importer to correctly create many indents
    Under the old model, we would attempt to discover
    the correct parent for a code and then create a
    single child indent node to represent the indent.
    
    This is not how indents work! An indent is just
    an expression of the depth of a node in the tree
    – when a parent node changes, there is no new
    indent created if the child depth is still the
    same.
    
    What this means is that we can't use the presence
    of a new indent to work out when there needs to
    be a new child node in the tree. That is to say,
    one indent can have *mulitple* parent indents.
    
    The way we get around this is to create one indent
    per parent and use the validity period to indicate
    which indent is active at each present time.
    
    We will also need to update the indent output
    code to understand that all the indents at the
    same depth should be treated the same. I.e. that
    if a new indent node is at the same depth as an
    old indent node, that does not represent an update
    in TARIC3.

--- importer/management/commands/import_indents.py
+++ importer/management/commands/import_indents.py
@@ -1,14 +1,16 @@
 import logging
-import sys
+import os
 from datetime import datetime
-from io import StringIO
+from datetime import timedelta
 from typing import Iterator
 from typing import List
 from typing import Optional
 
+import pytz
 import xlrd
 from django.contrib.auth.models import User
 from django.core.management import BaseCommand
+from psycopg2._range import DateTimeTZRange
 from xlrd.sheet import Cell
 
 import settings
@@ -16,10 +18,9 @@ from commodities.models import GoodsNomenclature
 from commodities.models import GoodsNomenclatureIndent
 from common.models import TrackedModel
 from common.validators import UpdateType
-from importer.management.commands.doc_importer import BREXIT
-from importer.management.commands.doc_importer import LONDON
 from importer.management.commands.doc_importer import RowsImporter
 from importer.management.commands.utils import EnvelopeSerializer
+from importer.management.commands.utils import maybe_min
 from workbaskets.models import WorkBasket
 from workbaskets.validators import WorkflowStatus
 
@@ -29,31 +30,52 @@ logger = logging.getLogger(__name__)
 class Row:
     def __init__(self, row: List[Cell]) -> None:
         self.sid = int(row[1].value)
-        self.commodity_code = str(row[4].value)
-        self.suffix = str(row[5].value)
-        self.indent = int(row[3].value)
+        self.commodity_code = str(row[5].value)
+        self.suffix = str(row[6].value)
+        self.indent = int(row[4].value)
         self.indent_sid = int(row[0].value)
         self.start_date = self.parse_date(row[2].value)
-        # self.end_date = self.parse_date(row[6].value)
-        # self.hs_level = int(row[7].value)
-        # self.description = str(row[8].value)
-        # self.parent_sid = int(row[9].value) if row[9].value != "" else None
-        # self.parent_code = str(row[10].value) if row[10].value != "" else None
-        # self.parent_suffix = str(row[11].value) if row[11].value != "" else None
-        # self.parent_indent = int(row[12].value) if row[12].value != 0 else None
-        # self.parent_indent_sid = int(row[13].value) if row[13].value != 0 else None
+        self.end_date = self.parse_date(row[3].value)
 
     def parse_date(self, value: str) -> Optional[datetime]:
         if value != "":
-            return LONDON.localize(datetime.strptime(value, r"%Y-%m-%d"))
+            return pytz.utc.localize(datetime.strptime(value, r"%Y-%m-%d"))
         else:
             return None
 
 
 class IndentImporter(RowsImporter):
+    """Import indents from a spreadsheet dump of existing indents.
+    The spreadsheed output should include a column for the end date of the indent.
+    This can be calculated by subtracting 1 from the start date of the next indent
+    for the same SID. A la:
+
+        MAX(goods_nomenclature_indents.validity_start_date) OVER (
+            PARTITION BY goods_nomenclature_indents.goods_nomenclature_sid
+            ORDER BY goods_nomenclature_indents.validity_end_date ASC
+            ROWS BETWEEN CURRENT ROW AND 1 FOLLOWING
+        ) AS validity_end_date
+
+    This importer will then, for each indent, walk forward from the start date
+    to the end date and discover all of the parents this indent has had for it's lifetime,
+    and create one child node with an appropriate validity range under that parent.
+    So, if the parent indent ends before the child indent, the next parent will be found
+    and a node created under that new parent from the end date of the old parent.
+
+    Note that indents will not have end dates in this spreadsheet if the indented
+    commodity code is ended, so the end date of the goods nomenclature must be taken into
+    account too.
+
+    The end result is a set of GoodsNomenclatureIndent models with correct parent codes and
+    validity ranges that represent the time that code was the parent, such that a call to
+
+        GoodsNomenclature.get(…).indents.as_at(…).get()
+
+    will return the correct indent and the correct parent code at that time."""
+
     def handle_row(
         self, new_row: Optional[Row], old_row: None
-    ) -> Iterator[TrackedModel]:
+    ) -> Iterator[List[TrackedModel]]:
         assert new_row
 
         indent = new_row.indent
@@ -61,15 +83,17 @@ class IndentImporter(RowsImporter):
         item_id = child.item_id
 
         if (
-            indent == 0 and item_id[2:] == "00000000"
+            indent == -1 and item_id[2:] == "00000000"
         ):  # This is a root indent (i.e. a chapter heading)
-            yield GoodsNomenclatureIndent.add_root(
-                sid=new_row.indent_sid,
-                indented_goods_nomenclature=child,
-                valid_between=(new_row.start_date, None),
-                workbasket=self.workbasket,
-                update_type=UpdateType.CREATE,
-            )
+            yield [
+                GoodsNomenclatureIndent.add_root(
+                    sid=new_row.indent_sid,
+                    indented_goods_nomenclature=child,
+                    valid_between=(new_row.start_date, new_row.end_date),
+                    workbasket=self.workbasket,
+                    update_type=UpdateType.CREATE,
+                )
+            ]
         else:
             # The indent is now too deep to use the item ID directly. Instead the code that is:
             #   - indent + 1
@@ -77,67 +101,91 @@ class IndentImporter(RowsImporter):
             #   - Has a code that starts with the current path, omitting the last step.
             #   - Has a code that is greater than or equal to the current path
             #      - Some child codes have the same code as their parents.
-            trimmed_id = item_id
-            while trimmed_id.endswith("00"):
-                trimmed_id = trimmed_id[:-2]
-            trimmed_id = trimmed_id[:-2]
-
             parent_indent = indent + 1
 
-            previously = (
-                GoodsNomenclatureIndent.objects.filter(
-                    indented_goods_nomenclature__sid=child.sid,
-                )
-                .order_by("valid_between")
-                .reverse()
-            )
+            # This is the time range that needs to be covered by
+            # the indents that we create. We will subtract from
+            # this range as we discover the correct parents.
+            remaining_range = [
+                new_row.start_date,
+                maybe_min(
+                    new_row.end_date,
+                    child.valid_between.upper,
+                ),
+            ]
 
-            # We must detect any previous indent for this comm code
-            # and end date it if this indent is overwriting it in the future
-            if any(previously):
-                previous = previously[0]
+            # Keep looking for parents whilst we have remaining time.
+            # The end condition is that the start date is now equal to the end date,
+            # or the end date of the indented code, or there is no end date so both are None.
+            while remaining_range[0] and (
+                (remaining_range[0] < remaining_range[1])
+                if remaining_range[1]
+                else True
+            ):
                 logger.debug(
-                    "Updating previous %s with start %s to have end %s",
-                    previous,
-                    previous.valid_between.lower,
-                    new_row.start_date,
+                    "Looking for parent for %s(%s) between time %s and %s",
+                    child.item_id,
+                    child.suffix,
+                    *remaining_range,
                 )
-                previous.valid_between = (
-                    previous.valid_between.lower,
-                    new_row.start_date,
+
+                try:
+                    next_parent = (
+                        GoodsNomenclatureIndent.objects.filter(
+                            indented_goods_nomenclature__item_id__lte=item_id,
+                            # TODO: this timedelta thing is obviously hacky,
+                            # how do we get it to just search for a single time?
+                            indented_goods_nomenclature__valid_between__contains=DateTimeTZRange(
+                                remaining_range[0],
+                                remaining_range[0] + timedelta(hours=1),
+                            ),
+                            valid_between__contains=DateTimeTZRange(
+                                remaining_range[0],
+                                remaining_range[0] + timedelta(hours=1),
+                            ),
+                            depth=parent_indent,
+                        )
+                        .order_by("indented_goods_nomenclature__item_id")
+                        .reverse()[0]
+                    )
+                except IndexError as e:
+                    logger.error(
+                        "Failed to find parent for %s between time %s and %s",
+                        child.item_id,
+                        *remaining_range,
+                    )
+                    raise e
+
+                indent_start = remaining_range[0]
+                indent_end = maybe_min(
+                    next_parent.valid_between.upper,
+                    next_parent.indented_goods_nomenclature.valid_between.upper,
+                    remaining_range[1],
                 )
-                previous.save()
 
-            logger.debug(
-                "Parent for %s: starts_with %s, <= %s, ind %s",
-                new_row.commodity_code,
-                trimmed_id,
-                item_id,
-                indent - 1,
-            )
+                assert indent_end is None or indent_start <= indent_end
 
-            parent = (
-                GoodsNomenclatureIndent.objects.filter(
-                    # indented_goods_nomenclature__item_id__startswith=trimmed_id,
-                    indented_goods_nomenclature__item_id__lte=item_id,
-                    indented_goods_nomenclature__valid_between__contains=(
-                        new_row.start_date,
-                        new_row.start_date,
-                    ),
-                    valid_between__contains=(new_row.start_date, new_row.start_date),
-                    depth=parent_indent,
+                logger.debug(
+                    "%s(%s) is parent of %s(%s) between %s and %s",
+                    next_parent.indented_goods_nomenclature.item_id,
+                    next_parent.indented_goods_nomenclature.suffix,
+                    child.item_id,
+                    child.suffix,
+                    indent_start,
+                    indent_end,
                 )
-                .order_by("indented_goods_nomenclature__item_id")
-                .reverse()[0]
-            )
 
-            yield parent.add_child(
-                sid=new_row.indent_sid,
-                indented_goods_nomenclature=child,
-                valid_between=(new_row.start_date, None),
-                workbasket=self.workbasket,
-                update_type=UpdateType.CREATE,
-            )
+                yield [
+                    next_parent.add_child(
+                        sid=new_row.indent_sid,
+                        indented_goods_nomenclature=child,
+                        valid_between=(indent_start, indent_end),
+                        workbasket=self.workbasket,
+                        update_type=UpdateType.CREATE,
+                    )
+                ]
+
+                remaining_range[0] = indent_end
 
 
 class Command(BaseCommand):
@@ -180,7 +228,7 @@ class Command(BaseCommand):
             next(new_rows)
 
         with EnvelopeSerializer(
-            StringIO(),
+            open(os.devnull, "w"),
             0,
         ) as env:
             importer = IndentImporter(workbasket, env)
