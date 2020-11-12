2d61da00b905d009bb6a56ef1f81fcb37aa1a4a9
Stuart Axon <stuaxo2@yahoo.com>
2020-11-12 08:59:58 +0000

    Reset EnvelopeFactory sequence so that test won't fail.

--- taric/tests/test_models.py
+++ taric/tests/test_models.py
@@ -7,6 +7,8 @@ pytestmark = pytest.mark.django_db
 
 
 def test_envelope_transactions(date_ranges):
+    factories.EnvelopeFactory.reset_sequence()
+
     envelope = factories.EnvelopeFactory()
 
     assert envelope.envelope_id == f"{date_ranges.now:%y}0001"
