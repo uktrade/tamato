9d4118fa7889d482c851ce77a336795abf0e3e5c
Nick Sarbicki <nick.a.sarbicki@gmail.com>
2020-11-18 10:52:16 +0000

    Minor cleanup

--- additional_codes/tests/bdd/conftest.py
+++ additional_codes/tests/bdd/conftest.py
@@ -9,16 +9,18 @@ pytestmark = pytest.mark.django_db
 
 @given("additional code X000", target_fixture="additional_code_X000")
 def additional_code_X000(date_ranges):
-    t = factories.AdditionalCodeTypeFactory(sid="X", valid_between=date_ranges.big)
-    ac = factories.AdditionalCodeFactory(
+    t = factories.AdditionalCodeTypeFactory.create(
+        sid="X", valid_between=date_ranges.big
+    )
+    ac = factories.AdditionalCodeFactory.create(
         code="000", type=t, valid_between=date_ranges.normal
     )
-    factories.AdditionalCodeDescriptionFactory(
+    factories.AdditionalCodeDescriptionFactory.create(
         described_additional_code=ac,
         description="This is X000",
         valid_between=date_ranges.starts_with_normal,
     )
-    factories.AdditionalCodeDescriptionFactory(
+    factories.AdditionalCodeDescriptionFactory.create(
         described_additional_code=ac,
         description="Another description",
         valid_between=date_ranges.overlap_normal,
