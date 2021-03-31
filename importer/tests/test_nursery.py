59f99cbe6cfed5a22854a12e1a983803f563c22e
Nick Sarbicki <nick.a.sarbicki@gmail.com>
2021-03-31 16:14:04 +0100

    TP-655: Added more tests for the importer system.
    Added tests for the nursery caching interface as well as the tasks.

--- importer/tests/test_nursery.py
+++ importer/tests/test_nursery.py
@@ -1,5 +1,9 @@
 import pytest
 
+from common.tests import factories
+from common.tests.models import TestModel1
+from common.validators import UpdateType
+from footnotes.models import Footnote
 from importer import nursery
 
 
@@ -10,6 +14,42 @@ def test_nursery_gets_handler_with_tag(object_nursery, handler_class):
 def test_nursery_throws_error_on_no_handler(object_nursery):
     with pytest.raises(nursery.HandlerDoesNotExistError):
         object_nursery.get_handler("non-existent-tag")
+
+
+@pytest.mark.django_db
+def test_nursery_clears_cache(
+    handler_class,
+    object_nursery,
+    date_ranges,
+    unapproved_transaction,
+):
+    handler = handler_class(
+        {
+            "data": {
+                "sid": 1,
+                "name": "clears_cache",
+                "update_type": UpdateType.CREATE,
+                "valid_between": {
+                    "lower": date_ranges.normal.lower,
+                    "upper": date_ranges.normal.upper,
+                },
+            },
+            "tag": handler_class.tag,
+            "transaction_id": unapproved_transaction.pk,
+        },
+        object_nursery,
+    )
+    object_nursery._cache_handler(handler)
+
+    assert TestModel1.objects.count() == 0
+    assert object_nursery.cache.get(handler.key)
+
+    object_nursery.clear_cache()
+
+    assert object_nursery.cache.get(handler.key) is None
+    assert TestModel1.objects.count() == 1
+    assert TestModel1.objects.get().name == "clears_cache"
+    assert TestModel1.objects.get().sid == 1
 
 
 def test_nursery_caches_object(object_nursery, handler_class):
@@ -28,3 +68,18 @@ def test_nursery_caches_object(object_nursery, handler_class):
     assert handler.data == cached_handler.data
     assert handler.tag == cached_handler.tag
     assert handler.key == cached_handler.key
+
+
+@pytest.mark.django_db
+def test_nursery_gets_object_from_cache(object_nursery):
+    instance = factories.FootnoteFactory.create()
+    object_nursery.cache_object(instance)
+
+    identifying_fields = instance.get_identifying_fields()
+
+    cached_instance = object_nursery.get_obj_from_cache(
+        Footnote,
+        identifying_fields.keys(),
+        identifying_fields,
+    )
+    assert cached_instance == (instance.pk, instance.__class__.__name__)
---
+++ importer/tests/test_tasks.py
@@ -0,0 +1,128 @@
+from unittest import mock
+
+import pytest
+
+from common.tests import factories
+from importer import tasks
+from importer.models import ImporterChunkStatus
+
+pytestmark = pytest.mark.django_db
+
+
+@mock.patch("importer.tasks.find_and_run_next_batch_chunks")
+def test_import_chunk(mock_find_and_run: mock.MagicMock, valid_user, chunk):
+    tasks.import_chunk(chunk.pk, "PUBLISHED", valid_user.username)
+
+    chunk.refresh_from_db()
+    assert chunk.status == ImporterChunkStatus.DONE
+    mock_find_and_run.assert_called_once_with(
+        chunk.batch,
+        "PUBLISHED",
+        valid_user.username,
+    )
+
+
+@mock.patch("importer.tasks.process_taric_xml_stream", side_effect=KeyError("test"))
+def test_import_chunk_failed(valid_user, chunk):
+    try:
+        tasks.import_chunk(chunk.pk, "PUBLISHED", valid_user.username)
+    except KeyError:
+        pass
+
+    chunk.refresh_from_db()
+    assert chunk.status == ImporterChunkStatus.ERRORED
+
+
+@mock.patch("importer.tasks.import_chunk")
+def test_setup_chunk_task_already_running(mock_import_chunk, batch, valid_user):
+    """Assert that if a batch already has a running chunk, nothing happens."""
+    factories.ImporterXMLChunkFactory.create(
+        batch=batch,
+        status=ImporterChunkStatus.RUNNING,
+    )
+    tasks.setup_chunk_task(batch, "PUBLISHED", valid_user.username)
+
+    mock_import_chunk.delay.assert_not_called()
+
+
+@mock.patch("importer.tasks.import_chunk")
+def test_setup_chunk_task_no_chunks(mock_import_chunk, batch, valid_user):
+    """Assert that if a batch has no chunks, nothing happens."""
+    tasks.setup_chunk_task(batch, "PUBLISHED", valid_user.username)
+
+    mock_import_chunk.delay.assert_not_called()
+
+
+@mock.patch("importer.tasks.import_chunk")
+def test_setup_chunk_task(mock_import_chunk, chunk, valid_user):
+    """Assert that if a batch has no running chunks, a chunk is set to run."""
+    tasks.setup_chunk_task(chunk.batch, "PUBLISHED", valid_user.username)
+
+    chunk.refresh_from_db()
+
+    assert chunk.status == ImporterChunkStatus.RUNNING
+    mock_import_chunk.delay.assert_called_once_with(
+        chunk.pk,
+        "PUBLISHED",
+        valid_user.username,
+    )
+
+
+@mock.patch("importer.tasks.setup_chunk_task")
+def test_find_and_run_next_batch_chunks_already_running(
+    mock_setup_chunk_task,
+    batch_dependency,
+    valid_user,
+):
+    """Assert that if a batch already has running chunk, nothing happens."""
+    batch_dependency.depends_on.chunks.update(status=ImporterChunkStatus.RUNNING)
+    batch = batch_dependency.dependent_batch
+    tasks.find_and_run_next_batch_chunks(batch, "PUBLISHED", valid_user.username)
+
+    mock_setup_chunk_task.assert_not_called()
+    assert list(batch.chunks.values_list("status", flat=True)) == [
+        ImporterChunkStatus.WAITING,
+    ]
+
+
+@mock.patch("importer.tasks.import_chunk")
+def test_find_and_run_next_batch_chunks_finished_runs_dependencies(
+    mock_import_chunk,
+    batch_dependency,
+    valid_user,
+):
+    """Assert that if a batch has completely finished, it's dependencies start
+    running."""
+    batch_dependency.depends_on.chunks.update(status=ImporterChunkStatus.DONE)
+    tasks.find_and_run_next_batch_chunks(
+        batch_dependency.depends_on,
+        "PUBLISHED",
+        valid_user.username,
+    )
+    batch = batch_dependency.dependent_batch
+
+    assert list(batch.chunks.values_list("status", flat=True)) == [
+        ImporterChunkStatus.RUNNING,
+    ]
+    mock_import_chunk.delay.assert_called_once_with(
+        batch.chunks.first().pk,
+        "PUBLISHED",
+        valid_user.username,
+    )
+
+
+@mock.patch("importer.tasks.import_chunk")
+def test_find_and_run_next_batch_chunks(mock_import_chunk, batch, valid_user):
+    """Assert that if a batch is not running that this starts the next chunk."""
+    for chunk_number in range(1, 3):
+        factories.ImporterXMLChunkFactory.create(batch=batch, chunk_number=chunk_number)
+
+    tasks.find_and_run_next_batch_chunks(batch, "PUBLISHED", valid_user.username)
+
+    assert batch.chunks.first().status == ImporterChunkStatus.RUNNING
+    assert batch.chunks.last().status == ImporterChunkStatus.WAITING
+    mock_import_chunk.delay.assert_called_once_with(
+        batch.chunks.first().pk,
+        "PUBLISHED",
+        valid_user.username,
+    )
