0675c66da5d69cab6b091c8b590d496bdac99eda
Simon Worthington <simon.worthington@digital.trade.gov.uk>
2021-04-14 19:24:35 +0100

    TP-746 Show memberships for countries and regions.

---
+++ geo_areas/tests/test_models.py
@@ -0,0 +1,61 @@
+import pytest
+
+from common.tests import factories
+
+pytestmark = pytest.mark.django_db
+
+
+@pytest.mark.parametrize(
+    ("membership_data", "expected"),
+    (
+        (lambda group: [], 0),
+        (lambda group: [{"geo_group": group}], 1),
+        (lambda group: [{}], 0),
+        (lambda group: [{"geo_group": group}, {"geo_group": group}], 2),
+    ),
+    ids=(
+        "no_memberships",
+        "membership",
+        "member_of_another_group",
+        "two_members",
+    ),
+)
+def test_get_current_memberships_on_groups(membership_data, expected):
+    group = factories.GeoGroupFactory()
+    for data in membership_data(group):
+        factories.GeographicalMembershipFactory(**data)
+
+    assert len(group.get_current_memberships()) == expected
+
+
+@pytest.mark.parametrize(
+    ("membership_data", "expected"),
+    (
+        (lambda area: [], 0),
+        (lambda area: [{"member": area}], 1),
+        (lambda area: [{}], 0),
+        (lambda area: [{"member": area}, {"member": area}], 2),
+    ),
+    ids=(
+        "no_memberships",
+        "membership",
+        "group_with_another_member",
+        "two_members",
+    ),
+)
+def test_get_current_memberships_on_areas(membership_data, expected):
+    area = factories.CountryFactory()
+    for data in membership_data(area):
+        factories.GeographicalMembershipFactory(**data)
+
+    assert len(area.get_current_memberships()) == expected
+
+
+def test_other_on_membership():
+    membership = factories.GeographicalMembershipFactory()
+    assert membership.other(membership.member) == membership.geo_group
+    assert membership.other(membership.geo_group) == membership.member
+    with pytest.raises(ValueError):
+        membership.other(factories.GeoGroupFactory())
+    with pytest.raises(ValueError):
+        membership.other(factories.CountryFactory())
