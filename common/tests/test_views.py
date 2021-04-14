d4a7feb70d12dc5f6b731adfd03ce5123bb08e3c
Andy Driver <andy@pagezero.net>
2021-04-14 17:11:16 +0100

    Quick refactor of the healthcheck endpoint
    * Removed unnecessary <strong> elements in XML payload
    * A bit DRYer
    * Added some test coverage

--- common/tests/test_views.py
+++ common/tests/test_views.py
@@ -1,6 +1,9 @@
+import xml.etree.ElementTree as etree
+
 import pytest
 from django.urls import reverse
 
+from common.views import HealthCheckResponse
 from workbaskets.models import WorkBasket
 
 pytestmark = pytest.mark.django_db
@@ -21,3 +24,19 @@ def test_index_doesnt_creates_workbasket_if_not_needed(
     response = valid_user_client.get(reverse("index"))
     assert response.status_code == 200
     assert WorkBasket.objects.is_not_approved().count() == 1
+
+
+@pytest.mark.parametrize(
+    "response, status_code, status",
+    [
+        (HealthCheckResponse(), 200, "OK"),
+        (HealthCheckResponse().fail("Not OK"), 503, "Not OK"),
+    ],
+)
+def test_healthcheck_response(response, status_code, status):
+    assert response.status_code == status_code
+    payload = etree.fromstring(response.content)
+    assert payload.tag == "pingdom_http_custom_check"
+    assert payload[0].tag == "status"
+    assert payload[0].text == status
+    assert payload[1].tag == "response_time"
