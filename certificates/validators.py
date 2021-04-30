efae562091dbf80b40dfb55b5113cbd3a7afbac9
DIT-CJWaszczuk <76431740+DIT-CJWaszczuk@users.noreply.github.com>
2021-04-30 14:35:37 +0100

    TP-739 Fix certificate search to find type and code together (#292)

--- certificates/validators.py
+++ certificates/validators.py
@@ -1,3 +1,5 @@
+import re
+
 from django.core.validators import RegexValidator
 
 CERTIFICATE_TYPE_SID_REGEX = r"[A-Z0-9]{1}"
@@ -5,3 +7,7 @@ certificate_type_sid_validator = RegexValidator(fr"^{CERTIFICATE_TYPE_SID_REGEX}
 
 CERTIFICATE_SID_REGEX = r"[A-Z0-9]{3}"
 certificate_sid_validator = RegexValidator(fr"^{CERTIFICATE_SID_REGEX}$")
+
+COMBINED_CERTIFICATE_AND_TYPE_ID = re.compile(
+    fr"(?P<certificate_type__sid>{CERTIFICATE_TYPE_SID_REGEX})(?P<sid>{CERTIFICATE_SID_REGEX})",
+)
