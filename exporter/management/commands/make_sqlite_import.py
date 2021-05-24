040734922b6f5e981d181876c1e2f4af490d4443
Simon Worthington <simon.worthington@digital.trade.gov.uk>
2021-05-24 16:25:27 +0100

    Update POC to output fields in correct order.

--- exporter/management/commands/make_sqlite_import.py
+++ exporter/management/commands/make_sqlite_import.py
@@ -1,26 +1,45 @@
 from itertools import chain
+from subprocess import run
 from typing import Any
-from typing import Iterable
+from typing import Iterator
 from typing import Optional
+from typing import Type
 
 from django.apps import apps
 from django.conf import settings
+from django.core.exceptions import FieldDoesNotExist
 from django.core.management import BaseCommand
+from django.db.models.base import Model
 from django.db.models.fields import BooleanField
-from django.db.models.fields import Field
 
-from common.fields import TaricDateRangeField
 from common.models.mixins.validity import ValidityMixin
 
 
-def field_to_expr(field: Field) -> Iterable[str]:
-    column = f'\\"{field.column}\\"'
-    if isinstance(field, TaricDateRangeField):
-        return [f"UPPER({column})", f"LOWER({column})"]
-    elif isinstance(field, BooleanField):
-        return [f"CASE WHEN {column} IS TRUE THEN 1 ELSE 0 END"]
-    else:
-        return [column]
+def column_name_to_expr(column: str, model: Type[Model]) -> str:
+    try:
+        field = model._meta.get_field(column)
+        if isinstance(field, BooleanField):
+            return f'CASE WHEN \\"{column}\\" IS TRUE THEN 1 ELSE 0 END'
+        else:
+            return f'\\"{column}\\"'
+    except FieldDoesNotExist:
+        if column == "validity_start":
+            return f'LOWER(\\"{ValidityMixin.valid_between.field.name}\\")'
+        elif column == "validity_end":
+            return f'UPPER(\\"{ValidityMixin.valid_between.field.name}\\")'
+        else:
+            raise
+
+
+def read_sqlite_column_order(filename: str, table: str) -> Iterator[str]:
+    cmd = run(
+        ["sqlite3", filename, "--cmd", f"PRAGMA table_info({table});"],
+        capture_output=True,
+        input="",
+    )
+    for column in cmd.stdout.splitlines():
+        name: bytes = column.split(b"|")[1]
+        yield name.decode("utf-8")
 
 
 class Command(BaseCommand):
@@ -32,17 +51,9 @@ class Command(BaseCommand):
         print(".echo on")
         print(".mode csv")
         for model in models:
-            fields = model._meta.local_fields
-
-            # Take out the validity field and put it at the end
-            if issubclass(model, ValidityMixin):
-                validity_field = model._meta.get_field(
-                    ValidityMixin.valid_between.field.name,
-                )
-                fields.remove(validity_field)
-                fields.append(validity_field)
-
-            field_names = ", ".join(chain(*[field_to_expr(f) for f in fields]))
+            columns = read_sqlite_column_order("cool.db", model._meta.db_table)
+            fields = (column_name_to_expr(name, model) for name in columns)
+            field_names = ", ".join(fields)
 
             print(
                 ".import '|psql -c \"COPY (SELECT {1} FROM {2}) TO STDOUT (FORMAT csv);\" {0[NAME]}' {2}".format(
@@ -53,3 +64,4 @@ class Command(BaseCommand):
             )
 
         print("VACUUM;")
+        print("PRAGMA optimize;")
