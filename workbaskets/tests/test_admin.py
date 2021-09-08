f97f7cb06f432fb62f2fa39c9b4972552b596155
Gabe Naughton <gabriel.naughton@digital.trade.gov.uk>
2021-09-08 11:34:59 +0100

    add patch to test to prevent calling celery task and remove self argument from tests now that class has been removed

--- workbaskets/tests/test_admin.py
+++ workbaskets/tests/test_admin.py
@@ -6,15 +6,18 @@ from django.urls import reverse
 pytestmark = pytest.mark.django_db
 
 
-def test_upload_returns_302_for_valid_staff_user(self, staff_user, client):
+def test_upload_returns_302_for_valid_staff_user(staff_user, client):
     client.force_login(staff_user)
-    response = client.post(reverse("admin:upload"))
+    with mock.patch(
+        "exporter.tasks.upload_workbaskets.delay",
+    ):
+        response = client.post(reverse("admin:upload"))
 
-    assert response.status_code == 302
-    assert response.url == reverse("admin:workbaskets_workbasket_changelist")
+        assert response.status_code == 302
+        assert response.url == reverse("admin:workbaskets_workbasket_changelist")
 
 
-def test_upload_redirects_non_staff_to_login(self, client, valid_user):
+def test_upload_redirects_non_staff_to_login(client, valid_user):
     client.force_login(valid_user)
     response = client.post(reverse("admin:upload"))
 
@@ -22,7 +25,7 @@ def test_upload_redirects_non_staff_to_login(self, client, valid_user):
     assert response.url == f"{reverse('admin:login')}?next={reverse('admin:upload')}"
 
 
-def test_upload_doesnt_call_task_for_non_staff(self, client, valid_user):
+def test_upload_doesnt_call_task_for_non_staff(client, valid_user):
     client.force_login(valid_user)
     with mock.patch(
         "exporter.tasks.upload_workbaskets.delay",
@@ -32,7 +35,7 @@ def test_upload_doesnt_call_task_for_non_staff(self, client, valid_user):
         mock_task.assert_not_called()
 
 
-def test_upload_calls_task(self, staff_user, client):
+def test_upload_calls_task(staff_user, client):
     with mock.patch(
         "exporter.tasks.upload_workbaskets.delay",
     ) as mock_task:
