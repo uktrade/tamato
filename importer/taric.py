fb510b0ffaa9a1c746dc2fad86c09a1d8d26b0cd
Tsvetan Kintisheff <kintisheff@gmail.com>
2021-10-25 17:48:08 +0200

    don't try to write a report if nothing to report

--- importer/taric.py
+++ importer/taric.py
@@ -27,6 +27,7 @@ from importer.parsers import ElementParser
 from importer.parsers import ParserError
 from importer.parsers import TextElement
 from importer.reports import CommodityChangeReports
+from importer.reports import write_report
 from taric.models import Envelope
 from taric.models import EnvelopeTransaction
 from workbaskets.models import TransactionPartitionScheme
@@ -174,7 +175,7 @@ class TransactionParser(ElementParser):
         super().end(element)
         if element.tag == self.tag:
             logging.debug(f"Saving import {self.data['id']}")
-            if int(self.data["id"]) % 1000 == 0:
+            if int(self.data["id"]) % 10 == 0:
                 logger.info(
                     "%s transactions done in %d seconds",
                     self.data["id"],
@@ -287,4 +288,7 @@ def process_taric_xml_stream(
                 elem.clear()
 
     reports = CommodityChangeReports(handler.workbasket)
-    reports.report_affected_measures()
+    data = reports.report_affected_measures()
+
+    if data:
+        write_report(data, handler.workbasket)
