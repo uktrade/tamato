79c8283681f461c461f00343e8201451aff549db
Paul Pepper <paul.pepper@digital.trade.gov.uk>
2022-03-24 18:24:31 +0000

    Optionally omit the 'name' attr of dummy autocomplete fields.

--- common/static/common/js/application.js
+++ common/static/common/js/application.js
@@ -8,7 +8,7 @@ import initStepNav from './step-by-step-nav';
 import { initAll } from 'govuk-frontend';
 import initCheckAllCheckboxes from './checkAllCheckboxes';
 showHideCheckboxes();
-initAutocomplete();
+initAutocomplete(false);
 initStepNav();
 initAll();
 initCheckAllCheckboxes();
