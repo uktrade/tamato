2775e4e7c672ca66963959d8ca3d12ab65f48623
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2022-05-12 09:38:25 +0100

    update docstring

--- geo_areas/util.py
+++ geo_areas/util.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 def with_description_string(qs):
-    """If a description object has no description value, return an empty string,
-    else return description value."""
+    """Returns a queryset annotated with description object's description field
+    value."""
     return qs.annotate(
         description=models.F(
             "descriptions__description",
