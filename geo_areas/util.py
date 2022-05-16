49e251ea304f14ba1657150551b7c9ffc5683130
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2022-05-16 15:34:00 +0100

    update docstring and make annotated name more descriptive

--- geo_areas/util.py
+++ geo_areas/util.py
@@ -2,11 +2,12 @@ from django.db import models
 
 
 def with_latest_description_string(qs):
-    """Returns a queryset annotated with description object's description field
-    value."""
+    """Returns a queryset annotate with its latest description's validity_start
+    date, filtered by this date and then annotated with that latest description
+    object's description field value."""
     return (
-        qs.annotate(latest_date=models.Max("descriptions__validity_start"))
-        .filter(descriptions__validity_start=models.F("latest_date"))
+        qs.annotate(latest_description_date=models.Max("descriptions__validity_start"))
+        .filter(descriptions__validity_start=models.F("latest_description_date"))
         .annotate(
             description=models.F(
                 "descriptions__description",
