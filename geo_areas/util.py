e9893ab03dedf6d0376f4a92cd8a8d0c69184eaa
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2022-05-16 14:06:24 +0100

    update with_description_string to filter by latest description

--- geo_areas/util.py
+++ geo_areas/util.py
@@ -1,11 +1,15 @@
 from django.db import models
 
 
-def with_description_string(qs):
+def with_latest_description_string(qs):
     """Returns a queryset annotated with description object's description field
     value."""
-    return qs.annotate(
-        description=models.F(
-            "descriptions__description",
-        ),
+    return (
+        qs.annotate(latest_date=models.Max("descriptions__validity_start"))
+        .filter(descriptions__validity_start=models.F("latest_date"))
+        .annotate(
+            description=models.F(
+                "descriptions__description",
+            ),
+        )
     )
