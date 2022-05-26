a6fc921fd63d405c16d871a57c6b03d0cf63ded4
Lauren <laurenqurashi25@gmail.com>
2022-05-26 12:21:17 +0100

    Refactor validation test

--- certificates/tests/test_forms.py
+++ certificates/tests/test_forms.py
@@ -1,16 +1,9 @@
 import datetime
 
-import factory
 import pytest
-from dateutil.relativedelta import relativedelta
-from django.core.exceptions import ValidationError
 
 from certificates import forms
-from certificates import models
 from common.tests import factories
-from common.tests.util import date_post_data
-from common.tests.util import raises_if
-from common.tests.util import validity_period_post_data
 from common.util import TaricDateRange
 
 pytestmark = pytest.mark.django_db
@@ -51,39 +44,27 @@ def test_form_save_creates_new_certificate(
     assert certificate_b.sid == "002"
 
 
-@pytest.mark.parametrize(
-    ("new_data", "expected_valid"),
-    (
-        (lambda data: {}, False),
-        (
-            lambda data: {
-                "description": "Test description",
-                "certificate_type": "Z",
-                "valid_between": validity_period_post_data(
-                    datetime.date.today(),
-                    datetime.date.today() + relativedelta(months=+1),
-                ),
-                **date_post_data("start_date", datetime.date.today()),
-                **factory.build(
-                    dict,
-                    sid="001",
-                    certificate_type=factories.CertificateTypeFactory.create().pk,
-                    description=factories.CertificateDescriptionFactory.create().pk,
-                    FACTORY_CLASS=factories.CertificateFactory,
-                ),
-            },
-            True,
-        ),
-    ),
-)
-def test_certificate_create_form(use_create_form, new_data, expected_valid):
-    """
-    Tests that if invalid data is passed to the create certificate form, a
-    validation error is raised.
+def test_certificate_create_form_validates_data(session_with_workbasket):
+    """A test to check that the create form validates data and ciphers out
+    incorrect submissions."""
 
-    In this particular test, the first test case is empty, and we assign
-    "expected_valid" to be false. The second test case passes in valid data, and
-    we assign "expected_valid" to be true.
-    """
-    with raises_if(ValidationError, not expected_valid):
-        use_create_form(models.Certificate, new_data)
+    certificate_data = {
+        "certificate_type": "I am not right",
+        "start_date_0": 2,
+        "start_date_1": 13,
+        "start_date_2": 2022,
+        "description": "A participation certificate",
+    }
+    form = forms.CertificateCreateForm(
+        data=certificate_data,
+        request=session_with_workbasket,
+    )
+    error_string = [
+        "Select a valid choice. That choice is not one of the available choices.",
+    ]
+    date_error_string = [
+        "Month must be in 1..12",
+    ]
+    assert form.errors["certificate_type"] == error_string
+    assert form.errors["start_date"] == date_error_string
+    assert not form.is_valid()
