da8309d71ffe395d87f3c339915f26fc23aff3e4
Edie Pearce <edie.pearce@digital.trade.gov.uk>
2022-07-06 11:31:55 +0100

    Use current version instead of transaction

--- geo_areas/util.py
+++ geo_areas/util.py
@@ -3,17 +3,17 @@ from django.db import models
 
 def with_latest_description_string(qs):
     """Returns a queryset annotated with the latest validity_start date and
-    latest transaction order number, filtered by these values and then annotated
-    with that latest description object's description field value."""
+    current version, filtered by these values and then annotated with that
+    latest description object's description field value."""
     return (
         qs.annotate(
             latest_transaction_order=models.Max(
-                "descriptions__version_group__current_version__transaction__order",
+                "descriptions__version_group__current_version__pk",
             ),
             latest_description_date=models.Max("descriptions__validity_start"),
         )
         .filter(
-            descriptions__version_group__current_version__transaction__order=models.F(
+            descriptions__version_group__current_version__pk=models.F(
                 "latest_transaction_order",
             ),
             descriptions__validity_start=models.F("latest_description_date"),
