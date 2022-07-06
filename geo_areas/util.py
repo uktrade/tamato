41f921997d76b358086f653fcee9dc42efef389f
Edie Pearce <edie.pearce@digital.trade.gov.uk>
2022-07-06 10:38:42 +0100

    Keep old filtering as well, add test

--- geo_areas/util.py
+++ geo_areas/util.py
@@ -2,19 +2,21 @@ from django.db import models
 
 
 def with_latest_description_string(qs):
-    """Returns a queryset annotate with its latest description's validity_start
-    date, filtered by this date and then annotated with that latest description
-    object's description field value."""
+    """Returns a queryset annotated with the latest validity_start date and
+    latest transaction order number, filtered by these values and then annotated
+    with that latest description object's description field value."""
     return (
         qs.annotate(
             latest_transaction_order=models.Max(
                 "descriptions__version_group__current_version__transaction__order",
             ),
+            latest_description_date=models.Max("descriptions__validity_start"),
         )
         .filter(
             descriptions__version_group__current_version__transaction__order=models.F(
                 "latest_transaction_order",
             ),
+            descriptions__validity_start=models.F("latest_description_date"),
         )
         .annotate(
             description=models.F(
