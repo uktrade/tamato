5c7c1e40b0e7fbe63ac6970a583727554a22f5ad
Edie Pearce <edie.pearce@digital.trade.gov.uk>
2022-07-08 15:53:12 +0100

    TP2000-225 Fix geo area search (#617)
    * Fix geo area search

--- geo_areas/util.py
+++ geo_areas/util.py
@@ -2,12 +2,22 @@ from django.db import models
 
 
 def with_latest_description_string(qs):
-    """Returns a queryset annotate with its latest description's validity_start
-    date, filtered by this date and then annotated with that latest description
-    object's description field value."""
+    """Returns a queryset annotated with the latest validity_start date and
+    current version, filtered by these values and then annotated with that
+    latest description object's description field value."""
     return (
-        qs.annotate(latest_description_date=models.Max("descriptions__validity_start"))
-        .filter(descriptions__validity_start=models.F("latest_description_date"))
+        qs.annotate(
+            description_current_version=models.Max(
+                "descriptions__version_group__current_version__pk",
+            ),
+            latest_description_date=models.Max("descriptions__validity_start"),
+        )
+        .filter(
+            descriptions__version_group__current_version__pk=models.F(
+                "description_current_version",
+            ),
+            descriptions__validity_start=models.F("latest_description_date"),
+        )
         .annotate(
             description=models.F(
                 "descriptions__description",
