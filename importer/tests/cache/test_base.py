bfc5d767e5cd70c1844f13ada6608f4e44401120
Doug Mills <doug.mills@digital.trade.gov.uk>
2022-08-16 09:46:45 +0100

    add docstrings for tests

--- importer/tests/cache/test_base.py
+++ importer/tests/cache/test_base.py
@@ -4,6 +4,9 @@ from importer.cache import base
 
 
 def test_initializer():
+    """
+    Test that the initializer gives type error since it's an abstract class
+    """
     err = 'Can\'t instantiate abstract class BaseEngine with abstract methods clear, dump, get, keys, pop, put'
     with pytest.raises(TypeError) as ex:
         base.BaseEngine()
--- importer/tests/cache/test_cache_param.py
+++ importer/tests/cache/test_cache_param.py
@@ -42,6 +42,9 @@ def clear_object_cache():
 
 @pytest.mark.parametrize("target", test_data)
 def test_put_stores_value(target):
+    """
+    For all parameterized cache engines, test that the put method stores a value in the respective cache
+    """
     object_cache = target()
     object_cache.put("test", 123)
     assert object_cache.get("test") == 123
@@ -49,6 +52,10 @@ def test_put_stores_value(target):
 
 @pytest.mark.parametrize("target", test_data)
 def test_put_stores_value_when_value_exists(target):
+    """
+    For all parameterized cache engines, test that the multiple put calls to same key overwrites stored value in the
+    respective caches
+    """
     object_cache = target()
     object_cache.put("test", 123)
     object_cache.put("test", 456)
@@ -57,25 +64,39 @@ def test_put_stores_value_when_value_exists(target):
 
 @pytest.mark.parametrize("target", test_data)
 def test_pop_when_no_values_available_returns_none(target):
+    """
+    For all parameterized cache engines, test that pop calls to non-existent key returns None and does not throw
+    exception etc
+    """
     object_cache = target()
     assert object_cache.pop("test") is None
 
 
 @pytest.mark.parametrize("target", test_data)
-def test_pop_removes_and_returns_value_whne_present(target):
+def test_pop_removes_and_returns_value_when_present(target):
+    """
+    For all parameterized cache engines, test that pop returns and removes value from cache
+    """
     object_cache = target()
     object_cache.put("test", 123)
     assert object_cache.pop("test") == 123
+    assert object_cache.pop("test") is None
 
 
 @pytest.mark.parametrize("target", test_data)
 def test_pop_returns_none_if_not_present(target):
+    """
+    For all parameterized cache engines, test that pop returns None when value not present
+    """
     object_cache = target()
     assert object_cache.pop("test") is None
 
 
 @pytest.mark.parametrize("target", test_data)
 def test_keys_return_keys_correctly_when_populated(target):
+    """
+    For all parameterized cache engines, test that keys returns as expected when populated
+    """
     object_cache = target()
     object_cache.put("test", 123)
     assert object_cache.keys() == {"test": ""}.keys()
@@ -83,18 +104,28 @@ def test_keys_return_keys_correctly_when_populated(target):
 
 @pytest.mark.parametrize("target", test_data)
 def test_keys_return_keys_correctly_when_empty(target):
+    """
+    For all parameterized cache engines, test that keys returns as expected when empty
+    """
     object_cache = target()
     assert object_cache.keys() == {}.keys()
 
 
 @pytest.mark.parametrize("target", test_data)
 def test_dump_return_correctly(target):
+    """
+    For all parameterized cache engines, test that dump returns as expected. Only used for pickle to write to file - but
+    exists in base class.
+    """
     object_cache = target()
     assert object_cache.dump() is None
 
 
 @pytest.mark.parametrize("target", test_data)
 def test_clear_returns_correctly(target):
+    """
+    For all parameterized cache engines, test that clear does clear the cache, verified by checking keys
+    """
     object_cache = target()
     object_cache.put("test", 123)
     assert object_cache.keys() == {"test": ""}.keys()
