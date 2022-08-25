46035c53e9f2c5f24f7daf9d045feb054d7a29e4
Lauren <laurenqurashi25@gmail.com>
2022-08-25 16:51:57 +0100

    Add tests and some stuff PII is excluding

--- commodities/tests/test_views.py
+++ commodities/tests/test_views.py
@@ -6,7 +6,12 @@ from bs4 import BeautifulSoup
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.urls import reverse
 
+from commodities.views import CommodityList
+from common.tests.factories import GoodsNomenclatureDescriptionFactory
+from common.tests.factories import GoodsNomenclatureFactory
 from common.tests.factories import ImportBatchFactory
+
+pytestmark = pytest.mark.django_db
 
 TEST_FILES_PATH = path.join(path.dirname(__file__), "test_files")
 
@@ -48,3 +53,53 @@ def test_commodities_import_failure(file_name, error_msg, valid_user_client):
     assert response.status_code == 200
     soup = BeautifulSoup(str(response.content), "html.parser")
     assert error_msg in soup.select(".govuk-error-message")[0].text
+
+
+def test_commodity_list_displays_commodity_and_description(valid_user_client):
+    """Test that a list of commodity codes with links and their descriptions are
+    displayed on the list view template."""
+    commodity1 = GoodsNomenclatureDescriptionFactory.create(
+        description="A commodity code description",
+    ).described_goods_nomenclature
+    commodity2 = GoodsNomenclatureDescriptionFactory.create(
+        description="A second commodity code description",
+    ).described_goods_nomenclature
+
+    url = reverse("commodity-ui-list")
+    response = valid_user_client.get(url)
+    page = BeautifulSoup(
+        response.content.decode(response.charset),
+        "html.parser",
+    )
+    assert page.find("tbody").find("td", text=commodity1.item_id)
+    assert page.find("tbody").find(href=f"/commodities/{commodity1.sid}/")
+    assert page.find("tbody").find("td", text="A commodity code description")
+    assert page.find("tbody").find("td", text=commodity2.item_id)
+    assert page.find("tbody").find(href=f"/commodities/{commodity2.sid}/")
+    assert page.find("tbody").find("td", text="A second commodity code description")
+
+
+def test_commodity_list_queryset():
+    """Tests that commodity list queryset orders commodities by item_id."""
+    GoodsNomenclatureFactory.create_batch(3)
+    view = CommodityList()
+    qs = view.get_queryset()
+    ordered_commodities = qs.order_by("item_id")
+
+    assert qs.first().item_id == ordered_commodities[0].item_id
+    assert qs.last().item_id == ordered_commodities.last().item_id
+
+
+def test_commodity_list_filter(search_terms, valid_user_client):
+    """Tests that passing an item_id to the filter retrieves the right commodity
+    code."""
+    GoodsNomenclatureFactory.create_batch(3)
+    list_url = reverse("geo_area-ui-list")
+    url = f"{list_url}?search={search_terms}"
+    response = valid_user_client.get(url)
+    page = BeautifulSoup(
+        response.content.decode(response.charset),
+        "html.parser",
+    )
+
+    assert page.find("tbody").find("td", text="Greenland")
