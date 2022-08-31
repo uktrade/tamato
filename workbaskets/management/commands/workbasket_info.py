cb14fdb1e2f92d09f5cfb158e9ebd78603c882de
Paul Pepper <paul.pepper@digital.trade.gov.uk>
2022-08-31 13:29:41 +0100

    Factor out getting valid workbasket.

--- workbaskets/management/commands/workbasket_info.py
+++ workbaskets/management/commands/workbasket_info.py
@@ -6,7 +6,6 @@ from django.core.management import BaseCommand
 from django.core.management.base import CommandParser
 
 from workbaskets.management.util import WorkBasketCommandMixin
-from workbaskets.models import WorkBasket
 
 logger = logging.getLogger(__name__)
 
@@ -18,17 +17,7 @@ class Command(WorkBasketCommandMixin, BaseCommand):
         parser.add_argument("WORKBASKET_PK", type=int)
 
     def handle(self, *args: Any, **options: Any) -> Optional[str]:
-        workbasket_pk = int(options["WORKBASKET_PK"])
-        try:
-            workbasket = WorkBasket.objects.get(
-                pk=workbasket_pk,
-            )
-        except WorkBasket.DoesNotExist:
-            self.stderr.write(
-                self.style.ERROR(f"Workbasket pk={workbasket_pk} not found."),
-            )
-            exit(1)
-
+        workbasket = self.get_workbasket_or_exit(int(options["WORKBASKET_PK"]))
         self.stdout.write(
             self.style.SUCCESS(
                 f"WorkBaskets {workbasket.pk} status {workbasket.status}",
