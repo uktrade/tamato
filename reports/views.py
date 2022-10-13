549a500f18f7c99e4bc2c9adb2ae099248a7c9f4
Doug Mills <doug.mills@digital.trade.gov.uk>
2022-10-13 09:29:57 +0100

    Basic reporting framework added, with examples

---
+++ reports/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+import reports.reports.index as index_model
+
+# Create your views here.
+import reports.utils as utils
+
+
+def index(request):
+
+    context = {
+        "report": index_model.IndexTable(),
+    }
+
+    return render(request, "reports/index.jinja", context)
+
+
+def report(request):
+
+    # find the report based on the request
+    report_class = utils.get_report_by_slug(request.resolver_match.url_name)
+
+    context = {
+        "report": report_class(),
+    }
+
+    return render(
+        request,
+        utils.get_template_by_type(report_class.report_template),
+        context,
+    )
--- settings/common.py
+++ settings/common.py
@@ -40,6 +40,7 @@ MEDIA_ROOT = join(BASE_DIR, "run", "uploads")
 STATICFILES_DIRS = [
     join(BASE_DIR, "static"),
     join(BASE_DIR, "node_modules", "govuk-frontend", "govuk", "assets"),
+    join(BASE_DIR, "node_modules", "chart.js", "dist"),
 ]
 
 STATICFILES_FINDERS = [
@@ -96,6 +97,7 @@ DOMAIN_APPS = [
     "geo_areas.apps.GeoAreasConfig",
     "measures.apps.MeasuresConfig",
     "quotas.apps.QuotasConfig",
+    "reports.apps.ReportsConfig",
     "regulations.apps.RegulationsConfig",
 ]
 
