e03e4b69720ea3293717cc78b402d1dbc71de54a
Edie Pearce <edie.pearce@digital.trade.gov.uk>
2022-11-01 15:44:29 +0000

    Separate duties step, make conditions not break on first step

--- measures/conditions.py
+++ measures/conditions.py
@@ -3,42 +3,56 @@ from measures import constants
 
 def show_step_measure_details(wizard):
     cleaned_data = wizard.get_cleaned_data_for_step(constants.START)
-    return constants.MEASURE_DETAILS in cleaned_data.get("fields_to_edit")
+    if cleaned_data:
+        return constants.MEASURE_DETAILS in cleaned_data.get("fields_to_edit")
 
 
 def show_step_regulation_id(wizard):
     cleaned_data = wizard.get_cleaned_data_for_step(constants.START)
-    return constants.REGULATION_ID in cleaned_data.get("fields_to_edit")
+    if cleaned_data:
+        return constants.REGULATION_ID in cleaned_data.get("fields_to_edit")
 
 
 def show_step_quota_order_number(wizard):
     cleaned_data = wizard.get_cleaned_data_for_step(constants.START)
-    return constants.QUOTA_ORDER_NUMBER in cleaned_data.get("fields_to_edit")
+    if cleaned_data:
+        return constants.QUOTA_ORDER_NUMBER in cleaned_data.get("fields_to_edit")
 
 
 def show_step_geographical_area(wizard):
     cleaned_data = wizard.get_cleaned_data_for_step(constants.START)
-    return constants.GEOGRAPHICAL_AREA in cleaned_data.get("fields_to_edit")
+    if cleaned_data:
+        return constants.GEOGRAPHICAL_AREA in cleaned_data.get("fields_to_edit")
 
 
 def show_step_commodities(wizard):
     cleaned_data = wizard.get_cleaned_data_for_step(constants.START)
-    return constants.COMMODITIES in cleaned_data.get("fields_to_edit")
+    if cleaned_data:
+        return constants.COMMODITIES in cleaned_data.get("fields_to_edit")
+
+
+def show_step_duties(wizard):
+    cleaned_data = wizard.get_cleaned_data_for_step(constants.START)
+    if cleaned_data:
+        return constants.DUTIES in cleaned_data.get("fields_to_edit")
 
 
 def show_step_additional_code(wizard):
     cleaned_data = wizard.get_cleaned_data_for_step(constants.START)
-    return constants.ADDITIONAL_CODE in cleaned_data.get("fields_to_edit")
+    if cleaned_data:
+        return constants.ADDITIONAL_CODE in cleaned_data.get("fields_to_edit")
 
 
 def show_step_conditions(wizard):
     cleaned_data = wizard.get_cleaned_data_for_step(constants.START)
-    return constants.CONDITIONS in cleaned_data.get("fields_to_edit")
+    if cleaned_data:
+        return constants.CONDITIONS in cleaned_data.get("fields_to_edit")
 
 
 def show_step_footnotes(wizard):
     cleaned_data = wizard.get_cleaned_data_for_step(constants.START)
-    return constants.FOOTNOTES in cleaned_data.get("fields_to_edit")
+    if cleaned_data:
+        return constants.FOOTNOTES in cleaned_data.get("fields_to_edit")
 
 
 measure_edit_condition_dict = {
@@ -47,6 +61,7 @@ measure_edit_condition_dict = {
     constants.QUOTA_ORDER_NUMBER: show_step_quota_order_number,
     constants.GEOGRAPHICAL_AREA: show_step_geographical_area,
     constants.COMMODITIES: show_step_commodities,
+    constants.DUTIES: show_step_duties,
     constants.ADDITIONAL_CODE: show_step_additional_code,
     constants.CONDITIONS: show_step_conditions,
     constants.FOOTNOTES: show_step_footnotes,
