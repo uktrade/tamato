2ae981b6da2b61991f10918d7599408c2d8802c4
Doug Mills <doug.mills@digital.trade.gov.uk>
2022-11-14 15:26:02 +0000

    minor updates from comments

--- common/tests/test_migrations.py
+++ common/tests/test_migrations.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 import pytest
 
+from common.models.transactions import TransactionPartition
 from common.util import TaricDateRange
 from common.validators import UpdateType
 
@@ -12,35 +13,35 @@ def test_missing_current_version_fix(migrator):
     migrator.reset()
 
     """Ensures that the initial migration works."""
-    # migrator.apply_initial_migration(("measures", "0010_add_requires_to_action_and_accepts_to_condition_code"))
-    # migrator.apply_initial_migration(("workbaskets", "0005_workbasket_rule_check_task_id"))
     new_state = migrator.apply_initial_migration(("common", "0006_auto_20221114_1000"))
     # setup
 
-    # create user
     user_class = new_state.apps.get_model("auth", "User")
+    workbasket_class = new_state.apps.get_model("workbaskets", "WorkBasket")
+    measurement_unit_class = new_state.apps.get_model("measures", "MeasurementUnit")
+    transaction_class = new_state.apps.get_model("common", "Transaction")
+    version_group_class = new_state.apps.get_model("common", "VersionGroup")
+
+    # create user
     user = user_class.objects.create(
         email="123123@sdfsdf.com",
         username="sdfsdfsdf",
         first_name="sdfdsf",
         last_name="sdfsdf",
     )
-
-    workbasket_class = new_state.apps.get_model("workbaskets", "WorkBasket")
-    measurement_unit_class = new_state.apps.get_model("measures", "MeasurementUnit")
-    transaction_class = new_state.apps.get_model("common", "Transaction")
-    version_group_class = new_state.apps.get_model("common", "VersionGroup")
-
+    # Create version group
     version_group = version_group_class.objects.create()
+    # create workbasket
     workbasket = workbasket_class.objects.create(
         title=f"xxx {datetime.time}",
         reason="some reason",
         author=user,
     )
+    # create transaction
     transaction = transaction_class.objects.create(
         workbasket=workbasket,
         order=1,
-        partition=2,
+        partition=TransactionPartition.REVISION,
     )
 
     kwargs = {
@@ -53,8 +54,8 @@ def test_missing_current_version_fix(migrator):
         "version_group": version_group,
     }
 
+    # create measurement unit
     measurement_unit = measurement_unit_class.objects.create(**kwargs)
-
     measurement_unit_id = measurement_unit.trackedmodel_ptr_id
 
     # publish workbasket
@@ -63,6 +64,7 @@ def test_missing_current_version_fix(migrator):
 
     assert measurement_unit.version_group.current_version_id is None
 
+    # run fix migration
     migrator.apply_tested_migration(
         ("common", "0007_auto_20221114_1040_fix_missing_current_versions"),
     )
