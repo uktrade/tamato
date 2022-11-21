886ed3d942ffb20042eebedecc73bf3d5ce740c7
Paul Pepper <paul.pepper@digital.trade.gov.uk>
2022-11-21 15:30:19 +0000

    Include publishing urls.py.

---
+++ publishing/urls.py
@@ -0,0 +1,12 @@
+urlpatterns = [
+    # path(
+    #    "",
+    #    views.QueuedPackagedWorkbasketView.as_view(),
+    #    name="queued-packaged-workbasket-ui-list",
+    # ),
+    # path(
+    #    "export/",
+    #    views.ExportPackagedWorkbasketView.as_view(),
+    #    name="export-packaged-workbasket-ui-list",
+    # ),
+]
