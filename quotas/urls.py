331b1a39ebc9d2468005142f3aa16207cb47f937
Anthoni Gleeson <anthoni.gleeson@digital.trade.gov.uk>
2022-11-21 17:25:26 +0000

    adding missing quota file for url updates

--- quotas/urls.py
+++ quotas/urls.py
@@ -14,37 +14,30 @@ api_router.register(
 api_router.register(
     r"quota_order_number_origins",
     views.QuotaOrderNumberOriginViewset,
-    basename="quotaordernumberorigin",
 )
 api_router.register(
     r"quota_order_number_origin_exclusions",
     views.QuotaOrderNumberOriginExclusionViewset,
-    basename="quotaordernumberoriginexclusion",
 )
 api_router.register(
     r"quota_definitions",
     views.QuotaDefinitionViewset,
-    basename="quotadefinition",
 )
 api_router.register(
     r"quota_associations",
     views.QuotaAssociationViewset,
-    basename="quotaassociation",
 )
 api_router.register(
     r"quota_suspensions",
     views.QuotaSuspensionViewset,
-    basename="quotasuspension",
 )
 api_router.register(
     r"quota_blocking_periods",
     views.QuotaBlockingViewset,
-    basename="quotablockingperiod",
 )
 api_router.register(
     r"quota_events",
     views.QuotaEventViewset,
-    basename="quotaevent",
 )
 
 ui_patterns = get_ui_paths(views, "<sid:sid>")
