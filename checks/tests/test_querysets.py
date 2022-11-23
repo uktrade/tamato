5b99be2a8c393021b93e1a47b65a48515036f783
Doug Mills <doug.mills@digital.trade.gov.uk>
2022-11-23 09:37:40 +0000

    +1 to head order - the last check would end up with both transactions on the same order value - possibly the reason last() would intermittently fail

--- checks/tests/test_querysets.py
+++ checks/tests/test_querysets.py
@@ -47,9 +47,9 @@ def test_current_queryset_returns_correct_results(
     else:
         latest = common_factories.TransactionFactory.create(
             approved=True,
-            order=head_order + 1,
+            order=head_order + 2,
         )
-    assert Transaction.objects.all().approved().last() == latest
+    assert Transaction.approved.last() == latest
 
     assert_current(check, expect_current)
 
