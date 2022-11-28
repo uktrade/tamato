c4c3af234c07697ad4b53ab400b34b61fec30545
Paul Pepper <paul.pepper@digital.trade.gov.uk>
2022-11-28 19:20:30 +0000

    Template development.

--- publishing/urls.py
+++ publishing/urls.py
@@ -3,18 +3,20 @@ from django.views.generic.base import RedirectView
 
 from publishing import views
 
+url_prefix = "publishing/"
+
 urlpatterns = [
     path(
-        "publishing/",
+        url_prefix + "",
         RedirectView.as_view(pattern_name="packaged-workbasket-queue-ui-list"),
     ),
     path(
-        "publishing/packaging-queue/",
+        url_prefix + "packaging-queue/",
         views.PackagedWorkbasketQueueView.as_view(),
         name="packaged-workbasket-queue-ui-list",
     ),
     path(
-        "publishing/envelope-queue/",
+        url_prefix + "envelope-queue/",
         views.EnvelopeQueueView.as_view(),
         name="envelope-queue-ui-list",
     ),
