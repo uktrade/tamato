12a6b7544e60e4db515c37482ebb097c9415fffc
Doug Mills <doug.mills@digital.trade.gov.uk>
2022-11-29 09:41:20 +0000

    WIP commit

--- measures/migrations/0011_pre_migration_dependencies.py
+++ measures/migrations/0011_pre_migration_dependencies.py
@@ -3,13 +3,10 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("contenttypes", "0001_initial"),
-        ("footnotes", "0006_allow_blank_descriptions"),
-        ("contenttypes", "0002_remove_content_type_name"),
         ("measures", "0010_add_requires_to_action_and_accepts_to_condition_code"),
-        ("commodities", "0010_delete_goodsnomenclatureindentnode"),
-        ("geo_areas", "0005_rename_described_area"),
+        ("common", "0006_auto_20221114_1000"),
         ("workbaskets", "0005_workbasket_rule_check_task_id"),
+        ("geo_areas", "0005_rename_described_area"),
     ]
 
     operations = []
