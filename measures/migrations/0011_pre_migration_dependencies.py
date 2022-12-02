d43f7afc5936894547180c7601efd5c37ab35885
Doug Mills <doug.mills@digital.trade.gov.uk>
2022-12-02 10:14:44 +0000

    added migration, and tests that verify that the migration only performs the change if the expected related data is present

--- measures/migrations/0011_pre_migration_dependencies.py
+++ measures/migrations/0011_pre_migration_dependencies.py
@@ -3,10 +3,13 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
+        ("contenttypes", "0001_initial"),
+        ("footnotes", "0006_allow_blank_descriptions"),
+        ("contenttypes", "0002_remove_content_type_name"),
         ("measures", "0010_add_requires_to_action_and_accepts_to_condition_code"),
-        ("common", "0006_auto_20221114_1000"),
-        ("workbaskets", "0005_workbasket_rule_check_task_id"),
+        ("commodities", "0010_delete_goodsnomenclatureindentnode"),
         ("geo_areas", "0005_rename_described_area"),
+        ("workbaskets", "0005_workbasket_rule_check_task_id"),
     ]
 
     operations = []
--- measures/migrations/0012_add_back_three_missing_measures_already_published.py
+++ measures/migrations/0012_add_back_three_missing_measures_already_published.py
@@ -6,136 +6,127 @@ from common.util import TaricDateRange
 
 
 def add_back_missing_measures(apps, schemaeditor):
+    from commodities.models import GoodsNomenclature
+    from common.validators import UpdateType
+    from geo_areas.models import GeographicalArea
+    from measures.models import DutyExpression
+    from measures.models import Measure
+    from measures.models import MeasureComponent
+    from measures.models import MeasureType
+    from regulations.models import Regulation
+    from workbaskets.models import WorkBasket
+
     target_workbasket_id = 545
-    measure = apps.get_model("measures", "Measure")
-    measure_component = apps.get_model("measures", "MeasureComponent")
-    apps.get_model("measures", "MeasureCondition")
-    geographical_area = apps.get_model("geo_areas", "GeographicalArea")
-    regulation = apps.get_model("regulations", "Regulation")
-    measure_type = apps.get_model("measures", "MeasureType")
-    goods_nomenclature = apps.get_model("commodities", "GoodsNomenclature")
-    work_basket = apps.get_model("workbaskets", "WorkBasket")
-    duty_expression = apps.get_model("measures", "DutyExpression")
 
-    # Guard clauseS
-    if not work_basket.objects.filter(id=target_workbasket_id).exists():
+    # Guard clauses
+    if not WorkBasket.objects.filter(id=target_workbasket_id).exists():
         return
 
-    target_work_basket = work_basket.objects.get(id=target_workbasket_id)
+    target_work_basket = WorkBasket.objects.get(id=target_workbasket_id)
 
-    if not geographical_area.objects.filter(sid=146).exists():
+    if not GeographicalArea.objects.filter(sid=146).exists():
         return
 
-    geographical_area_canada = geographical_area.objects.get(sid=146)  # Canada
+    geographical_area_canada = GeographicalArea.objects.get(sid=146)  # Canada
 
-    if (
-        not regulation.objects.latest_approved()
-        .filter(regulation_id="C2100006", approved=True)
-        .exists()
-    ):
+    if not Regulation.objects.filter(regulation_id="C2100006", approved=True).exists():
         return
 
-    regulation_C2100006 = regulation.objects.get(
+    regulation_C2100006 = Regulation.objects.get(
         regulation_id="C2100006",
         approved=True,
     )
 
-    if not measure_type.objects.latest_approved().filter(sid=142).exists():
+    if not MeasureType.objects.filter(sid=142).exists():
         return
 
-    measure_type_tariff_pref = measure_type.objects.latest_approved().get(sid=142)
+    measure_type_tariff_pref = MeasureType.objects.get(sid=142)
 
-    if not duty_expression.objects.latest_approved().filter(sid=1).exists():
+    if not DutyExpression.objects.filter(sid=1).exists():
         return
 
-    condition_duty_expression = duty_expression.objects.latest_approved().get(sid=1)
+    condition_duty_expression = DutyExpression.objects.get(sid=1)
 
-    if (
-        not goods_nomenclature.objects.latest_approved()
-        .filter(item_id="0306920000")
-        .exists()
-    ):
+    if not GoodsNomenclature.objects.filter(item_id="0306920000").exists():
         return
-    if (
-        not goods_nomenclature.objects.latest_approved()
-        .filter(item_id="0307190000")
-        .exists()
-    ):
+    if not GoodsNomenclature.objects.filter(item_id="0307190000").exists():
         return
-    if (
-        not goods_nomenclature.objects.latest_approved()
-        .filter(item_id="0307490000")
-        .exists()
-    ):
+    if not GoodsNomenclature.objects.filter(item_id="0307490000").exists():
         return
 
     transaction = target_work_basket.new_transaction()
 
     # first measure
-    new_measure = measure.objects.create(
+    new_measure = Measure.objects.create(
+        update_type=UpdateType.CREATE,
         transaction=transaction,
         sid=20194965,
         geographical_area=geographical_area_canada,
         valid_between=TaricDateRange(date(2022, 12, 28), None),
         generating_regulation=regulation_C2100006,
         measure_type=measure_type_tariff_pref,
-        goods_nomenclature=goods_nomenclature.objects.latest_approved().get(
+        goods_nomenclature=GoodsNomenclature.objects.get(
             item_id="0306920000",
         ),
         stopped=False,
     )
 
-    measure_component.objects.create(
+    MeasureComponent.objects.create(
+        update_type=UpdateType.CREATE,
         transaction=transaction,
-        measure=new_measure,
+        component_measure=new_measure,
         duty_expression=condition_duty_expression,
-        duty_ammount=0.00,
+        duty_amount=0.00,
     )
 
     transaction = target_work_basket.new_transaction()
 
     # second measure
-    new_measure = measure.objects.create(
+    new_measure = Measure.objects.create(
+        update_type=UpdateType.CREATE,
         transaction=transaction,
         sid=20194966,
         geographical_area=geographical_area_canada,
         valid_between=TaricDateRange(date(2022, 12, 28), None),
         generating_regulation=regulation_C2100006,
         measure_type=measure_type_tariff_pref,
-        goods_nomenclature=goods_nomenclature.objects.latest_approved().get(
+        goods_nomenclature=GoodsNomenclature.objects.latest_approved().get(
             item_id="0307190000",
         ),
         stopped=False,
     )
 
-    measure_component.objects.create(
+    MeasureComponent.objects.create(
+        update_type=UpdateType.CREATE,
         transaction=transaction,
-        measure=new_measure,
+        component_measure=new_measure,
         duty_expression=condition_duty_expression,
-        duty_ammount=0.00,
+        duty_amount=0.00,
     )
 
     transaction = target_work_basket.new_transaction()
 
     # third measure
-    new_measure = measure.objects.create(
+    new_measure = Measure.objects.create(
+        update_type=UpdateType.CREATE,
         transaction=transaction,
         sid=20194967,
         geographical_area=geographical_area_canada,
         valid_between=TaricDateRange(date(2022, 12, 28), None),
         generating_regulation=regulation_C2100006,
         measure_type=measure_type_tariff_pref,
-        goods_nomenclature=goods_nomenclature.objects.latest_approved().get(
+        goods_nomenclature=GoodsNomenclature.objects.latest_approved().get(
             item_id="0307490000",
         ),
         stopped=False,
     )
 
-    measure_component.objects.create(
+    MeasureComponent.objects.create(
+        update_type=UpdateType.CREATE,
         transaction=transaction,
-        measure=new_measure,
+        component_measure=new_measure,
         duty_expression=condition_duty_expression,
-        duty_ammount=0.00,
+        duty_amount=0.00,
     )
 
     target_work_basket.save()
