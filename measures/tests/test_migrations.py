fe485e8a213cebdfd605ba23f47b1e4b75a012bf
Doug Mills <doug.mills@digital.trade.gov.uk>
2022-12-02 10:14:44 +0000

    added migration, and tests that verify that the migration only performs the change if the expected related data is present

--- measures/tests/test_migrations.py
+++ measures/tests/test_migrations.py
@@ -1,66 +1,132 @@
-import pytest
+from datetime import date
+from datetime import timedelta
 
+import pytest
+from psycopg2._range import DateTimeTZRange
+
+import conftest
 from common.tests.factories import ApprovedWorkBasketFactory
 from common.tests.factories import DutyExpressionFactory
 from common.tests.factories import GeographicalAreaFactory
 from common.tests.factories import GoodsNomenclatureFactory
 from common.tests.factories import MeasureTypeFactory
+from common.tests.factories import RegulationFactory
+from common.tests.factories import RegulationGroupFactory
 
 
 @pytest.mark.django_db()
 def test_add_back_deleted_measures(migrator):
-    migrator.reset()
-
     """Ensures that the initial migration works."""
-    new_state = migrator.apply_initial_migration(
+    old_state = migrator.apply_initial_migration(
         (
             "measures",
             "0011_pre_migration_dependencies",
         ),
     )
 
+    conftest.setup_content_types(old_state.apps)
+
     # setup
     target_workbasket_id = 545
 
-    new_state.apps.get_model("geo_areas", "GeographicalArea")
-    new_state.apps.get_model("regulations", "Regulation")
-    new_state.apps.get_model("measures", "MeasureType")
-    new_state.apps.get_model("commodities", "GoodsNomenclature")
-    new_state.apps.get_model("workbaskets", "WorkBasket")
-    new_state.apps.get_model("measures", "DutyExpression")
-    measurement_class = new_state.apps.get_model("measures", "Measure")
+    measurement_class = old_state.apps.get_model("measures", "Measure")
+
+    assert measurement_class.objects.filter(sid=20194965).exists() is False
+    assert measurement_class.objects.filter(sid=20194966).exists() is False
+    assert measurement_class.objects.filter(sid=20194967).exists() is False
 
     # mock up workbasket
-    new_work_basket = ApprovedWorkBasketFactory.create(id=target_workbasket_id)
-    new_work_basket.save()
+    new_work_basket = ApprovedWorkBasketFactory.create(id=target_workbasket_id).save()
 
-    new_geographical_area = GeographicalAreaFactory.create(sid=146)
-    new_geographical_area.save()
-    new_measure_type = MeasureTypeFactory.create(
+    # create the three goods
+    goods_1 = GoodsNomenclatureFactory.create(item_id="0306920000").save(
+        force_write=True,
+    )
+    goods_2 = GoodsNomenclatureFactory.create(item_id="0307190000").save(
+        force_write=True,
+    )
+    goods_3 = GoodsNomenclatureFactory.create(item_id="0307490000").save(
+        force_write=True,
+    )
+
+    # create the geo area
+    new_geographical_area = GeographicalAreaFactory.create(sid=146).save(
+        force_write=True,
+    )
+
+    # create the regulation group
+    new_regulation_group = RegulationGroupFactory.create(
+        valid_between=DateTimeTZRange(
+            date.today() + timedelta(days=-1000),
+            date.today() + timedelta(days=1000),
+        ),
+    ).save(force_write=True)
+
+    # create the regulation
+    new_regulation = RegulationFactory.create(
+        valid_between=DateTimeTZRange(
+            date.today() + timedelta(days=-1000),
+            date.today() + timedelta(days=1000),
+        ),
+        regulation_group=new_regulation_group,
         regulation_id="C2100006",
         approved=True,
+    ).save(force_write=True)
+
+    # create the measure type
+    new_measure_type = MeasureTypeFactory.create(sid=142).save(force_write=True)
+
+    # create the duty expression
+    new_duty_expression = DutyExpressionFactory.create(sid=1).save(force_write=True)
+
+    # at this point all the appropriate elements are available within the database for the migration to create the
+    # measures and conditions
+
+    # run fix migration
+    new_state = migrator.apply_tested_migration(
+        ("measures", "0012_add_back_three_missing_measures_already_published"),
     )
-    new_measure_type.save()
-    new_duty_expression = DutyExpressionFactory.create(sid=1)
-    new_duty_expression.save()
-    goods_1 = GoodsNomenclatureFactory.create(item_id="0306920000")
-    goods_1.save()
-    goods_2 = GoodsNomenclatureFactory.create(item_id="0307190000")
-    goods_2.save()
-    goods_3 = GoodsNomenclatureFactory.create(item_id="0307490000")
-    goods_3.save()
+
+    measurement_class = new_state.apps.get_model("measures", "Measure")
+
+    # we should be able to get the measurements from the database now
+    assert measurement_class.objects.filter(sid=20194965).exists() is True
+    assert measurement_class.objects.filter(sid=20194966).exists() is True
+    assert measurement_class.objects.filter(sid=20194967).exists() is True
+
+    migrator.reset()
+
+
+@pytest.mark.django_db()
+def test_add_back_deleted_measures_fails_silently_if_data_not_present(migrator):
+    """Ensures that the initial migration works when no data to create measures
+    are present, for local dev etc."""
+
+    old_state = migrator.apply_initial_migration(
+        (
+            "measures",
+            "0011_pre_migration_dependencies",
+        ),
+    )
+
+    conftest.setup_content_types(old_state.apps)
+
+    measurement_class = old_state.apps.get_model("measures", "Measure")
 
     assert measurement_class.objects.filter(sid=20194965).exists() is False
     assert measurement_class.objects.filter(sid=20194966).exists() is False
     assert measurement_class.objects.filter(sid=20194967).exists() is False
 
     # run fix migration
-    migrator.apply_tested_migration(
+    new_state = migrator.apply_tested_migration(
         ("measures", "0012_add_back_three_missing_measures_already_published"),
     )
 
-    assert measurement_class.objects.filter(sid=20194965).exists() is True
-    assert measurement_class.objects.filter(sid=20194966).exists() is True
-    assert measurement_class.objects.filter(sid=20194967).exists() is True
+    measurement_class = new_state.apps.get_model("measures", "Measure")
+
+    # we not get records, but also not get an exception
+    assert measurement_class.objects.filter(sid=20194965).exists() is False
+    assert measurement_class.objects.filter(sid=20194966).exists() is False
+    assert measurement_class.objects.filter(sid=20194967).exists() is False
 
     migrator.reset()
