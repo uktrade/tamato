cf70fbe5404695b4d3713a40a3461a3d374f53d0
tommacca <tom.mcdonagh@trade.gov.uk>
2022-12-05 15:17:55 +0000

    - fixes migration dependency

--- commodities/migrations/0011_TOPS_745_migration_dependencies.py
+++ commodities/migrations/0011_TOPS_745_migration_dependencies.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('commodities', '0010_delete_goodsnomenclatureindentnode'),
+        ('commodities', '0011_delete_goodsnomenclatureindentnode'),
     ]
 
     operations = [
