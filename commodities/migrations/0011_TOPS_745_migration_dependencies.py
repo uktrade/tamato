6220981745178e75d554fdb3f54519b2aecf0d8f
Doug Mills <doug.mills@digital.trade.gov.uk>
2022-12-05 16:09:43 +0000

    fix migration tests

--- commodities/migrations/0011_TOPS_745_migration_dependencies.py
+++ commodities/migrations/0011_TOPS_745_migration_dependencies.py
@@ -6,8 +6,16 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('commodities', '0010_delete_goodsnomenclatureindentnode'),
+        ("contenttypes", "0001_initial"),
+        ("footnotes", "0006_allow_blank_descriptions"),
+        ("contenttypes", "0002_remove_content_type_name"),
+        ("measures", "0010_add_requires_to_action_and_accepts_to_condition_code"),
+        ("commodities", "0010_delete_goodsnomenclatureindentnode"),
+        ("geo_areas", "0005_rename_described_area"),
+        ("workbaskets", "0005_workbasket_rule_check_task_id"),
+        ("importer", "0003_auto_20210302_1349"),
+        ("taric", "0002_auto_20210219_1043"),
+        ("exporter", "0004_upload_created_date"),
     ]
 
-    operations = [
-    ]
+    operations = []
--- commodities/tests/test_migrations.py
+++ commodities/tests/test_migrations.py
@@ -1,8 +1,11 @@
 import datetime
+
 import pytest
 
 import conftest
-from common.tests.factories import GoodsNomenclatureDescriptionFactory
+from common.tests.factories import ApprovedWorkBasketFactory
+from common.util import TaricDateRange
+from common.validators import UpdateType
 
 
 @pytest.mark.django_db()
@@ -12,27 +15,56 @@ def test_main_migration0012(migrator):
 
     # before migration
     old_state = migrator.apply_initial_migration(
-        ('commodities', '0011_TOPS_745_migration_dependencies'))
+        ("commodities", "0011_TOPS_745_migration_dependencies"),
+    )
 
     conftest.setup_content_types(old_state.apps)
 
-    GoodsNomenclatureDescription = old_state.apps.get_model('commodities',
-                                                            'GoodsNomenclatureDescription')
+    GoodsNomenclatureDescription = old_state.apps.get_model(
+        "commodities",
+        "GoodsNomenclatureDescription",
+    )
+    GoodsNomenclature = old_state.apps.get_model("commodities", "GoodsNomenclature")
+    Transaction = old_state.apps.get_model("common", "Transaction")
+    Workbasket = old_state.apps.get_model("workbaskets", "WorkBasket")
+    VersionGroup = old_state.apps.get_model("common", "VersionGroup")
 
-    gn_description = GoodsNomenclatureDescriptionFactory.create(
-        trackedmodel_ptr_id=10008934,
-        validity_start=datetime.date(2021, 1, 6)
+    ApprovedWorkBasketFactory.create().save()
+    workbasket = Workbasket.objects.last()
+    new_transaction = Transaction.objects.create(
+        workbasket=workbasket,
+        order=99,
     )
 
+    GoodsNomenclature.objects.create(
+        update_type=UpdateType.CREATE,
+        transaction=new_transaction,
+        version_group=VersionGroup.objects.create(),
+        valid_between=TaricDateRange(datetime.date(2020, 1, 6)),
+        statistical=False,
+    ).save()
+
+    gnd = GoodsNomenclatureDescription.objects.create(
+        update_type=UpdateType.CREATE,
+        trackedmodel_ptr_id=10008934,
+        transaction=new_transaction,
+        validity_start=datetime.date(2021, 1, 6),
+        described_goods_nomenclature_id=GoodsNomenclature.objects.last().trackedmodel_ptr_id,
+        version_group=VersionGroup.objects.create(),
+    ).save()
+
     assert GoodsNomenclatureDescription.objects.get(
-        trackedmodel_ptr_id=10008934).validity_start == datetime.date(2021, 1,
-                                                                      6)
+        trackedmodel_ptr_id=10008934,
+    ).validity_start == datetime.date(2021, 1, 6)
 
     # after migration
-    new_state = migrator.before(
-        ('commodities', '0012_TOPS_745_description_date_fix'))
-    GoodsNomenclatureDescription = new_state.apps.get_model('commodities',
-                                                            'GoodsNomenclatureDescription')
+    new_state = migrator.apply_tested_migration(
+        ("commodities", "0012_TOPS_745_description_date_fix"),
+    )
+    GoodsNomenclatureDescription = new_state.apps.get_model(
+        "commodities",
+        "GoodsNomenclatureDescription",
+    )
     assert GoodsNomenclatureDescription.objects.get(
-        trackedmodel_ptr_id=10008934).validity_start == datetime.date(2022, 1,
-                                                                      6)
+        trackedmodel_ptr_id=10008934,
+    ).validity_start == datetime.date(2022, 1, 6)
