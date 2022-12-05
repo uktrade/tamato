38a7d569610b7c5b30c90d29ff202dcaa8ffcf20
Doug Mills <doug.mills@digital.trade.gov.uk>
2022-12-05 08:15:40 +0000

    initial seed setup - WIP

---
+++ common/management/commands/seed.py
@@ -0,0 +1,38 @@
+from django.core.management.base import BaseCommand
+
+# python manage.py seed --mode=refresh
+
+""" Clear all data and creates addresses """
+MODE_REFRESH = "refresh"
+
+""" Clear all data and do not create any object """
+MODE_CLEAR = "clear"
+
+
+class Command(BaseCommand):
+    help = "seed database for testing and development."
+
+    def add_arguments(self, parser):
+        parser.add_argument("--mode", type=str, help="Mode")
+
+    def handle(self, *args, **options):
+        run_seed(self, options["mode"])
+
+
+def clear_data():
+    """Deletes all the table data."""
+
+
+def run_seed(self, mode):
+    """
+    Seed database based on mode.
+
+    :param mode: refresh / clear
+    :return:
+    """
+    # Clear data from tables
+    clear_data()
+    if mode == MODE_CLEAR:
+        return
+
+    # Creating 15 addresses
