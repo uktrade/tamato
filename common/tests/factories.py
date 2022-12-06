a817baefe2bab36dc07a1a19984a7e3a402f1173
Gabe Naughton <gabriel.naughton@digital.trade.gov.uk>
2022-12-06 16:39:11 +0000

    create notifications app; add migrations; add send_emails task; update docs (#731)

--- common/tests/factories.py
+++ common/tests/factories.py
@@ -1209,6 +1209,22 @@ class BatchDependenciesFactory(factory.django.DjangoModelFactory):
     depends_on = factory.SubFactory(ImportBatchFactory)
 
 
+class NotifiedUserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = "notifications.NotifiedUser"
+
+    email = factory.Faker("email")
+    enrol_packaging = True
+
+
+class NotificationLogFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = "notifications.NotificationLog"
+
+    template_id = string_sequence(length=10)
+    recipients = factory.Faker("text", max_nb_chars=24)
+
+
 class PackagedWorkBasketFactory(factory.django.DjangoModelFactory):
     """Creates a PackagedWorkBasket instance associated with an approved
     WorkBasket."""
---
+++ notifications/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+
+from notifications.forms import NotifiedUserAdminForm
+from notifications.models import NotificationLog
+from notifications.models import NotifiedUser
+
+
+class NotifiedUserAdmin(admin.ModelAdmin):
+    form = NotifiedUserAdminForm
+
+
+class NotificationLogAdmin(admin.ModelAdmin):
+    """Class providing read-only list and detail views for notification logs."""
+
+    readonly_fields = []
+
+    def get_readonly_fields(self, request, obj=None):
+        return list(self.readonly_fields) + [field.name for field in obj._meta.fields]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def changeform_view(self, request, object_id=None, form_url="", extra_context=None):
+        extra_context = extra_context or {}
+        extra_context["show_save_and_continue"] = False
+        extra_context["show_save"] = False
+        return super(NotificationLogAdmin, self).changeform_view(
+            request,
+            object_id,
+            extra_context=extra_context,
+        )
+
+
+admin.site.register(NotifiedUser, NotifiedUserAdmin)
+admin.site.register(NotificationLog, NotificationLogAdmin)
