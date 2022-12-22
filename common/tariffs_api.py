c8614b68449ecd516540395e794adcf4c54e8fb9
Edie Pearce <edie.pearce@digital.trade.gov.uk>
2022-12-22 16:17:18 +0000

    Get data from tariffs api

---
+++ common/tariffs_api.py
@@ -0,0 +1,69 @@
+import asyncio
+from urllib.parse import urlencode
+
+import aiohttp
+import requests
+
+BASE_URL = "https://www.trade-tariff.service.gov.uk/api/v2/"
+QUOTAS = f"{BASE_URL}quotas/search"
+
+
+def get_quota_data(order_number):
+    params = urlencode({"order_number": order_number})
+    response = requests.get(f"{QUOTAS}?{params}")
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+
+async def async_get(url, session):
+    async with session.get(url=url) as response:
+        try:
+            assert response.status == 200
+        except AssertionError:
+            return None
+        return await response.json()
+
+
+async def async_get_all(urls):
+    async with aiohttp.ClientSession() as session:
+        return await asyncio.gather(*[async_get(url, session) for url in urls])
+
+
+def get_quota_definitions_data(order_number, object_list):
+    """
+    Since the API does not return all definition periods past and future from
+    one endpoint we need to make multiple requests with different params.
+
+    We use the quota order number and start date of each of its definition
+    periods to build urls to get the data for all of them.
+    """
+
+    definition_params = [
+        {
+            "order_number": order_number,
+            "year": d.valid_between.lower.year,
+            "month": d.valid_between.lower.month,
+            "day": d.valid_between.lower.day,
+        }
+        for d in object_list
+    ]
+
+    urls = [f"{QUOTAS}?{urlencode(params)}" for params in definition_params]
+
+    data = asyncio.run(async_get_all(urls))
+
+    json_data = [
+        json["data"][0]["attributes"] for json in data if json and json["data"]
+    ]
+
+    serialized = {
+        json["quota_definition_sid"]: {
+            "status": json["status"],
+            "balance": json["balance"],
+        }
+        for json in json_data
+    }
+
+    return serialized
