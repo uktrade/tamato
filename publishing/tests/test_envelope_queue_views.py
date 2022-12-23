be8434d2d24ec8bc671aef84ece0a4555e966185
Paul Pepper <paul.pepper@digital.trade.gov.uk>
2022-12-23 10:34:05 +0000

    Add unit tests for accept and reject form views.

--- publishing/tests/test_envelope_queue_views.py
+++ publishing/tests/test_envelope_queue_views.py
@@ -62,3 +62,59 @@ def test_processing_envelope(valid_user_client):
     process_envelope = page.select("table.queued-envelopes tbody .process-envelope")
     assert len(process_envelope) == 1
     assert "Download envelope" in process_envelope[0].text
+
+
+def test_accept_envelope(valid_user_client):
+    packaged_work_basket = factories.PackagedWorkBasketFactory()
+    packaged_work_basket.begin_processing()
+
+    accept_view_url = reverse(
+        "publishing:accept-envelope-ui-list",
+        kwargs={"pk": packaged_work_basket.pk},
+    )
+    # Get the form view and ensure it renders correctly.
+    response = valid_user_client.get(accept_view_url)
+    page = BeautifulSoup(
+        response.content.decode(response.charset),
+        "html.parser",
+    )
+    accept_envelope = page.select("h1")
+    assert "Accept envelope" in accept_envelope[0].text
+
+    # Submit the form and test the packaged workbasket has transitioned correctly.
+    response = valid_user_client.post(
+        accept_view_url,
+        {"report_file": "", "comment": "Test comment."},
+    )
+    assert response.status_code == 302
+    packaged_work_basket.refresh_from_db()
+    assert (
+        packaged_work_basket.processing_state == ProcessingState.SUCCESSFULLY_PROCESSED
+    )
+
+
+def test_reject_envelope(valid_user_client):
+    packaged_work_basket = factories.PackagedWorkBasketFactory()
+    packaged_work_basket.begin_processing()
+
+    reject_view_url = reverse(
+        "publishing:reject-envelope-ui-list",
+        kwargs={"pk": packaged_work_basket.pk},
+    )
+    # Get the form view and ensure it renders correctly.
+    response = valid_user_client.get(reject_view_url)
+    page = BeautifulSoup(
+        response.content.decode(response.charset),
+        "html.parser",
+    )
+    reject_envelope = page.select("h1")
+    assert "Reject envelope" in reject_envelope[0].text
+
+    # Submit the form and test the packaged workbasket has transitioned correctly.
+    response = valid_user_client.post(
+        reject_view_url,
+        {"report_file": "", "comment": "Test comment."},
+    )
+    assert response.status_code == 302
+    packaged_work_basket.refresh_from_db()
+    assert packaged_work_basket.processing_state == ProcessingState.FAILED_PROCESSING
