83f9b1541ac49a956c37295280a19e9e432b690e
Dale Cannon <dale.cannon@digital.trade.gov.uk>
2023-02-02 16:38:50 +0000

    Add and update tests

--- common/tests/test_views.py
+++ common/tests/test_views.py
@@ -17,14 +17,25 @@ def test_index_displays_workbasket_action_form(valid_user_client):
     assert response.status_code == 200
 
     page = BeautifulSoup(str(response.content), "html.parser")
-    assert "What would you like to do?" in page.select("legend")[0].text
-    assert "Edit an existing workbasket" in page.select("label")[1].text
-    assert "Create a new workbasket" in page.select("label")[0].text
+    assert "Create new workbasket" in page.select("label")[0].text
+    assert "Select an existing workbasket" in page.select("label")[1].text
+    assert "Order and package workbaskets" in page.select("label")[2].text
+    assert (
+        "Customs Declaration Service (CDS) - Process envelopes"
+        in page.select("label")[3].text
+    )
+    assert "Search the tariff" in page.select("label")[4].text
 
 
 @pytest.mark.parametrize(
     ("data", "response_url"),
     (
+        (
+            {
+                "workbasket_action": "CREATE",
+            },
+            "workbaskets:workbasket-ui-create",
+        ),
         (
             {
                 "workbasket_action": "EDIT",
@@ -33,9 +44,21 @@ def test_index_displays_workbasket_action_form(valid_user_client):
         ),
         (
             {
-                "workbasket_action": "CREATE",
+                "workbasket_action": "PACKAGE_WORKBASKETS",
             },
-            "workbaskets:workbasket-ui-create",
+            "publishing:packaged-workbasket-queue-ui-list",
+        ),
+        (
+            {
+                "workbasket_action": "PROCESS_ENVELOPES",
+            },
+            "publishing:envelope-queue-ui-list",
+        ),
+        (
+            {
+                "workbasket_action": "SEARCH",
+            },
+            "search-page",
         ),
     ),
 )
@@ -76,6 +99,16 @@ def test_app_info(valid_user_client):
     assert "Active business rule checks" in page.select("h2")[0].text
 
 
+def test_index_displays_navbar_links(valid_user_client):
+    response = valid_user_client.get(reverse("home"))
+
+    assert response.status_code == 200
+
+    page = BeautifulSoup(str(response.content), "html.parser")
+    links = page.select(".homepage-navigation > ul > li > a")
+    assert len(links) == 4
+
+
 def test_index_displays_footer_links(valid_user_client):
     response = valid_user_client.get(reverse("home"))
 
@@ -89,6 +122,16 @@ def test_index_displays_footer_links(valid_user_client):
         a_tags[0].attrs["href"]
         == "https://workspace.trade.gov.uk/working-at-dit/policies-and-guidance/policies/tariff-application-privacy-policy/"
     )
+
+
+def test_search_page_response(client):
+    url = reverse("search-page")
+    response = client.get(url)
+    assert response.status_code == 200
+
+    page = BeautifulSoup(str(response.content), "html.parser")
+    links = page.select(".govuk-link")
+    assert len(links) == 8
 
 
 def test_handler403(client):
