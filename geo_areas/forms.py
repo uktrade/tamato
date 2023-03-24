5e022fdddf092849ecd48b3c6641382600e515ec
Dale Cannon <dale.cannon@digital.trade.gov.uk>
2023-03-24 12:22:19 +0000

    Implement edit geo area end date

--- geo_areas/forms.py
+++ geo_areas/forms.py
@@ -1,6 +1,11 @@
+from crispy_forms_gds.helper import FormHelper
 from crispy_forms_gds.layout import Field
+from crispy_forms_gds.layout import Layout
+from crispy_forms_gds.layout import Size
+from crispy_forms_gds.layout import Submit
 
 from common.forms import CreateDescriptionForm
+from common.forms import ValidityPeriodForm
 from common.forms import delete_form_for
 from geo_areas.models import GeographicalArea
 from geo_areas.models import GeographicalAreaDescription
@@ -27,3 +32,42 @@ GeographicalAreaDeleteForm = delete_form_for(GeographicalArea)
 
 
 GeographicalAreaDescriptionDeleteForm = delete_form_for(GeographicalAreaDescription)
+
+
+class GeographicalAreaEndDateForm(ValidityPeriodForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["start_date"].required = False
+        self.fields[
+            "end_date"
+        ].help_text = (
+            "Leave empty if a geographical area is needed for an unlimited time"
+        )
+
+    def clean(self):
+        self.cleaned_data["start_date"] = self.instance.valid_between.lower
+        return super().clean()
+
+    class Meta:
+        model = GeographicalArea
+        fields = ("valid_between",)
+
+
+class GeographicalAreaEditForm(GeographicalAreaEndDateForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper(self)
+        self.helper.label_size = Size.SMALL
+        self.helper.legend_size = Size.SMALL
+
+        self.helper.layout = Layout(
+            "end_date",
+            Submit(
+                "submit",
+                "Save",
+                data_module="govuk-button",
+                data_prevent_double_click="true",
+            ),
+        )
