038601859248431308bdbd9ae8ed7acc4b1126d6
Dale Cannon <dale.cannon@digital.trade.gov.uk>
2023-04-05 15:49:39 +0100

    Re-arrange form tests in file

--- geo_areas/tests/test_forms.py
+++ geo_areas/tests/test_forms.py
@@ -33,6 +33,24 @@ def test_geographical_area_create_description_form_missing_required_fields():
     assert "Enter the day, month and year" in form.errors["validity_start"]
 
 
+def test_geographical_area_create_description_form_invalid_description(date_ranges):
+    """Test that GeographicalAreaCreateDescriptionForm is not valid when
+    restricted characters are used."""
+    geo_area = factories.GeographicalAreaFactory.create()
+    data = {
+        "described_geographicalarea": geo_area.pk,
+        "description": "<bad_code></script>",
+        "validity_start_0": date_ranges.normal.lower.day,
+        "validity_start_1": date_ranges.normal.lower.month,
+        "validity_start_2": date_ranges.normal.lower.year,
+        "validity_start_2": date_ranges.normal.lower.year,
+    }
+    form = forms.GeographicalAreaCreateDescriptionForm(data=data)
+
+    assert not form.is_valid()
+    assert "Only symbols .,/()&£$@!+-% are allowed." in form.errors["description"]
+
+
 def test_geographical_area_end_date_form_valid(date_ranges):
     geo_area = factories.GeographicalAreaFactory.create(
         valid_between=date_ranges.normal,
@@ -61,21 +79,3 @@ def test_geographical_area_end_date_form_invalid(date_ranges):
     form = forms.GeographicalAreaEndDateForm(data=form_data, instance=geo_area)
 
     assert not form.is_valid()
-
-
-def test_geographical_area_create_description_form_invalid_description(date_ranges):
-    """Test that GeographicalAreaCreateDescriptionForm is not valid when
-    restricted characters are used."""
-    geo_area = factories.GeographicalAreaFactory.create()
-    data = {
-        "described_geographicalarea": geo_area.pk,
-        "description": "<bad_code></script>",
-        "validity_start_0": date_ranges.normal.lower.day,
-        "validity_start_1": date_ranges.normal.lower.month,
-        "validity_start_2": date_ranges.normal.lower.year,
-        "validity_start_2": date_ranges.normal.lower.year,
-    }
-    form = forms.GeographicalAreaCreateDescriptionForm(data=data)
-
-    assert not form.is_valid()
-    assert "Only symbols .,/()&£$@!+-% are allowed." in form.errors["description"]
