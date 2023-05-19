cd1471b0975b3cc16db83fb088504f1d63c90a5b
Edie Pearce <edie.pearce@digital.trade.gov.uk>
2023-05-19 11:49:13 +0100

    Add docstring

--- commodities/helpers.py
+++ commodities/helpers.py
@@ -5,6 +5,9 @@ from measures.snapshots import MeasureSnapshot
 
 
 def get_measures_on_declarable_commodities(transaction, item_id, date=None):
+    """Uses CommodityTreeSnapshot and MeasureSnapshot to look up the commodity
+    tree to find measures defined on parent commodities that therefore also
+    apply to the given child commodity."""
     prefix = item_id[0:2]
     commodities_collection = CommodityCollectionLoader(prefix=prefix).load()
 
