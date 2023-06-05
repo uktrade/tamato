0702246bb729998b936b795d5386f4f9b72b3a7f
Anthoni Gleeson <anthoni.gleeson@digital.trade.gov.uk>
2023-06-05 16:42:31 +0100

    remove comment

--- publishing/tasks.py
+++ publishing/tasks.py
@@ -90,32 +90,6 @@ def schedule_create_xml_envelope_file(
         packaged_work_basket.save()
 
 
-LOCK_EXPIRE = 60 * 10  # Lock expires in 10 minutes
-API_TASK_LOCK = "crown_dependencies_lock"
-
-# from contextlib import contextmanager
-# from django.core.cache import cache
-# from hashlib import md5
-# from djangofeeds.models import Feed
-
-# @contextmanager
-# def memcache_lock(lock_id, oid):
-#     timeout_at = time.monotonic() + LOCK_EXPIRE - 3
-#     # cache.add fails if the key already exists
-#     status = cache.add(lock_id, oid, LOCK_EXPIRE)
-#     try:
-#         yield status
-#     finally:
-#         # memcache delete is very slow, but we have to use it to take
-#         # advantage of using add() for atomic locking
-#         if time.monotonic() < timeout_at and status:
-#             # don't release the lock if we exceeded the timeout
-#             # to lessen the chance of releasing an expired lock
-#             # owned by someone else
-#             # also don't release the lock if we didn't acquire it
-#             cache.delete(lock_id)
-
-
 @app.task(
     default_retry_delay=settings.CROWN_DEPENDENCIES_API_DEFAULT_RETRY_DELAY,
     max_retries=settings.CROWN_DEPENDENCIES_API_MAX_RETRIES,
