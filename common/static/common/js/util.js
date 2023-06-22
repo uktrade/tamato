e1357c7339788e2eb0b5774060b8d6d072dcdf85
Edie Pearce <edie.pearce@digital.trade.gov.uk>
2023-06-22 11:48:41 +0100

    Add autocomplete progressive enhancement js

--- common/static/common/js/util.js
+++ common/static/common/js/util.js
@@ -7,4 +7,19 @@ const nodeListForEach = (nodes, callback) => {
     }
 }
 
-export { nodeListForEach }
+const newLine = /\n/g;
+const removeNewLine = (str) => str.replace(newLine, "")
+
+const cleanResults = (results) => {
+    /*
+    Results which contain new line characters are considered as a new query when
+    selected, causing results to be repopulated unnecessarily. To prevent this,
+    we can remove new line characters from results since they're unimportant here.
+    */
+    for (let i = 0, len = results.length; i < len; i++) {
+        results[i].label = removeNewLine(results[i].label);
+    }
+    return results
+}
+
+export { nodeListForEach, removeNewLine, cleanResults }
