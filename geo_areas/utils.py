71666fd1775c33ca61eede6b4487e17dc15436c3
Dale Cannon <dale.cannon@digital.trade.gov.uk>
2023-06-28 12:37:53 +0100

    Use current() and return unique members

--- geo_areas/utils.py
+++ geo_areas/utils.py
@@ -4,21 +4,21 @@ from geo_areas.validators import AreaCode
 
 
 def get_all_members_of_geo_groups(validity, geo_areas):
-    all_members = []
+    all_members = set()
     for geo_area in geo_areas:
         if geo_area.area_code == AreaCode.GROUP:
-            valid_memberships = [
-                member
-                for member in GeographicalMembership.objects.filter(
-                    geo_group=geo_area,
-                )
-                if date_ranges_overlap(member.valid_between, validity)
-            ]
-            origins = set(m.member for m in valid_memberships)
-            for membership in valid_memberships:
-                if membership.member.sid in [m.sid for m in origins]:
-                    all_members.append(membership.member)
+            all_members.update(
+                {
+                    membership.member
+                    for membership in GeographicalMembership.objects.filter(
+                        geo_group=geo_area,
+                    )
+                    .current()
+                    .prefetch_related("member")
+                    if date_ranges_overlap(membership.valid_between, validity)
+                },
+            )
         else:
-            all_members.append(geo_area)
+            all_members.add(geo_area)
 
     return all_members
