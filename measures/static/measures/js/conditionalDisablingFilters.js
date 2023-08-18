3bfe374d3849ad0500eeb66849df389d0413714b
Charles Prichard <charles.prichard@digital.trade.gov.uk>
2023-08-18 11:01:10 +0100

    note in conditionalDisablingFilters.js

--- measures/static/measures/js/conditionalDisablingFilters.js
+++ measures/static/measures/js/conditionalDisablingFilters.js
@@ -1,4 +1,5 @@
-// conditionally disables a filter based on the condition of another filter
+// conditionally disables modc filter based on goods_nomenclature_autocomplete filter having a value
+// TODO: refactor to allow passing two widget ids: 1. widget condition is based on 2. widget to be disabled.
 
 const setFilterDisabledToggle = () => {
     document.addEventListener('DOMContentLoaded', function() {
