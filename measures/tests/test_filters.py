3c0ed2dfc9c51ea4066f97d498a30b09d0175cb1
Charlie Prichard <46421052+CPrich905@users.noreply.github.com>
2023-08-21 16:03:38 +0100

    TP-2000-875 - Implement the improved Find and Edit measures design - Search page (#995)
    * reorder of widgets on measures search page; created new widget to add certificates on same
    
    * current filter seems to work, confirming
    
    * active measures and current workbasket as radios
    
    * investigating styling change when results show
    
    * removing filter-layout as discussed with Edie
    
    * conditional disabling of modc on value of goods_nomenclature
    
    * refactored filters.py to change from setting ._mutable = True (undocumented) to making a mutable deep copy instead
    
    * initial responses to CR: removed extra div; changed conditional filter name; tidied up active measures filter query
    
    * refactor of tests for session; refactor of __init__ method, removed comments

---
+++ measures/tests/test_filters.py
@@ -0,0 +1,84 @@
+from datetime import date
+from datetime import timedelta
+
+import pytest
+
+from common.tests import factories
+from common.util import TaricDateRange
+from measures.filters import MeasureFilter
+from measures.models import Measure
+from workbaskets.models import WorkBasket
+
+pytestmark = pytest.mark.django_db
+
+
+def test_filter_by_active_measures():
+    active_measure = factories.MeasureFactory.create(
+        valid_between=TaricDateRange(date.today() + timedelta(days=-100)),
+    )
+    measure_starts_today = factories.MeasureFactory.create(
+        valid_between=TaricDateRange(date.today()),
+    )
+    expired_measure = factories.MeasureFactory.create(
+        valid_between=TaricDateRange(
+            date.today() + timedelta(days=-100),
+            date.today() + timedelta(days=-99),
+        ),
+    )
+    future_measure = factories.MeasureFactory.create(
+        valid_between=TaricDateRange(
+            date.today() + timedelta(days=10),
+            date.today() + timedelta(days=99),
+        ),
+    )
+    self = MeasureFilter(data={"measure_filters_modifier": "active"})
+    qs = Measure.objects.all()
+    result = MeasureFilter.measures_filter(
+        self,
+        queryset=qs,
+        name="measure_filters_modifier",
+        value="active",
+    )
+
+    assert len(result) == 2
+    assert result[0] == qs[0]
+    assert expired_measure and future_measure not in result
+
+
+@pytest.fixture
+def queryset(session_workbasket):
+    with session_workbasket.new_transaction() as transaction:
+        measure_in_workbasket_1 = factories.MeasureFactory.create(
+            transaction=transaction,
+        )
+        measure_in_workbasket_2 = factories.MeasureFactory.create(
+            transaction=transaction,
+        )
+
+    factories.MeasureFactory.create()
+    factories.MeasureFactory.create()
+
+
+def test_filter_by_current_workbasket(
+    valid_user_client,
+    session_workbasket: WorkBasket,
+    session_request,
+):
+    session = valid_user_client.session
+    session["workbasket"] = {"id": session_workbasket.pk}
+    session.save()
+    self = MeasureFilter(
+        data={"measure_filters_modifier": "current", "_mutable": False},
+        request=session_request,
+    )
+    # self.request.session["workbasket"] = session_workbasket
+    qs = Measure.objects.all()
+    # assert 0
+    result = MeasureFilter.measures_filter(
+        self,
+        queryset=qs,
+        name="measure_filters_modifier",
+        value="current",
+    )
+    assert len(result) == len(session_workbasket.measures)
+    assert set(session_workbasket.measures) == set(result)
