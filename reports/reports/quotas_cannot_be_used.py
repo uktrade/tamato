6548916f37fe16557e7f0679f1dc756262cd6c1f
nboyse <natasha.boyse@digital.trade.gov.uk>
2023-08-21 12:18:38 +0100

    commit changes before refactoring for cache

--- reports/reports/quotas_cannot_be_used.py
+++ reports/reports/quotas_cannot_be_used.py
@@ -1,11 +1,12 @@
+import datetime
 from datetime import date
 from datetime import timedelta
 
-from django.db.models import Count
+from django.db.models import Count, Q
 from django.db.models.functions import TruncDate
 from django.core.exceptions import ObjectDoesNotExist
 
-from reports.reports.base_chart import ReportBaseChart
+from reports.reports.base_table import ReportBaseTable
 
 from quotas.models import QuotaOrderNumber, QuotaDefinition, QuotaOrderNumberOriginExclusion
 from measures.models import Measure, MeasureExcludedGeographicalArea
@@ -14,94 +15,166 @@ from collections import defaultdict
 
 
 # Third attempt - Ensure everything is cached.. hopefully speeds it up!
-class Report(ReportBaseChart):
+from collections import defaultdict
+
+class Report(ReportBaseTable):
     name = "Quotas Missing Data"
-    description = "This report shows the quotas that cannot be used by traders due to missing data"
-    report_template = "text"
-    hover_text = "approved"
-    chart_type = "line"
-    days_in_past = 365
+    enabled = True
 
-    def data(self):
-        result = []
+    def headers(self) -> [dict]:
+        return [
+            {"text": "order number"},
+            {"text": "start date"},
+            {"text": "end date"},
+        ]
 
+    def row(self, row: Measure) -> [dict]:
+        return [
+            {"text": row.order_number},
+            {"text": row.valid_between.lower},
+            {"text": row.valid_between.upper},
+        ]
+
+    def rows(self) -> [[dict]]:
+        table_rows = []
         for row in self.query():
-            result.append({"y": row["count"], "x": str(row["date"])})
+            table_rows.append(self.row(row))
 
-        return result
-
-    def labels(self):
-        return []
+        return table_rows
 
     def query(self):
-        approved_quota_sids = self.get_approved_quota_sids()
-
-        quotas_with_definition_periods = self.get_quotas_with_definition_periods(approved_quota_sids)
-
+        quotas_with_definition_periods = self.get_quotas_with_definition_periods()
         quotas_can_be_used = self.find_quotas_that_can_be_used(quotas_with_definition_periods)
-
         return quotas_can_be_used
 
-    def get_approved_quota_sids(self):
-        return QuotaOrderNumber.objects.latest_approved().all()
+    def get_quotas_with_definition_periods(self):
+        quota_definitions = QuotaDefinition.objects.latest_approved().filter(valid_between__isnull=False,
+                                                                             valid_between__startswith__lt=datetime.datetime.now(),
+                                                                             valid_between__endswith__gte=datetime.datetime.now(),
+                                                                             )
 
-    def get_quotas_with_definition_periods(self, approved_quota_sids):
-        quotas_with_periods = []
-        quota_definitions_cache = defaultdict(list)
-
-        for quota_number in approved_quota_sids:
-            quota_definitions = QuotaDefinition.objects.latest_approved().filter(order_number=quota_number)
-            quota_definitions_cache[quota_number].extend(quota_definitions)
-
-        for quota_number, quota_definitions in quota_definitions_cache.items():
-            for quota in quota_definitions:
-                if quota.valid_between:
-                    quotas_with_periods.append(quota)
-
-        return quotas_with_periods
+        return quota_definitions
 
     def find_quotas_that_can_be_used(self, quotas_with_definition_periods):
         quotas_can_be_used = []
 
-        measure_cache = defaultdict(list)
+        # measure_cache = defaultdict(list)
         excluded_geo_area_cache = defaultdict(dict)
+        quotas = [quota.order_number for quota in quotas_with_definition_periods]
 
-        for quota in quotas_with_definition_periods:
-            quotas = QuotaOrderNumber.objects.latest_approved().filter(sid=quota.sid)
+        quota_definitions = []
+        check_quota_measures = []
 
-            for quota_number in quotas:
-                if quota_number in measure_cache:
-                    measures = measure_cache[quota_number]
-                else:
-                    measures = Measure.objects.latest_approved().filter(order_number=quota_number)
-                    measure_cache[quota_number] = measures
+        for quota_number in quotas:
+            if quota_number.valid_between.upper is not None:
+                quotas_can_be_used = (Q(
+                        order_number=quota_number.order_number,
+                        valid_between__startswith__lt=quota_number.valid_between.lower,
+                    ) & (
+                        Q(valid_between__endswith__gte=quota_number.valid_between.upper)
 
-                for measure in measures:
-                    if measure.trackedmodel_ptr_id in excluded_geo_area_cache:
-                        measure_excluded_geo_area = excluded_geo_area_cache[measure.trackedmodel_ptr_id]
-                    else:
-                        try:
-                            measure_excluded_geo_area = MeasureExcludedGeographicalArea.current_objects.get(
-                                trackedmodel_ptr_id=measure.trackedmodel_ptr_id).excluded_geographical_area
-                        except ObjectDoesNotExist:
-                            measure_excluded_geo_area = None
-                        excluded_geo_area_cache[measure.trackedmodel_ptr_id] = measure_excluded_geo_area
+                    ))
 
-                    if quota_number.trackedmodel_ptr_id in excluded_geo_area_cache:
-                        order_number_excluded_geo_area = excluded_geo_area_cache[quota_number.trackedmodel_ptr_id]
-                    else:
-                        try:
-                            order_number_excluded_geo_area = QuotaOrderNumberOriginExclusion.objects.latest_approved().get(
-                                origin_id=quota_number.trackedmodel_ptr_id)
-                        except ObjectDoesNotExist:
-                            order_number_excluded_geo_area = None
-                        excluded_geo_area_cache[quota_number.trackedmodel_ptr_id] = order_number_excluded_geo_area
+                if Measure.objects.latest_approved().filter(quotas_can_be_used):
+                    check_quota_measures.append(quota_number)
 
-                    if measure.geographical_area == quota_number.origins and \
-                            measure_excluded_geo_area == order_number_excluded_geo_area:
-                        quotas_can_be_used.append(quota_number.sid)
+            else:
+                quotas_cant_be_used = (
+                    Q(
+                        order_number=quota_number.order_number,
+                        valid_between__startswith__lt=quota_number.valid_between.lower,
+                    )
+                )
+                if Measure.objects.latest_approved().filter(quotas_cant_be_used):
+                    if quota_number not in quota_definitions:
+                        quota_definitions.append(quota_number)
 
-        return quotas_can_be_used
+            measure_queryset = Measure.objects.latest_approved().filter(order_number=quota_number).select_related(
+                'transaction', 'order_number')
+
+            for measure in measure_queryset.iterator():
+                transaction = measure.transaction
+                exclusions = measure.exclusions.approved_up_to_transaction(transaction)
+
+                valid_between_upper = measure.valid_between.upper
+                if valid_between_upper is not None:
+                    member_exists_query = measure.geographical_area.members.filter(
+                        Q(
+                            member__area_id__in=[exclusion.excluded_geographical_area.area_id for exclusion in
+                                                 exclusions],
+                            valid_between__startswith__lte=measure.valid_between.lower
+                        )
+                        &
+                        (
+                                Q(valid_between__endswith__gte=valid_between_upper) |
+                                Q(valid_between__endswith=None)
+                        )
+                    )
+
+                    if member_exists_query.exists():
+                        if quota_number not in quota_definitions:
+                            quota_definitions.append(quota_number)
+
+        return quota_definitions
+
+
+        # quota_definitions = set()
+        # cached_measures = {}  # Store cached measures by quota number
+        #
+        # for quota in quotas_with_definition_periods:
+        #     if quota.valid_between.upper is not None:
+        #         quotas_can_be_used = (
+        #                 Q(
+        #                     order_number=quota.order_number,
+        #                     valid_between__startswith__lt=quota.valid_between.lower,
+        #                 ) &
+        #                 (
+        #                     Q(valid_between__endswith__gte=quota.valid_between.upper)
+        #                 )
+        #         )
+        #     else:
+        #         quotas_cant_be_used = (
+        #             Q(
+        #                 order_number=quota.order_number,
+        #                 valid_between__startswith__lt=quota.valid_between.lower,
+        #             )
+        #         )
+        #
+        #     # Check if cached measure exists for this quota, otherwise query the database
+        #     if quota.order_number in cached_measures:
+        #         measure_queryset = cached_measures[quota.order_number]
+        #     else:
+        #         measure_queryset = Measure.objects.latest_approved().filter(
+        #             order_number=quota.order_number).select_related(
+        #             'transaction', 'order_number')
+        #         cached_measures[quota.order_number] = measure_queryset
+        #
+        #     if Measure.objects.latest_approved().filter(quotas_can_be_used).count() == 0:
+        #         quota_definitions.add(quota.order_number)
+        #
+        #     for measure in measure_queryset.iterator():
+        #         transaction = measure.transaction
+        #         exclusions = measure.exclusions.approved_up_to_transaction(transaction)
+        #
+        #         valid_between_upper = measure.valid_between.upper
+        #         if valid_between_upper is not None:
+        #             member_exists_query = measure.geographical_area.members.filter(
+        #                 Q(
+        #                     member__area_id__in=[exclusion.excluded_geographical_area.area_id for exclusion in
+        #                                          exclusions],
+        #                     valid_between__startswith__lte=measure.valid_between.lower
+        #                 )
+        #                 &
+        #                 (
+        #                         Q(valid_between__endswith__gte=valid_between_upper) |
+        #                         Q(valid_between__endswith=None)
+        #                 )
+        #             )
+        #
+        #             if member_exists_query.exists():
+        #                 quota_definitions.add(quota.order_number)
+        #
+        # return list(quota_definitions)
 
 # First attempt.. generally terrible code
 # class Report(ReportBaseChart):
