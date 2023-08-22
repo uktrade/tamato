e8c29c4c8f74ca906a8611e6c370f4634ae112ea
nboyse <natasha.boyse@digital.trade.gov.uk>
2023-08-22 10:39:20 +0100

    refactored to load faster

--- reports/reports/quotas_cannot_be_used.py
+++ reports/reports/quotas_cannot_be_used.py
@@ -1,21 +1,10 @@
 import datetime
-from datetime import date
-from datetime import timedelta
-
-from django.db.models import Count, Q
-from django.db.models.functions import TruncDate
-from django.core.exceptions import ObjectDoesNotExist
 
 from reports.reports.base_table import ReportBaseTable
 
-from quotas.models import QuotaOrderNumber, QuotaDefinition, QuotaOrderNumberOriginExclusion
-from measures.models import Measure, MeasureExcludedGeographicalArea
+from quotas.models import QuotaOrderNumber, QuotaDefinition
+from measures.models import Measure
 
-from collections import defaultdict
-
-
-# Third attempt - Ensure everything is cached.. hopefully speeds it up!
-from collections import defaultdict
 
 class Report(ReportBaseTable):
     name = "Quotas Missing Data"
@@ -49,8 +38,7 @@ class Report(ReportBaseTable):
         return quotas_can_be_used
 
     def get_quotas_with_definition_periods(self):
-        quota_definitions = QuotaDefinition.objects.latest_approved().select_related("order_number",
-                                                                                     ).filter(
+        quota_definitions = QuotaDefinition.objects.latest_approved().filter(
             valid_between__isnull=False,
             valid_between__startswith__lt=datetime.datetime.now(),
             valid_between__endswith__gte=datetime.datetime.now(),
@@ -58,10 +46,49 @@ class Report(ReportBaseTable):
 
         return quota_definitions
 
+    # def find_quotas_that_can_be_used(self, quotas_with_definition_periods):
+    #     # Quota definition periods that have measures in place which match
+    #     matching_exclusion_data = []
+    #     matching_data = []
+    #     for quota in quotas_with_definition_periods:
+    #         measures = Measure.objects.latest_approved().filter(order_number=quota.order_number)
+    #         for measure in measures:
+    #             transaction = measure.transaction
+    #             exclusions = measure.exclusions.approved_up_to_transaction(transaction)
+    #
+    #             for exclusion in exclusions:
+    #                 geo_area = exclusion.excluded_geographical_area
+    #                 quota_geographical_exclusions_data = QuotaOrderNumber.objects.latest_approved().filter(
+    #                     order_number=quota.order_number)
+    #
+    #                 for quota_exclusions_query in quota_geographical_exclusions_data:
+    #                     for geographical_exclusions in quota_exclusions_query.geographical_exclusions:
+    #                         if geographical_exclusions == geo_area and quota.order_number not in matching_exclusion_data:
+    #                             matching_exclusion_data.append(quota)
+    #
+    #         if quota not in matching_data:
+    #             quota.reason = 'Geographical area/exclusions data does not have any measures with matching data'
+    #             matching_data.append(quota)
+    #
+    #     quota_order_numbers_without_definitions = QuotaOrderNumber.objects.latest_approved().filter(
+    #         valid_between__isnull=False,
+    #         valid_between__startswith__lt=datetime.datetime.now(),
+    #         valid_between__endswith__gte=datetime.datetime.now()).exclude(
+    #         definitions__in=quotas_with_definition_periods
+    #     )
+    #
+    #     for quota in quota_order_numbers_without_definitions:
+    #         if quota not in matching_data:
+    #             quota.reason = 'Definition period has not been set'
+    #             matching_data.append(quota)
+    #
+    #     return matching_data
+
     def find_quotas_that_can_be_used(self, quotas_with_definition_periods):
-        # Quota definition periods that have measures in place which match
-        matching_exclusion_data = []
-        matching_data = []
+        matching_exclusion_data = set()
+        matching_data = set()
+        excluded_geo_areas = set()
+
         for quota in quotas_with_definition_periods:
             measures = Measure.objects.latest_approved().filter(order_number=quota.order_number)
             for measure in measures:
@@ -70,28 +97,28 @@ class Report(ReportBaseTable):
 
                 for exclusion in exclusions:
                     geo_area = exclusion.excluded_geographical_area
-                    quota_geographical_exclusions_data = QuotaOrderNumber.objects.latest_approved().filter(
-                        order_number=quota.order_number)
+                    excluded_geo_areas.add(geo_area)
 
-                    for quota_exclusions_query in quota_geographical_exclusions_data:
-                        for geographical_exclusions in quota_exclusions_query.geographical_exclusions:
-                            if geographical_exclusions == geo_area and quota.order_number not in matching_exclusion_data:
-                                matching_exclusion_data.append(quota)
+            if not excluded_geo_areas:
+                matching_exclusion_data.add(quota)
 
-            if quota not in matching_data:
-                quota.reason = 'Geographical area/exclusions data does not have any measures with matching data'
-                matching_data.append(quota)
-
+        current_time = datetime.datetime.now()
         quota_order_numbers_without_definitions = QuotaOrderNumber.objects.latest_approved().filter(
             valid_between__isnull=False,
-            valid_between__startswith__lt=datetime.datetime.now(),
-            valid_between__endswith__gte=datetime.datetime.now()).exclude(
-            definitions__in=quotas_with_definition_periods
-        )
+            valid_between__startswith__lt=current_time,
+            valid_between__endswith__gte=current_time
+        ).exclude(definitions__in=quotas_with_definition_periods)
 
         for quota in quota_order_numbers_without_definitions:
-            if quota not in matching_data:
-                quota.reason = 'Definition period has not been set'
-                matching_data.append(quota)
+            matching_data.add(quota)
 
-        return matching_data
+        for quota in matching_exclusion_data:
+            matching_data.add(quota)
+
+        for quota in matching_data:
+            if quota in matching_exclusion_data:
+                quota.reason = 'Geographical area/exclusions data does not have any measures with matching data'
+            else:
+                quota.reason = 'Definition period has not been set'
+
+        return list(matching_data)
