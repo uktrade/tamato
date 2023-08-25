fc7b0a46836c6a656b8ddfb93d681ca417229c77
Doug Mills <doug.mills@digital.trade.gov.uk>
2023-08-25 08:48:21 +0100

    update after comments

--- measures/business_rules.py
+++ measures/business_rules.py
@@ -1338,12 +1338,9 @@ class ME70(BusinessRule):
 
 
 class ME71(FootnoteApplicability):
-    """
-    Footnotes with a footnote type for which the application type = "CN
-    footnotes" cannot be associated with TARIC codes (codes with pos.
-
-    9-10 different from 00).
-    """
+    """Footnotes with a footnote type for which the application type = "CN
+    footnotes" cannot be associated with TARIC codes (codes with pos 9-10
+    different from 00)."""
 
     applicable_field = "footnoted_measure"
 
