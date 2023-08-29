33baf195d191cd28d1a45b508eb55dba4655c874
Charles Prichard <charles.prichard@digital.trade.gov.uk>
2023-08-29 08:58:50 +0100

    hooks

--- common/static/common/js/conditionalDisablingFilters.js
+++ common/static/common/js/conditionalDisablingFilters.js
@@ -5,20 +5,22 @@ const initFilterDisabledToggleForComCode = () => {
     document.addEventListener('DOMContentLoaded', function() {
         let specificCommodityCode = document.getElementById('goods_nomenclature_autocomplete');
         let includeInheritedMeasures = document.getElementById('id_modc');
-
-        if (specificCommodityCode.value) {
-            includeInheritedMeasures.disabled = false;
-        } else {
-            includeInheritedMeasures.disabled = true;
-        }
-
-        specificCommodityCode.addEventListener('change', function() {
-            if (this.value) {
+        
+        if(specificCommodityCode && includeInheritedMeasures){
+            if (specificCommodityCode.value) {
                 includeInheritedMeasures.disabled = false;
             } else {
                 includeInheritedMeasures.disabled = true;
             }
-        })
+            
+            specificCommodityCode.addEventListener('change', function() {
+                if (this.value) {
+                    includeInheritedMeasures.disabled = false;
+                } else {
+                    includeInheritedMeasures.disabled = true;
+                }
+            })
+        }
     })
 }
 
