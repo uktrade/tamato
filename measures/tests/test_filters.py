1a570baa51d0fbefc404066f19c7c100a7cc1eba
Charles Prichard <charles.prichard@digital.trade.gov.uk>
2023-08-30 09:13:30 +0100

    refactor tests to remove fixture as not used

--- measures/tests/test_filters.py
+++ measures/tests/test_filters.py
@@ -8,8 +8,11 @@ from workbaskets.models import WorkBasket
 pytestmark = pytest.mark.django_db
 
 
-@pytest.fixture
-def queryset(session_workbasket):
+def test_filter_by_current_workbasket(
+    valid_user_client,
+    session_workbasket: WorkBasket,
+    session_request,
+):
     with session_workbasket.new_transaction() as transaction:
         measure_in_workbasket_1 = factories.MeasureFactory.create(
             transaction=transaction,
@@ -20,13 +23,6 @@ def queryset(session_workbasket):
 
     factories.MeasureFactory.create()
     factories.MeasureFactory.create()
-
-
-def test_filter_by_current_workbasket(
-    valid_user_client,
-    session_workbasket: WorkBasket,
-    session_request,
-):
     session = valid_user_client.session
     session["workbasket"] = {"id": session_workbasket.pk}
     session.save()
