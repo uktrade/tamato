27bc3010f67b0cbc40092317652b5a3ce181267f
Charles Prichard <charles.prichard@digital.trade.gov.uk>
2023-08-30 17:55:10 +0100

    removed leftovers, fixed breaking test

--- commodities/tests/test_filters.py
+++ commodities/tests/test_filters.py
@@ -29,30 +29,3 @@ def test_commodity_filter_footnotes(rf):
     assert commodity1 in filtered
     assert commodity2 in filtered
     assert commodity3 not in filtered
-
-
-# @pytest.fixture
-# def queryset(session_workbasket):
-#     with session_workbasket.new_transaction() as transaction:
-#         commodity_in_workbasket_1 = factories.GoodsNomenclatureFactory.create(transaction=transaction)
-#         commodity_in_workbasket_2 = factories.GoodsNomenclatureFactory.create(transaction=transaction)
-
-#     commodity_not_in_workbasket_1 = factories.GoodsNomenclatureFactory.create(transaction=transaction)
-#     commodity_not_in_workbasket_2 = factories.GoodsNomenclatureFactory.create(transaction=transaction)
-
-# def test_current_workbasket_mixin_commodities(valid_user_client, session_workbasket, session_request, rf):
-#     session = valid_user_client.session
-#     session["workbasket"] = {"id": session_workbasket.pk}
-#     session.save()
-
-#     self = CommodityFilter(data={"current_work_basket": "current"})
-
-#     qs = GoodsNomenclature.objects.all()
-
-#     result = CommodityFilter(
-#         self,
-#         queryset=qs,
-#         request=rf
-#     )
-#     print(f'{result=}')
-#     assert 0
