c9f03afee6b6af38219722bde49afbe5df3cd0cb
nboyse <natasha.boyse@digital.trade.gov.uk>
2023-09-13 11:24:11 +0100

    black

--- reports/migrations/0001_report.py
+++ reports/migrations/0001_report.py
@@ -6,8 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
