8f62f8ec1200c5d06316b4b2bb9d2ecd8a43b308
Charles Prichard <charles.prichard@digital.trade.gov.uk>
2023-09-15 16:06:40 +0100

    applyinng ticket changes

---
+++ common/static/common/js/openCloseAccordion.js
@@ -0,0 +1,18 @@
+// TODO: adjust the pathname filter to account for all objects search/filter pages
+const initOpenCloseAccordionSection = () => {
+    document.addEventListener('DOMContentLoaded', function() {
+
+        let expandedSection = document.getElementsByClassName('govuk-accordion__section')
+        let pathname = window.location.pathname
+        if(expandedSection.length > 0){
+            if (pathname.includes('measures/search')){
+                expandedSection[0].classList.add('govuk-accordion__section--expanded');
+            } else if (pathname.includes('measures') && !pathname.includes('search')){
+                expandedSection[0].classList.remove('govuk-accordion__section--expanded');
+            }
+        }
+    }
+    )
+}
+
+export default initOpenCloseAccordionSection;
