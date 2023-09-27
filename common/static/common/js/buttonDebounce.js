05657ff74edb70de3836202f90e0a7db40349ee3
Charles Prichard <charles.prichard@digital.trade.gov.uk>
2023-09-27 12:07:56 +0100

    changed alert to infotext

--- common/static/common/js/buttonDebounce.js
+++ common/static/common/js/buttonDebounce.js
@@ -1,12 +1,12 @@
 let button = document.getElementById('add-debounce')
+let infoText = document.getElementById('add-debounce-inset-text')
 
 const debounceButton = () => {
     if (button) {
-        console.log('button', button)
         addEventListener('submit', function() {
             button.disabled = true,
-            alert("Your action is being processed and the submit button is now disabled. You will be taken to a confirmation screen once the action has been processed.")}
-        )
+            infoText.classList.remove('js-hidden')
+        })
     }
 }
 
