8755e42362fb2ebb9db9ad096797ddba975e49a5
nboyse <natasha.boyse@digital.trade.gov.uk>
2023-10-11 17:21:05 +0100

    Add correct path to allow quota order numbers to be edited

--- quotas/urls.py
+++ quotas/urls.py
@@ -60,6 +60,11 @@ urlpatterns = [
         name="quota_order_number_origin-ui-edit",
     ),
     path(
+        f"quotas/<sid>/edit-create/",
+        views.QuotaEditUpdate.as_view(),
+        name="quota-ui-edit-create",
+    ),
+    path(
         f"quotas/<sid>/quota_order_number_origins/",
         views.QuotaOrderNumberOriginCreate.as_view(),
         name="quota_order_number_origin-ui-create",
