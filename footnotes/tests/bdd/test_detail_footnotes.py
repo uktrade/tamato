d3103fa18288cdf049752c73252e39830873df37
Matthew McKenzie <97194636+mattjamc@users.noreply.github.com>
2023-10-31 16:27:57 +0000

    TP2000-1023 Measures tab for footnotes (#1068)
    * Setting up fake tabs for footnotes detail
    
    * Add measures tab for footnotes
    
    * Add view tests
    
    * Simplified view queryset and added geo_area columns for consistency
    
    ---------
    
    Co-authored-by: Edie Pearce <edie.pearce@digital.trade.gov.uk>

--- footnotes/tests/bdd/test_detail_footnotes.py
+++ footnotes/tests/bdd/test_detail_footnotes.py
@@ -1,5 +1,6 @@
 """Tests for view footnotes behaviours."""
 import pytest
+from django.urls import reverse
 from pytest_bdd import scenarios
 from pytest_bdd import then
 from pytest_bdd import when
@@ -8,7 +9,6 @@ from common.models.utils import override_current_transaction
 
 pytestmark = pytest.mark.django_db
 
-
 scenarios("features/detail-footnotes.feature")
 
 
@@ -16,6 +16,19 @@ scenarios("features/detail-footnotes.feature")
 @when("I select footnote NC000")
 def footnote_detail(client, footnote_NC000):
     return client.get(footnote_NC000.get_url())
+
+
+@pytest.fixture
+@when("I select footnote NC000")
+def footnote_descriptions(client, footnote_NC000):
+    url = reverse(
+        "footnote-ui-detail-descriptions",
+        kwargs={
+            "footnote_type__footnote_type_id": footnote_NC000.footnote_type.footnote_type_id,
+            "footnote_id": footnote_NC000.footnote_id,
+        },
+    )
+    return client.get(url)
 
 
 @then("a summary of the core information should be presented")
@@ -32,8 +45,8 @@ def footnote_core_data(footnote_detail, footnote_NC000):
 
 
 @then("the descriptions against the footnote should be presented")
-def footnote_description_data(footnote_detail, footnote_NC000):
-    content = footnote_detail.content.decode()
+def footnote_description_data(footnote_descriptions, footnote_NC000):
+    content = footnote_descriptions.content.decode()
     f = footnote_NC000
 
     for description in f.descriptions.all():
