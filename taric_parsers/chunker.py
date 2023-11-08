ffae380d046cf77200e457ff65249ea110b41aa8
Doug Mills <doug.mills@digital.trade.gov.uk>
2023-11-08 10:05:02 +0000

    minor updates based on PR comments

--- taric_parsers/chunker.py
+++ taric_parsers/chunker.py
@@ -2,15 +2,14 @@ import os
 import xml.etree.ElementTree as ET
 from logging import getLogger
 from tempfile import TemporaryFile
-from typing import List
 from typing import Optional
 from typing import Sequence
 
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.template.loader import render_to_string
 
-from common.util import xml_fromstring
 from importer import models
+from importer.models import BatchImportError
 from importer.namespaces import make_schema_dataclass
 from importer.namespaces import nsmap
 from importer.namespaces import xsd_schema_paths
@@ -22,7 +21,7 @@ Tags = make_schema_dataclass(xsd_schema_paths)
 logger = getLogger(__name__)
 
 
-def get_chunk(
+def find_or_create_chunk(
     chunks_in_progress: dict,
     envelope_id: str,
     record_code=None,
@@ -93,54 +92,6 @@ def close_chunk(chunk: TemporaryFile, batch: models.ImportBatch, key):
     )
 
 
-def sort_commodity_codes(transactions: List[ET.Element]) -> List[ET.Element]:
-    """
-    Sort the commodity code transactions by item ID, indent, suffix and
-    transaction ID (which represents the order they were given in).
-
-    Commodity codes in seed files have historically arrived unsorted. As the legacy system has
-    no active representation of the commodity hierarchy (instead opting to use views) this works
-    for the legacy system. However the new system has an in situ representation of the hierarchy.
-    This representation requires the commodities to be entered in the right order to be built
-    properly.
-
-    The initial obvious way to sort things is via item ID (as 0100000000 would always come before
-    0101000000 this way). However there are cases where multiple commodities have the same item ID.
-    In this case they need to also be sorted on indent (as 00 would come before 01). Furthermore there
-    are commodities with the same item ID and indent. In this case the commodities must also be sorted
-    on suffix, as 80 means an endline product and therefore must come last always - all other suffixes
-    are lesser than 80.
-
-    Lastly if there is still a conflict commodities are ordered on transaction order.
-
-    These four factors in that order produce an accurate hierarchical representation.
-    """
-
-    def comm_code_key(transaction):
-        """Sort Commodity codes by item id, indent, suffix and transaction
-        id."""
-        item_ids = transaction.findall("*/*/*/*/ns2:goods.nomenclature.item.id", nsmap)
-        indents = transaction.findall("*/*/*/*/ns2:number.indents", nsmap)
-        suffixes = transaction.findall("*/*/*/*/ns2:producline.suffix", nsmap)
-
-        # Find the item ID, indent and suffix. If one of these isn't found then it is
-        # replaced with the largest possible number of equivalent size so that it is
-        # sorted to the end.
-        item_id = min(item.text for item in item_ids) if item_ids else "999999999999"
-        indent = min(indent_obj.text for indent_obj in indents) if indents else "99"
-
-        suffix = (
-            min("00" if suffix.text != "80" else "80" for suffix in suffixes)
-            if suffixes
-            else "99"
-        )
-
-        return item_id, indent, suffix, transaction.items()[0][1]
-
-    transactions.sort(key=comm_code_key)
-    return transactions
-
-
 def sort_comm_code_messages(message):
     """
     Sort the messages within a commodity code transaction.
@@ -159,41 +110,6 @@ def sort_comm_code_messages(message):
     # a relationship to indents).
     indent = indent.text if indent is not None else "00"
     return code, indent
-
-
-def rewrite_comm_codes(batch: models.ImportBatch, envelope_id: str, record_code="400"):
-    """
-    Take the given commodity code data and rewrite it in the correct order
-    required by the hierarchical tree.
-
-    Commodity codes in seed files often are given out of order which breaks the
-    hierarchical tree representing them. This function takes all transactions
-    with the relevant record_code (400), sorts them and rewrites them in the
-    expected order.
-
-    N.B. This happens entirely within memory.
-    """
-    chunks = batch.chunks.filter(record_code=record_code)
-
-    transactions = []
-
-    for chunk in chunks:
-        transactions.extend(xml_fromstring(chunk.chunk_text))
-
-    logger.info("%d to sort, deleting old ones", len(transactions))
-
-    chunks.delete()
-
-    transactions = sort_commodity_codes(transactions)
-
-    chunks_in_progress = {}
-
-    for transaction in transactions:
-        transaction[:] = sorted(transaction, key=sort_comm_code_messages)
-        write_transaction_to_chunk(transaction, chunks_in_progress, batch, envelope_id)
-
-    for key, chunk in chunks_in_progress.items():
-        close_chunk(chunk, batch, key)
 
 
 def get_record_code(transaction: ET.Element) -> str:
@@ -244,7 +160,7 @@ def write_transaction_to_chunk(
     else:
         record_code = None
 
-    chunk = get_chunk(
+    chunk = find_or_create_chunk(
         chunks_in_progress,
         envelope_id,
         record_code=record_code,
@@ -373,6 +289,18 @@ def chunk_taric(
         close_chunk(chunk, batch, key)
 
     if batch.split_job:
-        rewrite_comm_codes(batch, envelope_id)
+        BatchImportError.objects.create(
+            batch=batch,
+            description="This Import has been flagged as a split job. Please review file size. Maximum import size is 50mb, "
+            "anything larger than this should be split before attempting import. This importer does not support "
+            "split jobs",
+            taric_change_type="",
+            issue_type="ERROR",
+            transaction_id="",
+            object_details="",
+            related_object_identity_keys="",
+            object_type="",
+        )
+        raise Exception("Unexpected split job, could be ")
 
     return chunk_count
