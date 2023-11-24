8d05095298d8d0c178fc18c50bdf7ce513f305c5
Dale Cannon <dale.cannon@digital.trade.gov.uk>
2023-11-24 13:08:39 +0000

    Refactor test_get_branch_measures

--- measures/tests/test_measure_snapshot.py
+++ measures/tests/test_measure_snapshot.py
@@ -41,19 +41,14 @@ def test_init(seed_database_with_indented_goods):
 
 
 def test_get_branch_measures(seed_database_with_indented_goods):
-    goods = GoodsNomenclature.objects.all().get(item_id="2903691900")
-    commodities_collection = CommodityCollectionLoader(prefix="2903").load()
+    goods = GoodsNomenclature.objects.get(item_id="2903691900")
+    commodities_collection = CommodityCollectionLoader(prefix="2903").load().commodities
 
     target_commodity = None
-
-    for commodity in commodities_collection.commodities:
-        if commodity.item_id == "2903691900":
+    for commodity in commodities_collection:
+        if commodity.item_id == goods.item_id:
             target_commodity = commodity
-
-    archived_transaction = factories.TransactionFactory.create(archived=True)
-    old_regulation = factories.RegulationFactory.create(
-        valid_between=TaricDateRange(date(1982, 1, 1), date(1982, 12, 31)),
-    )
+    assert target_commodity
 
     draft_transaction = factories.TransactionFactory.create(draft=True)
     draft_measure = factories.MeasureFactory.create(
@@ -61,25 +56,16 @@ def test_get_branch_measures(seed_database_with_indented_goods):
         valid_between=TaricDateRange(date.today() + timedelta(days=-100)),
     )
 
-    # call get_dependent_measures and check archived measure does not exist in results
-
     sn_date = date.today()
     moment = SnapshotMoment(draft_transaction, sn_date)
 
-    # used the loader to simplify the commodities list generation
-    ccl = CommodityCollectionLoader(prefix="2903")
-    commodities = ccl.load().commodities
-    edges = {}
-    ancestors = {}
-    descendants = {}
-    tree = CommodityTreeSnapshot(commodities, moment, edges, ancestors, descendants)
+    tree = CommodityTreeSnapshot(
+        commodities_collection,
+        moment,
+        edges={},
+        ancestors={},
+        descendants={},
+    )
 
     target = MeasureSnapshot(moment, tree)
-
     assert target.get_branch_measures(target_commodity).count() == 1
-    assert len(target.tree.descendants) == 4
-    assert len(target.tree.edges) == 6
-    assert len(target.tree.commodities) == 6
-    assert len(target.tree.ancestors) == 6
-
-    assert target.extent == TaricDateRange(date.today())
