2ec2899c1fb6ab179ca400196e503582fe330bd4
Paul Pepper <85895113+paulpepper-trade@users.noreply.github.com>
2023-12-19 19:03:33 +0000

    TP2000-1142 Synchronous only operation errors (#1114)

--- common/tests/test_tariffs_api.py
+++ common/tests/test_tariffs_api.py
@@ -1,10 +1,14 @@
+from unittest.mock import patch
+
 import pytest
+import requests
+import requests_mock
 
 from common.tariffs_api import Endpoints
-from common.tariffs_api import async_get_all
 from common.tariffs_api import build_quota_definition_urls
+from common.tariffs_api import deserialize_quota_data
 from common.tariffs_api import get_quota_data
-from common.tariffs_api import serialize_quota_data
+from common.tariffs_api import threaded_get_from_endpoint
 from common.tests import factories
 
 pytestmark = pytest.mark.django_db
@@ -24,48 +28,78 @@ def test_get_quota_data_error(quota_order_number, requests_mock):
     assert data is None
 
 
-async def test_get_quota_data_ok(quota_order_number, requests_mock, quotas_json):
+def test_get_quota_data_ok(quota_order_number, requests_mock, quotas_json):
     requests_mock.get(url=Endpoints.QUOTAS.value, json=quotas_json, status_code=200)
     data = get_quota_data({"order_number": quota_order_number.id})
     assert data == quotas_json
 
 
-@pytest.mark.asyncio
-async def test_async_get_all(
-    mock_aioresponse,
-    quota_order_number,
-    quota_definitions,
+@pytest.mark.parametrize(
+    "has_json_payload, response_status",
+    [
+        (True, 200),
+        (False, 200),
+        (False, 404),
+    ],
+)
+@patch("common.tariffs_api.threaded_get_request_session")
+def test_threaded_get_from_endpoint(
+    get_thread_request_session_mock,
     quotas_json,
+    has_json_payload,
+    response_status,
 ):
-    urls = build_quota_definition_urls(
-        quota_order_number.order_number,
-        quota_definitions,
-    )
-    for url in urls:
-        mock_aioresponse.get(url, status=200, payload=quotas_json)
-    data = await async_get_all(urls)
-    assert data
-    for d in data:
-        assert d == quotas_json
+    """Test that threaded_get_from_endpoint correctly handles HTTP responses and
+    JSON payloads."""
+
+    url = Endpoints.QUOTAS.value
+
+    # Create a requests.Session instance and associate it with our
+    # requests_mock.Mocker instance, allowing requests mock to associate it
+    # as part of its transport replacement when handling requests API calls.
+    session = requests.Session()
+
+    with requests_mock.Mocker(session=session) as requests_mocker:
+        get_thread_request_session_mock.return_value = session
+        requests_mocker.get(
+            url=url,
+            json=quotas_json if has_json_payload else None,
+            status_code=response_status,
+        )
+
+        returned_json = threaded_get_from_endpoint(url)
+        expected_json = quotas_json if has_json_payload else None
+
+        assert returned_json == expected_json
 
 
-@pytest.mark.asyncio
-async def test_async_get_all_failure(
-    mock_aioresponse,
-    quota_order_number,
-    quota_definitions,
-    quotas_json,
+@patch("common.tariffs_api.threaded_get_request_session")
+def test_threaded_get_from_endpoint_network_error(
+    get_thread_request_session_mock,
 ):
-    urls = build_quota_definition_urls(
-        quota_order_number.order_number,
-        quota_definitions,
-    )
-    for url in urls:
-        mock_aioresponse.get(url, status=400, payload=quotas_json)
-    data = await async_get_all(urls)
-    assert data
-    for d in data:
-        assert d == None
+    """Test that threaded_get_from_endpoint correctly handles a network
+    error."""
+
+    url = Endpoints.QUOTAS.value
+
+    # Create a requests.Session instance and associate it with our
+    # requests_mock.Mocker instance, allowing requests mock to associate it
+    # as part of its transport replacement when handling requests API calls.
+    session = requests.Session()
+
+    # Create an adapter, register url with an associated connection error status
+    # against it and mount it on the session.
+    adapter = requests_mock.Adapter()
+    adapter.register_uri("GET", url, exc=requests.exceptions.ConnectTimeout)
+    session.mount(url, adapter=adapter)
+
+    with requests_mock.Mocker(session=session) as requests_mocker:
+        get_thread_request_session_mock.return_value = session
+        requests_mocker.get(url=url)
+
+        returned_json = threaded_get_from_endpoint(url)
+
+        assert returned_json == None
 
 
 def test_build_quota_definition_urls(quota_order_number, quota_definitions):
@@ -193,12 +227,12 @@ def test_serialize_quota_data():
         },
         None,
     ]
-    serialized = serialize_quota_data(data)
-    assert len(serialized) == 3
-    assert not set(serialized.keys()).difference({"1111", "2222", "3333"})
-    assert serialized["1111"]["status"] == "Open"
-    assert serialized["1111"]["balance"] == "80601000.0"
-    assert serialized["2222"]["status"] == "Open"
-    assert serialized["2222"]["balance"] == "23401000.0"
-    assert serialized["3333"]["status"] == "Open"
-    assert serialized["3333"]["balance"] == "78849000.0"
+    deserialized = deserialize_quota_data(data)
+    assert len(deserialized) == 3
+    assert not set(deserialized.keys()).difference({"1111", "2222", "3333"})
+    assert deserialized["1111"]["status"] == "Open"
+    assert deserialized["1111"]["balance"] == "80601000.0"
+    assert deserialized["2222"]["status"] == "Open"
+    assert deserialized["2222"]["balance"] == "23401000.0"
+    assert deserialized["3333"]["status"] == "Open"
+    assert deserialized["3333"]["balance"] == "78849000.0"
