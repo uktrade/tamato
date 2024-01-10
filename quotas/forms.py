ef22a5f664718f3b5604f65a91d165a3f051f9d4
nboyse <natasha.boyse@digital.trade.gov.uk>
2024-01-10 15:04:45 +0000

    black

--- quotas/forms.py
+++ quotas/forms.py
@@ -604,7 +604,7 @@ class QuotaDefinitionCreateForm(
                     "Description",
                     "description",
                     "order_number",
-                    summary="Adding a description is optional."
+                    summary="Adding a description is optional.",
                 ),
                 AccordionSection(
                     "Validity period",
@@ -615,14 +615,16 @@ class QuotaDefinitionCreateForm(
                     "Measurements",
                     Field("measurement_unit", css_class="govuk-!-width-full"),
                     Field("measurement_unit_qualifier", css_class="govuk-!-width-full"),
-                    summary="A measurement unit qualifier is not always required."
+                    summary="A measurement unit qualifier is not always required.",
                 ),
                 AccordionSection(
                     "Volume",
                     "initial_volume",
                     "volume",
                     "maximum_precision",
-                    summary=mark_safe("The initial volume is the legal balance applied to the definition period.<br><br>The current volume is the starting balance for the quota.")
+                    summary=mark_safe(
+                        "The initial volume is the legal balance applied to the definition period.<br><br>The current volume is the starting balance for the quota."
+                    ),
                 ),
                 AccordionSection(
                     "Criticality",
