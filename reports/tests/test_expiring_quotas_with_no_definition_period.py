98be663d1606ee2555fa6687343826050c013916
nboyse <natasha.boyse@digital.trade.gov.uk>
2024-01-11 17:10:29 +0000

    linting

--- reports/tests/test_expiring_quotas_with_no_definition_period.py
+++ reports/tests/test_expiring_quotas_with_no_definition_period.py
@@ -7,9 +7,11 @@ from common.util import TaricDateRange
 from reports.reports.expiring_quotas_with_no_definition_period import Report
 from quotas.models import QuotaDefinition
 
+
 @pytest.fixture
 def quota_order_number(db):
     return factories.QuotaOrderNumberFactory.create()
+
 
 @pytest.fixture
 def expired_quota_definition(quota_order_number):
@@ -17,9 +19,10 @@ def expired_quota_definition(quota_order_number):
         order_number=quota_order_number,
         valid_between=TaricDateRange(
             datetime.datetime.today().date() + relativedelta(weeks=-2),
-            datetime.datetime.today().date() + relativedelta(weeks=-1)
+            datetime.datetime.today().date() + relativedelta(weeks=-1),
         ),
     )
+
 
 @pytest.fixture
 def expiring_soon_quota_definition(quota_order_number):
@@ -27,9 +30,10 @@ def expiring_soon_quota_definition(quota_order_number):
         order_number=quota_order_number,
         valid_between=TaricDateRange(
             datetime.datetime.today().date() + relativedelta(weeks=1),
-            datetime.datetime.today().date() + relativedelta(weeks=2)
+            datetime.datetime.today().date() + relativedelta(weeks=2),
         ),
     )
+
 
 @pytest.fixture
 def future_quota_definition(quota_order_number):
@@ -37,16 +41,19 @@ def future_quota_definition(quota_order_number):
         order_number=quota_order_number,
         valid_between=TaricDateRange(
             datetime.datetime.today().date() + relativedelta(weeks=3),
-            datetime.datetime.today().date() + relativedelta(weeks=4)
+            datetime.datetime.today().date() + relativedelta(weeks=4),
         ),
     )
 
+
 @pytest.mark.django_db
 class TestQuotasExpiringSoonReport:
-
     def test_quotas_expiring_soon_report_logic(
-        self, quota_order_number,
-        expired_quota_definition, expiring_soon_quota_definition, future_quota_definition
+        self,
+        quota_order_number,
+        expired_quota_definition,
+        expiring_soon_quota_definition,
+        future_quota_definition,
     ):
         report = Report()
         quotas = report.query()
@@ -59,7 +66,9 @@ class TestQuotasExpiringSoonReport:
 
         assert expired_quota_definition not in quotas
 
-    def test_quotas_expiring_soon_report_row(self, quota_order_number, expiring_soon_quota_definition):
+    def test_quotas_expiring_soon_report_row(
+        self, quota_order_number, expiring_soon_quota_definition
+    ):
         report = Report()
         row_data = report.row(expiring_soon_quota_definition)
 
@@ -68,4 +77,3 @@ class TestQuotasExpiringSoonReport:
         # Check if the correct columns are present
         assert {"text": expiring_soon_quota_definition.valid_between.lower} in row_data
         assert {"text": expiring_soon_quota_definition.valid_between.upper} in row_data
-
