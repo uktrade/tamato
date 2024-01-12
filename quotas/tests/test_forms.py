ada49e76dc07d3dcce0f570a7e899ed06ba8d6b7
Edie Pearce <edie.pearce@digital.trade.gov.uk>
2024-01-12 16:27:50 +0000

    Test form cleaned_data

--- quotas/tests/test_forms.py
+++ quotas/tests/test_forms.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pytest
 from bs4 import BeautifulSoup
 from django.urls import reverse
@@ -5,6 +7,8 @@ from django.urls import reverse
 from common.models.transactions import Transaction
 from common.models.utils import override_current_transaction
 from common.tests import factories
+from common.util import TaricDateRange
+from geo_areas.models import GeographicalArea
 from quotas import forms
 from quotas import validators
 
@@ -125,3 +129,67 @@ def test_quota_definition_volume_validation(date_ranges):
             form.errors["__all__"][0]
             == "Current volume cannot be higher than initial volume"
         )
+
+
+def test_quota_update_react_form_cleaned_data(session_with_workbasket):
+    quota = factories.QuotaOrderNumberFactory.create()
+    geo_group = factories.GeoGroupFactory.create()
+    area_1 = factories.GeographicalMembershipFactory.create(geo_group=geo_group).member
+    area_2 = factories.GeographicalMembershipFactory.create(geo_group=geo_group).member
+    area_3 = factories.GeographicalMembershipFactory.create(geo_group=geo_group).member
+    # create geo area group with members to be excluded
+    data = {
+        "category": quota.category,
+        "start_date_0": 1,
+        "start_date_1": 1,
+        "start_date_2": 2000,
+        "end_date_0": 1,
+        "end_date_1": 1,
+        "end_date_2": 2010,
+        "origins-0-geographical_area": quota.quotaordernumberorigin_set.first().geographical_area.pk,
+        "origins-0-start_date_0": 1,
+        "origins-0-start_date_1": 1,
+        "origins-0-start_date_2": 2000,
+        "origins-0-end_date_0": 1,
+        "origins-0-end_date_1": 1,
+        "origins-0-end_date_2": 2010,
+        "origins-0-exclusions-0-geographical_area": area_1.pk,
+        "origins-0-exclusions-1-geographical_area": area_2.pk,
+        "submit": "Save",
+    }
+    with override_current_transaction(quota.transaction):
+        geo_area_options = (
+            GeographicalArea.objects.all()
+            .prefetch_related("descriptions")
+            .with_latest_description()
+            .as_at_today_and_beyond()
+            .order_by("description")
+        )
+        existing_origins = (
+            quota.quotaordernumberorigin_set.current().with_latest_geo_area_description()
+        )
+        form = forms.QuotaUpdateForm(
+            data=data,
+            instance=quota,
+            initial={},
+            request=session_with_workbasket,
+            geo_area_options=geo_area_options,
+            existing_origins=existing_origins,
+        )
+        assert form.is_valid()
+
+        assert "valid_between" in form.cleaned_data["origins"][0].keys()
+        assert "exclusions" in form.cleaned_data["origins"][0].keys()
+        assert "geographical_area" in form.cleaned_data["origins"][0].keys()
+
+        assert form.cleaned_data["origins"][0]["valid_between"] == TaricDateRange(
+            datetime.date(2000, 1, 1),
+            datetime.date(2010, 1, 1),
+        )
+
+        assert (
+            form.cleaned_data["origins"][0]["geographical_area"]
+            == quota.quotaordernumberorigin_set.first().geographical_area
+        )
+
+        assert len(form.cleaned_data["origins"][0]["exclusions"]) == 2
