3a1eca26e91115ca77fc58efa760d0100dd409c3
Paul Pepper <paul.pepper@digital.trade.gov.uk>
2024-01-15 09:40:03 +0000

    Move import to top.

--- measures/tasks.py
+++ measures/tasks.py
@@ -1,12 +1,11 @@
 from common.celery import app
+from measures.models import MeasuresBulkCreator
 
 
 @app.task
 def bulk_create_measures(measures_bulk_creator_pk: int) -> None:
     """Bulk create measures from serialized measures form data saved within an
     instance of MeasuresBulkCreator."""
-    from measures.models import MeasuresBulkCreator
-
     print(f"*** bulk_create_measures()")
 
     measures_bulk_creator = MeasuresBulkCreator.objects.get(pk=measures_bulk_creator_pk)
