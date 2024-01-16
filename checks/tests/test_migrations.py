bb69f3f5ba38013f0fcb9cc89454e36e1011a26e
Anthoni Gleeson <anthoni.gleeson@digital.trade.gov.uk>
2024-01-16 14:59:28 +0000

    remove comment

--- checks/tests/test_migrations.py
+++ checks/tests/test_migrations.py
@@ -1,13 +1,3 @@
-# test transaction checks
-
-# create tracked model, transaction, workbasket, tracked model check and transaction check
-# create a workbasket in published, editing, queued
-# apply 0006
-# check transaction check datetime
-# apply 0007
-# check transaction check incompletful, unsuccessful and equal to tracked model checl
-
-
 import pytest
 
 from checks.models import TransactionCheck
