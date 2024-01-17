628b6d34fff7d135051b10fa6e6ae3701669b1df
Tash Boyse <57753415+nboyse@users.noreply.github.com>
2024-01-17 13:10:41 +0000

    Feat: expand expiring quotas report to include tabs (#1131)

--- reports/reports/base_table.py
+++ reports/reports/base_table.py
@@ -6,6 +6,7 @@ from reports.reports.base import ReportBase
 class ReportBaseTable(ReportBase):
     name = "Base Table Report"
     report_template = "table"
+    tabular_reports = False
 
     def __init__(self):
         pass
@@ -19,9 +20,42 @@ class ReportBaseTable(ReportBase):
         pass
 
     @abstractmethod
-    def rows(self) -> [[dict]]:
+    def rows(self) -> [dict]:
         pass
 
     @abstractmethod
     def row(self, row) -> [dict]:
         pass
+
+    def headers2(self) -> [dict]:
+        return []
+
+    def rows2(self) -> [dict]:
+        return []
+
+    def row2(self, row) -> [dict]:
+        return []
+
+    def headers3(self) -> [dict]:
+        return []
+
+    def rows3(self) -> [dict]:
+        return []
+
+    def row3(self, row) -> [dict]:
+        return []
+
+    def headers4(self) -> [dict]:
+        return []
+
+    def rows4(self) -> [dict]:
+        return []
+
+    def row4(self, row) -> [dict]:
+        return []
+
+    def query3(self):
+        return []
+
+    def query4(self):
+        return []
