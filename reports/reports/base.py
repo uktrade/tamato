e5f70b87835189d4f04ffba6c26f2cab925be20b
nboyse <natasha.boyse@digital.trade.gov.uk>
2024-01-18 11:51:53 +0000

    remove unused class

--- reports/reports/base.py
+++ reports/reports/base.py
@@ -18,9 +18,3 @@ class ReportBase(ABC):
         result = slugify(cls.name).replace("-", "_")
         result = result.replace("__", "_")
         return result
-
-    @classmethod
-    def slugify(tab_name):
-        result = slugify(tab_name).replace("-", "_")
-        result = result.lower()
-        return result
