51849124764c917d10cf4ddf49593abe44080ec9
nboyse <natasha.boyse@digital.trade.gov.uk>
2024-01-18 11:06:37 +0000

    feat: adjust UI and remove JS as per review

--- reports/reports/base.py
+++ reports/reports/base.py
@@ -18,3 +18,9 @@ class ReportBase(ABC):
         result = slugify(cls.name).replace("-", "_")
         result = result.replace("__", "_")
         return result
+
+    @classmethod
+    def slugify(tab_name):
+        result = slugify(tab_name).replace("-", "_")
+        result = result.lower()
+        return result
