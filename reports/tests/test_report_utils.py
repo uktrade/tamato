cef3f826cb98800eeca75892f6afae1f318156fa
Tash Boyse <57753415+nboyse@users.noreply.github.com>
2024-01-23 14:04:02 +0000

    feat: implement URLs for quota reports to ease navigation (#1135)

--- reports/tests/test_report_utils.py
+++ reports/tests/test_report_utils.py
@@ -1,6 +1,9 @@
 # Create your tests here.
 import pytest
+from django.urls import reverse
+from django.utils.safestring import SafeString
 
+from common.tests import factories
 from reports.reports.base import ReportBase
 from reports.reports.base_table import ReportBaseTable
 from reports.reports.blank_goods_nomenclature_descriptions import Report
@@ -45,3 +48,26 @@ class TestUtils:
         with pytest.raises(Exception) as ex:
             get_template_by_type("werwer")
             assert str(ex) == "Unknown chart type : werwer"
+
+    @pytest.mark.django_db
+    def test_link_renderer_for_quotas(self, db):
+        order_number_obj = factories.QuotaOrderNumberFactory.create()
+
+        report_instance = Report()
+
+        # Test without fragment
+        result = report_instance.link_renderer_for_quotas(order_number_obj, "Test Text")
+        expected_url = reverse("quota-ui-detail", args=[order_number_obj.sid])
+        expected_output = f"<a class='govuk-link govuk-!-font-weight-bold' href='{expected_url}'>Test Text</a>"
+        assert result == SafeString(expected_output)
+
+        # Test with fragment
+        result = report_instance.link_renderer_for_quotas(
+            order_number_obj, "Test Text", fragment="#blocking-periods"
+        )
+        expected_url = (
+            reverse("quota-ui-detail", args=[order_number_obj.sid])
+            + "#blocking-periods"
+        )
+        expected_output = f"<a class='govuk-link govuk-!-font-weight-bold' href='{expected_url}'>Test Text</a>"
+        assert result == SafeString(expected_output)
