3ecc22c9f1aea44159e6c8cb97d3fc38664d0ec3
Dale Cannon <dale.cannon@digital.trade.gov.uk>
2024-01-24 15:57:41 +0000

    Add forgotten content type data migration

--- common/migrations/0008_user_current_workbasket.py
+++ common/migrations/0008_user_current_workbasket.py
@@ -5,6 +5,25 @@ from django.db import migrations
 from django.db import models
 
 
+def change_user_content_type(apps, schema_editor):
+    """
+    The addition of the new current_workbasket field marks the move to a custom
+    user model from this point in migration history onwards.
+
+    As a result, the auth.User content type must be updated to reflect
+    common.User (custom user model) to preserve existing references.
+    """
+
+    ContentType = apps.get_model("contenttypes", "ContentType")
+    ct = ContentType.objects.filter(
+        app_label="auth",
+        model="user",
+    ).first()
+    if ct:
+        ct.app_label = "common"
+        ct.save()
+
+
 class Migration(migrations.Migration):
     dependencies = [
         ("workbaskets", "0008_datarow_dataupload"),
@@ -12,6 +31,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(change_user_content_type),
         migrations.AddField(
             model_name="user",
             name="current_workbasket",
