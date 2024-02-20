41680fc8b2bd106050e1b7a3b3e7e29832281a23
Edie Pearce <edie.pearce@digital.trade.gov.uk>
2024-02-20 11:06:57 +0000

    Add duty sentence validation

--- measures/duty_sentence_parser.py
+++ measures/duty_sentence_parser.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ValidationError
 from django.template.loader import render_to_string
 from lark import Lark
 from lark import Transformer
@@ -17,27 +18,41 @@ class DutySyntaxError(SyntaxError):
             hint_display = f" \n\n{self.hint} "
         else:
             hint_display = ""
-        return f"{self.label} at character {column}.{hint_display}\n\n{context}"
+        return f"{self.label} at character {column}.{hint_display}"
 
 
-class DutyInvalidDutyExpression(DutySyntaxError):
+class InvalidDutyExpression(DutySyntaxError):
     label = "No matching duty expression found"
     hint = "Check the validity period of the duty expression and that you are using the correct prefix."
 
 
-class DutyInvalidMonetaryUnit(DutySyntaxError):
+class InvalidMonetaryUnit(DutySyntaxError):
     label = "No matching monetary unit or percentage amount found"
     hint = f"Check the validity period of the monetary unit and that you are using the correct code. If the amount is a percentage make sure to include the % symbol after the number."
 
 
-class DutyInvalidMeasurementUnit(DutySyntaxError):
+class InvalidMeasurementUnit(DutySyntaxError):
     label = "No matching measurement unit found"
     hint = "Check the validity period of the measurement unit and that you are using the correct abbreviation (not code)."
 
 
-class DutyInvalidMeasurementUnitQualififer(DutySyntaxError):
+class InvalidMeasurementUnitQualififer(DutySyntaxError):
     label = "No matching measurement unit qualifier found"
     hint = "Check the validity period of the measurement unit qualifier and that you are using the correct abbreviation."
+
+
+INVALID_DUTY_EXPERESSION_MESSAGE = (
+    f"{InvalidDutyExpression.label}. {InvalidDutyExpression.hint}"
+)
+INVALID_DUTY_MONETARY_UNIT_MESSAGE = (
+    f"{InvalidMonetaryUnit.label}. {InvalidMonetaryUnit.hint}"
+)
+INVALID_DUTY_MEASUREMENT_UNIT_MESSAGE = (
+    f"{InvalidMeasurementUnit.label}. {InvalidMeasurementUnit.hint}"
+)
+INVALID_DUTY_MEASUREMENT_UNIT_QUALIFIER_MESSAGE = (
+    f"{InvalidMeasurementUnitQualififer.label}. {InvalidMeasurementUnitQualififer.hint}"
+)
 
 
 class DutySentenceParser:
@@ -56,6 +71,13 @@ class DutySentenceParser:
             "duties/parser/duty_expression.jinja",
             {
                 "code": ApplicabilityCode.MANDATORY,
+                "duty_expressions": duty_expressions,
+            },
+        )
+        duty_expression_amount_permitted = render_to_string(
+            "duties/parser/duty_expression.jinja",
+            {
+                "code": ApplicabilityCode.PERMITTED,
                 "duty_expressions": duty_expressions,
             },
         )
@@ -84,6 +106,7 @@ class DutySentenceParser:
 
             # DutyExpression
             !expr_amount_mandatory: {duty_expression_amount_mandatory}
+            !expr_amount_permitted: {duty_expression_amount_permitted}
             !expr_amount_not_permitted: {duty_expression_amount_not_permitted}
             # variables output as "-" | "+" | "MAX" | "MIN" | "NIHIL" | "+ AC" etc. see template
 
@@ -101,7 +124,7 @@ class DutySentenceParser:
             # MeasureComponent
             !phrase: expr_amount_not_permitted [slash measurement_unit [measurement_unit_qualifier]]
                 | duty_amount monetary_unit [slash measurement_unit [measurement_unit_qualifier]]
-                | expr_amount_mandatory duty_amount monetary_unit [slash measurement_unit [measurement_unit_qualifier]]
+                | (expr_amount_mandatory | expr_amount_permitted) duty_amount monetary_unit [slash measurement_unit [measurement_unit_qualifier]]
 
             !sentence: phrase+
 
@@ -121,24 +144,24 @@ class DutySentenceParser:
             exc_class = u.match_examples(
                 self.parser.parse,
                 {
-                    DutyInvalidDutyExpression: [
+                    InvalidDutyExpression: [
                         "10% + Blah duty (reduced)",
                         "5.5% + ABCDE + Some other fake duty expression",
                         "10%&@#^&",
                         "ABC",
                         "@(*&$#)",
                     ],
-                    DutyInvalidMonetaryUnit: [
+                    InvalidMonetaryUnit: [
                         "10% + 100 ABC / 100 kg",
                         "100 DEF",
                         "5.5% + 100 XYZ + AC (reduced)",
                     ],
-                    DutyInvalidMeasurementUnit: [
+                    InvalidMeasurementUnit: [
                         "10% + 100 GBP / 100 abc",
                         "100 GBP / foobar measurement",
                         "5.5% + 100 EUR / foobar",
                     ],
-                    DutyInvalidMeasurementUnitQualififer: [
+                    InvalidMeasurementUnitQualififer: [
                         "10% + 100 GBP / 100 kg ABC",
                         "100 GBP / 100 kg XYZ foo bar",
                         "5.5% + 100 EUR / % vol foo bar",
@@ -151,14 +174,6 @@ class DutySentenceParser:
             raise exc_class(u.get_context(duty_sentence), u.line, u.column)
 
 
-# Measure components (only 1):
-#         m1: duty.expression.id (01 or 37 if NIHIL)
-#         m2: duty.amount
-#         m3: monetary.unit.code
-#         m4: measurement.unit.code
-#         m5: measurement.unit.qualifier.code
-
-
 class DutyTransformer(Transformer):
     """Takes the output from the DutySentenceParser and returns objects for
     DutyExpressions, MonetaryUnits, MeasurementUnits and
@@ -168,13 +183,121 @@ class DutyTransformer(Transformer):
         self.date = kwargs.pop("date")
         super().__init__()
 
-    @property
-    def duty_expressions(self):
-        return (
+    def validate_duty_expressions(self, transformed):
+        duty_expressions = (
             models.DutyExpression.objects.as_at(self.date)
             .exclude(prefix__isnull=True)
             .order_by("sid")
         )
+
+        duty_expression_sids = [d.sid for d in duty_expressions]
+
+        for phrase in transformed:
+            duty_expression = phrase.get("duty_expression", "")
+            match = (
+                models.DutyExpression.objects.as_at(self.date)
+                .filter(
+                    prefix__iexact=duty_expression,
+                    sid__in=duty_expression_sids,
+                )
+                .order_by("sid")
+                .first()
+            )
+            if match is None:
+                potential_match = (
+                    models.DutyExpression.objects.as_at(self.date)
+                    .filter(
+                        prefix__iexact=duty_expression,
+                    )
+                    .order_by("sid")
+                    .first()
+                )
+                raise ValidationError(
+                    f"A duty expression cannot be used more than once in a duty sentence. Matching expression: {potential_match.description} ({potential_match.prefix})",
+                )
+
+            # Each duty expression can only be used once in a sentence and in order of increasing
+            # SID so once we have a match, remove it from the list of duty expression sids
+            duty_expression_sids.remove(match.sid)
+            # Update with the matching DutyExpression we found
+            phrase["duty_expression"] = match
+
+        transformed_duty_expression_sids = [
+            phrase["duty_expression"].sid for phrase in transformed
+        ]
+
+        if transformed_duty_expression_sids != sorted(transformed_duty_expression_sids):
+            raise ValidationError(
+                "Duty expressions must be used in the duty sentence in ascending order of SID.",
+            )
+
+    @staticmethod
+    def validate_according_to_applicability_code(
+        code,
+        duty_expression,
+        item,
+        item_name,
+    ):
+        if code == ApplicabilityCode.MANDATORY and not item:
+            raise ValidationError(
+                f"Duty expression {duty_expression.description} ({duty_expression.prefix}) requires a {item_name}.",
+            )
+        if code == ApplicabilityCode.NOT_PERMITTED and item:
+            if isinstance(item, object):
+                message = f"{item_name} {item.abbreviation} ({item.code}) cannot be used with duty expression {duty_expression.description} ({duty_expression.prefix})."
+            else:
+                message = f"{item_name} cannot be used with duty expression {duty_expression.description} ({duty_expression.prefix})."
+            raise ValidationError(message)
+
+    def validate_phrase(self, phrase):
+        # Each measure component can have an amount, monetary unit and measurement.
+        # Which expression elements are allowed in a component is controlled by
+        # the duty expression applicability codes
+        duty_expression = phrase["duty_expression"]
+        amount_code = duty_expression.duty_amount_applicability_code
+        monetary_code = duty_expression.monetary_unit_applicability_code
+        measurement_unit_code = duty_expression.measurement_unit_applicability_code
+
+        duty_amount = phrase.get("duty_amount")
+        monetary_unit = phrase.get("monetary_unit")
+        measurement_unit = phrase.get("measurement_unit")
+
+        self.validate_according_to_applicability_code(
+            amount_code,
+            duty_expression,
+            duty_amount,
+            "duty amount",
+        )
+        self.validate_according_to_applicability_code(
+            monetary_code,
+            duty_expression,
+            monetary_unit,
+            "monetary unit",
+        )
+        self.validate_according_to_applicability_code(
+            measurement_unit_code,
+            duty_expression,
+            measurement_unit,
+            "measurement unit",
+        )
+
+        # If there is a measurement unit qualifier, validate that it can be used with the measurement
+
+    def validate_sentence(self, transformed):
+        for phrase in transformed:
+            self.validate_phrase(phrase)
+
+    def is_valid(self, transformed):
+        self.validate_duty_expressions(transformed)
+        self.validate_sentence(transformed)
+
+        return True
+
+    def transform(self, tree):
+        # perform validation and raise any errors before returning the transformed tree
+        transformed = self._transform_tree(tree)
+        if self.is_valid(transformed):
+            return transformed
 
     def sentence(self, items):
         return items
@@ -185,25 +308,11 @@ class DutyTransformer(Transformer):
 
     def expr_amount_mandatory(self, value):
         (value,) = value
-        try:
-            match = self.duty_expressions.filter(
-                prefix__iexact=value,
-                duty_amount_applicability_code=ApplicabilityCode.MANDATORY,
-            ).first()
-        except ObjectDoesNotExist:
-            match = None
-        return ("duty_expression", match)
+        return ("duty_expression", value)
 
     def expr_amount_not_permitted(self, value):
         (value,) = value
-        try:
-            match = self.duty_expressions.filter(
-                prefix__iexact=value,
-                duty_amount_applicability_code=ApplicabilityCode.NOT_PERMITTED,
-            ).first()
-        except ObjectDoesNotExist:
-            match = None
-        return ("duty_expression", match)
+        return ("duty_expression", value)
 
     def duty_amount(self, value):
         (value,) = value
@@ -216,7 +325,7 @@ class DutyTransformer(Transformer):
         try:
             match = models.MonetaryUnit.objects.as_at(self.date).get(code__iexact=value)
         except ObjectDoesNotExist:
-            match = None
+            raise ValidationError(INVALID_DUTY_MONETARY_UNIT_MESSAGE)
         return ("monetary_unit", match)
 
     def measurement_unit(self, value):
@@ -226,7 +335,7 @@ class DutyTransformer(Transformer):
                 abbreviation__iexact=value,
             )
         except ObjectDoesNotExist:
-            match = None
+            raise ValidationError(INVALID_DUTY_MEASUREMENT_UNIT_MESSAGE)
         return ("measurement_unit", match)
 
     def measurement_unit_qualifier(self, value):
@@ -236,7 +345,7 @@ class DutyTransformer(Transformer):
                 abbreviation__iexact=value,
             )
         except ObjectDoesNotExist:
-            match = None
+            raise ValidationError(INVALID_DUTY_MEASUREMENT_UNIT_QUALIFIER_MESSAGE)
         return ("measurement_unit_qualifier", match)
 
     def slash(self, value):
