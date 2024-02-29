38ca1993e953347551a1eee5a017fe847c16c5ed
Paul Pepper <paul.pepper@digital.trade.gov.uk>
2024-02-29 18:38:44 +0000

    Manage processing_state in task execution and add logging.

--- measures/tasks.py
+++ measures/tasks.py
@@ -12,17 +12,38 @@ def bulk_create_measures(measures_bulk_creator_pk: int) -> None:
     instance of MeasuresBulkCreator."""
 
     measures_bulk_creator = MeasuresBulkCreator.objects.get(pk=measures_bulk_creator_pk)
-    measures = measures_bulk_creator.create_measures()
+    measures_bulk_creator.begin_processing()
+    measures_bulk_creator.save()
 
-    if len(measures) > 0:
-        transaction = measures[0].transaction
-        workbasket = transaction.workbasket
+    try:
+        measures = measures_bulk_creator.create_measures()
+    except Exception as e:
+        # TODO:
+        # - Does a BulkProcessor.cancel_task(), which calls
+        #   app.control.revoke(self.task_id, terminate=True) and in turn issues
+        #   a SIGINT to the Celery process, result in an exception here?
+
+        measures_bulk_creator.processing_failed()
+        measures_bulk_creator.save()
+        logger.error(
+            f"MeasuresBulkCreator({measures_bulk_creator.pk}) task failed "
+            f"attempting to create measures in "
+            f"WorkBasket({measures_bulk_creator.workbasket.pk}).",
+        )
+        raise e
+
+    measures_bulk_creator.processing_succeeded()
+    measures_bulk_creator.save()
+
+    if measures:
         logger.info(
-            f"MeasuresBulkCreator({measures_bulk_creator.pk}) created "
-            f"{len(measures)} Measures in WorkBasket({workbasket.pk}) on "
-            f"Transaction({transaction.pk}).",
+            f"MeasuresBulkCreator({measures_bulk_creator.pk}) task "
+            f"succeeded in creating {len(measures)} Measures in "
+            f"WorkBasket({measures_bulk_creator.workbasket.pk}).",
         )
     else:
         logger.info(
-            f"MeasuresBulkCreator {measures_bulk_creator.pk} created no " f"measures.",
+            f"MeasuresBulkCreator({measures_bulk_creator.pk}) task "
+            f"succeeded but created no measures in "
+            f"WorkBasket({measures_bulk_creator.workbasket.pk}).",
         )
