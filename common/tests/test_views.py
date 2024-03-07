242576a267e40af0b1bf39b846dbe600d5311c20
Dale Cannon <dale.cannon@digital.trade.gov.uk>
2024-03-07 17:07:28 +0000

    Add tests

--- common/tests/test_views.py
+++ common/tests/test_views.py
@@ -1,5 +1,3 @@
-import re
-
 import pytest
 from bs4 import BeautifulSoup
 from django.conf import settings
@@ -8,40 +6,15 @@ from django.test import modify_settings
 from django.test import override_settings
 from django.urls import reverse
 
+from checks.tests.factories import TrackedModelCheckFactory
 from common.tests import factories
 from common.util import xml_fromstring
 from common.views import HealthCheckResponse
 from common.views import handler403
 from common.views import handler500
+from tasks.models import UserAssignment
 
 pytestmark = pytest.mark.django_db
-
-
-@pytest.mark.parametrize(
-    ("action", "permission"),
-    [
-        ("Create a new workbasket", "add_workbasket"),
-        ("Edit workbaskets", "add_workbasket"),
-        ("Package workbaskets", "manage_packaging_queue"),
-        ("Process envelopes", "consume_from_packaging_queue"),
-        ("Search the tariff", ""),
-        ("Import EU TARIC files", "add_trackedmodel"),
-        ("Search for workbaskets", "view_workbasket"),
-    ],
-)
-def test_home_form_actions_match_permissions(action, permission, client):
-    """Tests that the workbasket action form on the home page displays the
-    appropriate radio options for the user's permissions."""
-    user = factories.UserFactory.create()
-    if permission:
-        user.user_permissions.add(Permission.objects.get(codename=permission))
-    client.force_login(user)
-
-    response = client.get(reverse("home"))
-    assert response.status_code == 200
-
-    page = BeautifulSoup(response.content.decode(response.charset), "html.parser")
-    assert page.find("label", class_="govuk-radios__label", string=re.compile(action))
 
 
 def test_index_displays_logout_buttons_correctly_SSO_off_logged_in(valid_user_client):
@@ -71,65 +44,6 @@ def test_index_displays_login_buttons_correctly_SSO_on(valid_user_client):
     page = BeautifulSoup(str(response.content), "html.parser")
     assert not page.find("form", {"action": reverse("logout")})
     assert not page.find_all("a", {"href": "/login"})
-
-
-@pytest.mark.parametrize(
-    ("data", "response_url"),
-    (
-        (
-            {
-                "workbasket_action": "CREATE",
-            },
-            "workbaskets:workbasket-ui-create",
-        ),
-        (
-            {
-                "workbasket_action": "EDIT",
-            },
-            "workbaskets:workbasket-ui-list",
-        ),
-        (
-            {
-                "workbasket_action": "PACKAGE_WORKBASKETS",
-            },
-            "publishing:packaged-workbasket-queue-ui-list",
-        ),
-        (
-            {
-                "workbasket_action": "PROCESS_ENVELOPES",
-            },
-            "publishing:envelope-queue-ui-list",
-        ),
-        (
-            {
-                "workbasket_action": "SEARCH",
-            },
-            "search-page",
-        ),
-        (
-            {
-                "workbasket_action": "IMPORT",
-            },
-            "commodity_importer-ui-list",
-        ),
-        (
-            {
-                "workbasket_action": "WORKBASKET_LIST_ALL",
-            },
-            "workbaskets:workbasket-ui-list-all",
-        ),
-    ),
-)
-def test_workbasket_action_form_response_redirects_user(
-    valid_user,
-    client,
-    data,
-    response_url,
-):
-    client.force_login(valid_user)
-    response = client.post(reverse("home"), data)
-    assert response.status_code == 302
-    assert response.url == reverse(response_url)
 
 
 @pytest.mark.parametrize(
@@ -259,3 +173,132 @@ def test_maintenance_mode_page_content(valid_user_client):
     response = valid_user_client.get(reverse("maintenance"))
     assert response.status_code == 200
     assert "Sorry, the service is unavailable" in str(response.content)
+
+
+@pytest.mark.parametrize(
+    ("card", "permission"),
+    [
+        ("What would you like to do?", "view_workbasket"),
+        ("Currently working on", "view_workbasket"),
+        ("EU TARIC files", "add_trackedmodel"),
+        ("Envelopes", "consume_from_packaging_queue"),
+        ("Tools", ""),
+        ("Get help", ""),
+        ("Other ways of support", ""),
+    ],
+)
+def test_homepage_cards_match_user_permissions(card, permission, client):
+    user = factories.UserFactory.create()
+    client.force_login(user)
+
+    if permission:
+        response = client.get(reverse("home"))
+        assert not card in str(response.content)
+        user.user_permissions.add(Permission.objects.get(codename=permission))
+
+    response = client.get(reverse("home"))
+    assert card in str(response.content)
+
+
+def test_homepage_cards_contain_expected_links(superuser_client):
+    expected_urls = {
+        "Create a new workbasket": "workbaskets:workbasket-ui-create",
+        "Edit a workbasket": "workbaskets:workbasket-ui-list",
+        "Package a workbasket": "publishing:packaged-workbasket-queue-ui-list",
+        "Search for workbaskets": "workbaskets:workbasket-ui-list-all",
+        "View EU import list": "commodity_importer-ui-list",
+        "Process envelopes": "publishing:envelope-queue-ui-list",
+        "Application information": "app-info",
+        "Importer V1": "import_batch-ui-list",
+        "Importer V2": "taric_parser_import_ui_list",
+        "TAP reports": "reports:index",
+    }
+    response = superuser_client.get(reverse("home"))
+    page = BeautifulSoup(response.content.decode(response.charset), "html.parser")
+    links = [a["href"] for a in page.find_all("a")]
+
+    for url in expected_urls.values():
+        assert reverse(url) in links
+
+
+def test_homepage_card_currently_working_on(valid_user, valid_user_client):
+    workbasket = factories.WorkBasketFactory.create()
+    review_assignment = factories.UserAssignmentFactory.create(
+        user=valid_user,
+        assignment_type=UserAssignment.AssignmentType.WORKBASKET_REVIEWER,
+        task__workbasket=workbasket,
+    )
+    rule_violation = TrackedModelCheckFactory.create(
+        transaction_check__transaction=workbasket.new_transaction(),
+        successful=False,
+    )
+
+    response = valid_user_client.get(reverse("home"))
+    page = BeautifulSoup(response.content.decode(response.charset), "html.parser")
+    card = page.find("h3", string="Currently working on")
+    assigned_workbasket = card.find_next("a")
+    details = card.find_next("span")
+
+    assert f"Workbasket ID {workbasket.pk}" in assigned_workbasket.text
+    assert "Reviewing" and "rule violation" in details.text
+
+
+@pytest.mark.parametrize(
+    ("factory", "id", "kwargs"),
+    [
+        (factories.AdditionalCodeFactory, None, {}),
+        (factories.CertificateFactory, "code", {}),
+        (factories.FootnoteFactory, None, {}),
+        (factories.GeographicalAreaFactory, "area_id", {}),
+        (factories.GoodsNomenclatureFactory, "item_id", {}),
+        (factories.MeasureFactory, "sid", {"sid": 123456}),
+        (factories.QuotaOrderNumberFactory, "order_number", {}),
+        (factories.RegulationFactory, "regulation_id", {}),
+    ],
+)
+def test_homepage_search_by_element_id_returns_result(
+    factory,
+    id,
+    kwargs,
+    valid_user_client,
+):
+    model = factory(**kwargs)
+    form_data = {
+        "search_term": getattr(model, id) if id else model.__str__(),
+    }
+    response = valid_user_client.post(reverse("home"), form_data)
+    assert response.status_code == 302
+    assert response.url == model.get_url()
+
+
+@pytest.mark.parametrize(
+    ("name", "expected_url"),
+    [
+        ("additional codes", "additional_code-ui-list"),
+        ("certificates", "certificate-ui-list"),
+        ("footnotes", "footnote-ui-list"),
+        ("geographical areas", "geo_area-ui-list"),
+        ("commodities", "commodity-ui-list"),
+        ("measures", "measure-ui-search"),
+        ("quotas", "quota-ui-list"),
+        ("regulations", "regulation-ui-list"),
+    ],
+)
+def test_homepage_search_by_element_name_returns_result(
+    name,
+    expected_url,
+    valid_user_client,
+):
+    form_data = {
+        "search_term": name,
+    }
+    response = valid_user_client.post(reverse("home"), form_data)
+    assert response.status_code == 302
+    assert response.url == reverse(expected_url)
+
+
+def test_homepage_search_no_result(valid_user_client):
+    factories.FootnoteFactory.create()
+    response = valid_user_client.post(reverse("home"), {"search_term": "empty"})
+    assert response.status_code == 302
+    assert response.url == reverse("search-page")
