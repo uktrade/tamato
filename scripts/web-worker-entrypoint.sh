6b15f655bd4afe9da2a0927f1169f0a7586fd0ad
Paul Pepper <paul.pepper@digital.trade.gov.uk>
2024-04-24 16:37:10 +0100

    Execute script file via shell interpreter as a string.

--- scripts/web-worker-entrypoint.sh
+++ scripts/web-worker-entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -e
 
-WORKER_COMMAND="gunicorn wsgi --bind 0.0.0.0:$PORT --timeout 1000 --worker-class=gevent --worker-connections=1000 --workers 9"
+WORKER_COMMAND="gunicorn wsgi --bind 0.0.0.0:${PORT} --timeout 1000 --worker-class=gevent --worker-connections=1000 --workers 9"
 
 if [ -n "${COPILOT_ENVIRONMENT_NAME}" ]; then
-    echo "Running in DBT Platform"
-    WORKER_COMMAND="opentelemetry-instrument ${WORKER_COMMAND}
+    WORKER_COMMAND="opentelemetry-instrument ${WORKER_COMMAND}"
 fi
 
-eval $WORKER_COMMAND
+#echo "Starting worker using the command: \'${WORKER_COMMAND}\'"
+
+eval ${WORKER_COMMAND}
