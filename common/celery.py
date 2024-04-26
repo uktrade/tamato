839ff39ddc64b59a61c5a3a4cf8d0c152f326ee4
Paul Pepper <paul.pepper@digital.trade.gov.uk>
2024-04-26 17:04:50 +0100

    Add DBT Celery health check support.

--- common/celery.py
+++ common/celery.py
@@ -2,6 +2,7 @@ import os
 
 from celery import Celery
 from celery.signals import setup_logging
+from dbt_copilot_python.celery_health_check import healthcheck
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
@@ -31,3 +32,6 @@ app.autodiscover_tasks()
 # this should be automactically configured via ^^ config_from_object
 # but it isn't so here it's configured here
 app.conf.task_routes = settings.CELERY_ROUTES
+
+# Setup and expose the DBT Celery healthcheck interface.
+app = healthcheck.setup(app)
