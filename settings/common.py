a1001e60a8b228bc26c3c10a955c161dca6bbf18
Matthew McKenzie <97194636+mattjamc@users.noreply.github.com>
2024-05-14 14:40:39 +0100

    Celery log level always be DEBUG

--- settings/common.py
+++ settings/common.py
@@ -693,7 +693,7 @@ LOGGING = {
         },
         "celery": {
             "handlers": ["celery"],
-            "level": os.environ.get("CELERY_LOG_LEVEL", "DEBUG"),
+            "level": "DEBUG",
             "propagate": False,
         },
     },
