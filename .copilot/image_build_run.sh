b2935af9cd204ab1d1e62a9a8fef58e60da38883
Emile Swarts <emile.swarts@digital.trade.gov.uk>
2024-05-15 16:45:52 +0100

    Add copilot configuration files

---
+++ .copilot/image_build_run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Exit early if something goes wrong
+set -e
+
+# Add commands below to run inside the container after all the other buildpacks have been applied
---
+++ .copilot/phases/post_build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Exit early if something goes wrong
+set -e
+
+# Add commands below to run as part of the post_build phase
