f1f431095e543327c5894d66b6661984f198755e
Emile Swarts <emile.swarts@digital.trade.gov.uk>
2024-05-17 15:25:15 +0100

    Test compiling assets when the task starts

--- scripts/web-worker-entrypoint.sh
+++ scripts/web-worker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -e
 
+# Django Migrations
+python manage.py makemigrations
+python manage.py migrate
+npm run build
+python manage.py collectstatic --no-input
 WORKER_COMMAND="gunicorn wsgi --bind 0.0.0.0:${PORT} --timeout 1000 --worker-class=gevent --worker-connections=1000 --workers 9"
 
 echo "Starting worker using the command: ${WORKER_COMMAND}"
