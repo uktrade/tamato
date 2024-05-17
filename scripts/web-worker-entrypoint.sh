73a8da7b84fe8a4c6a5e8af7f7800367df63e83d
Emile Swarts <emile.swarts@digital.trade.gov.uk>
2024-05-17 14:28:54 +0100

    Reduce number of gunicorn workers and increase timeout as a test

--- scripts/web-worker-entrypoint.sh
+++ scripts/web-worker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-WORKER_COMMAND="gunicorn wsgi --bind 0.0.0.0:${PORT} --timeout 1000 --worker-class=gevent --worker-connections=1000 --workers 9"
+WORKER_COMMAND="gunicorn wsgi --bind 0.0.0.0:${PORT} --timeout 4000 --worker-class=gevent --worker-connections=1000 --workers 3"
 
 echo "Starting worker using the command: ${WORKER_COMMAND}"
 
