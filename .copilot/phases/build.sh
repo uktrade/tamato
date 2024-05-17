dae8dbae6fbddac35b98946c08cc3587800f4b6e
Emile Swarts <emile.swarts@digital.trade.gov.uk>
2024-05-17 15:42:22 +0100

    Remove makemigrations

--- .copilot/phases/build.sh
+++ .copilot/phases/build.sh
@@ -5,7 +5,6 @@ set -ex
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-python manage.py makemigrations
 npm install
 npm run build
 python manage.py collectstatic --no-input
