06b9aa00de9b482db56ffada698203a6a025aeff
Emile Swarts <emile.swarts@digital.trade.gov.uk>
2024-05-17 15:31:31 +0100

    Move asset compilation to build

--- .copilot/phases/build.sh
+++ .copilot/phases/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# Exit early if something goes wrong
 set -e
 
-# Add commands below to run as part of the build phase
+python manage.py makemigrations
+npm run build
+python manage.py collectstatic --no-input
