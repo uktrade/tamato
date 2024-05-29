a096189443612bb9f3fdf3befaae5e8be8742b6b
Matthew McKenzie <97194636+mattjamc@users.noreply.github.com>
2024-05-29 10:33:28 +0100

    Return workers / reviewers as single values rather than tuples

--- workbaskets/models.py
+++ workbaskets/models.py
@@ -671,12 +671,12 @@ class WorkBasket(TimestampedMixin):
 
     @property
     def assigned_workers(self):
-        user_ids = self.worker_assignments.values_list("user_id")
+        user_ids = self.worker_assignments.values_list("user_id", flat=True)
         return User.objects.filter(id__in=user_ids)
 
     @property
     def assigned_reviewers(self):
-        user_ids = self.reviewer_assignments.values_list("user_id")
+        user_ids = self.reviewer_assignments.values_list("user_id", flat=True)
         return User.objects.filter(id__in=user_ids)
 
     class Meta:
