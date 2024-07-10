f8b6915b88e1a3d7b5a9cdb4b26dfa9c0852a72b
Paul Pepper <paul.pepper@digital.trade.gov.uk>
2024-07-10 10:58:55 +0100

    Rename mixin.

--- exporter/storages.py
+++ exporter/storages.py
@@ -31,15 +31,15 @@ class HMRCStorage(S3Boto3Storage):
         return super().get_object_parameters(name)
 
 
-class SQLiteStorageMixin:
+class SQLiteExportMixin:
     def make_export(self, filename: str):
         """Export to SQLite database."""
         raise NotImplementedError
 
 
-class SQLiteS3VFSStorage(SQLiteStorageMixin, S3Boto3Storage):
-    """Remote Sqlite DB storage using s3sqlite to provide virtual file system
-    storage (see https://pypi.org/project/s3sqlite/)."""
+class SQLiteS3VFSStorage(SQLiteExportMixin, S3Boto3Storage):
+    """Remote Sqlite DB creation and storage using s3sqlite to provide virtual
+    file system storage (see https://pypi.org/project/s3sqlite/)."""
 
     def get_default_settings(self):
         from django.conf import settings
@@ -83,7 +83,9 @@ class SQLiteS3VFSStorage(SQLiteStorageMixin, S3Boto3Storage):
         logger.info(f"Export {filename} complete")
 
 
-class SQLiteLocalStorage(SQLiteStorageMixin, Storage):
+class SQLiteLocalStorage(SQLiteExportMixin, Storage):
+    """Local file system SQLite DB creation and storage."""
+
     def __init__(self, location) -> None:
         self._location = Path(location).expanduser().resolve()
         logger.info(f"Normalised path `{location}` to `{self._location}`.")
