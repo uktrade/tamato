35a0f468fbdefadff05007f3e18e0ba616e39195
Dale Cannon <dale.cannon@digital.trade.gov.uk>
2024-07-11 13:28:30 +0100

    Set up E2E test fixtures to work with an existing server & DB instance

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -1,94 +1,213 @@
 import os
 
 import pytest
-from django.contrib.auth.models import Permission
+from django.conf import settings
 from playwright.sync_api import Page
-from pytest_django.live_server_helper import LiveServer
 
+from checks.tests.factories import TrackedModelCheckFactory
+from commodities.models import GoodsNomenclature
+from common.models.transactions import Transaction
+from common.models.utils import override_current_transaction
 from common.tests import factories
-from publishing.models import QueueState
+from common.validators import ApplicabilityCode
+from footnotes.models import Footnote
+from footnotes.models import FootnoteType
+from measures.models import MeasureType
+from measures.validators import MeasureExplosionLevel
+from publishing.models import OperationalStatus
+from publishing.models import PackagedWorkBasket
+from regulations.models import Regulation
+from tasks.models import UserAssignment
 
+from .utils import get_unique_id
 from .utils import login
 
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
 
 @pytest.fixture(scope="session")
-def celery_config():
-    return {
-        "task_always_eager": False,
-    }
+def django_db_setup():
+    """
+    Enables an existing, external database to be used instead of a temporary
+    test database.  Defaults to `settings.DATABASES["default"]`.
+
+    To avoid flushing the database during `TransactionTestCase` teardown, a no-op function replaces the default flush command.
+    """
+    env = settings.ENV
+    if env.startswith("prod"):
+        pytest.exit(
+            f"{env} is an unsuitable environment for running end-to-end tests that may have enduring side effects.",
+        )
+
+    def no_op(obj, **options):
+        pass
+
+    from django.core.management.commands import flush
+
+    flush.Command.handle = no_op
 
 
 @pytest.fixture(scope="session")
-def celery_includes():
-    return [
-        "measures.tasks",
-    ]
+def base_url():
+    """Returns the base URL of the server under test from
+    `settings.BASE_SERVICE_URL`."""
+    return settings.BASE_SERVICE_URL
 
 
 @pytest.fixture
-def tariff_manager_group():
-
-    def set_permissions():
-        for app_label, codename in [
-            ("common", "add_trackedmodel"),
-            ("common", "change_trackedmodel"),
-            ("publishing", "manage_packaging_queue"),
-            ("tasks", "add_userassignment"),
-            ("workbaskets", "add_workbasket"),
-            ("workbaskets", "view_workbasket"),
-        ]:
-            group.permissions.add(
-                Permission.objects.get(
-                    content_type__app_label=app_label,
-                    codename=codename,
-                ),
-            )
-
-    group = factories.UserGroupFactory.create(name="Tariff Managers")
-    set_permissions()
-    return group
-
-
-@pytest.fixture
-def user(tariff_manager_group):
-    user = factories.UserFactory.create(username="test_user")
-    user.set_password("password")
-    user.save()
-    tariff_manager_group.user_set.add(user)
-    return user
-
-
-@pytest.fixture
-def empty_current_workbasket(user):
-    workbasket = factories.WorkBasketFactory.create(author=user)
-    workbasket.set_as_current(user)
-    return workbasket
-
-
-@pytest.fixture()
-def paused_queue():
-    return factories.OperationalStatusFactory(
-        created_by=None,
-        queue_state=QueueState.PAUSED,
-    )
-
-
-@pytest.fixture()
-def unpaused_queue():
-    return factories.OperationalStatusFactory(
-        created_by=None,
-        queue_state=QueueState.UNPAUSED,
-    )
-
-
-@pytest.fixture(scope="session")
-def base_url(live_server: LiveServer):
-    return live_server.url
-
-
-@pytest.fixture
-def page(live_server: LiveServer, page: Page, user):
-    login(page, user, live_server.url)
+def page(page: Page, user, base_url):
+    """Returns a Playwright browser page with a logged-in user."""
+    login(page, user, base_url)
     return page
+
+
+def clear_and_delete_workbasket(workbasket):
+    """Deletes `workbasket` and all associated objects."""
+    PackagedWorkBasket.objects.filter(workbasket=workbasket).delete()
+    tracked_models = workbasket.tracked_models.all().record_ordering().reverse()
+    for obj in tracked_models:
+        obj.delete()
+    workbasket.purge_empty_transactions()
+    workbasket.tasks.all().delete()
+    workbasket.delete_checks()
+    workbasket.delete()
+
+
+def create_user_assignments(workbasket):
+    """Assigns a worker and reviewer to `workbasket`."""
+    factories.UserAssignmentFactory.create(
+        user=workbasket.author,
+        assigned_by=workbasket.author,
+        assignment_type=UserAssignment.AssignmentType.WORKBASKET_WORKER,
+        task__workbasket=workbasket,
+    )
+    factories.UserAssignmentFactory.create(
+        user=workbasket.author,
+        assigned_by=workbasket.author,
+        assignment_type=UserAssignment.AssignmentType.WORKBASKET_REVIEWER,
+        task__workbasket=workbasket,
+    )
+
+
+def create_tracked_model_checks(tracked_models):
+    """Creates a `TrackedModelCheck` for each object in `tracked_models`."""
+    for obj in tracked_models:
+        TrackedModelCheckFactory.create(
+            model=obj,
+            transaction_check__transaction=obj.transaction,
+            transaction_check__latest_tracked_model=obj,
+            transaction_check__head_transaction=obj.transaction,
+            successful=True,
+        )
+
+
+@pytest.fixture
+def user(django_user_model):
+    """Returns an instance of the user specified in `settings.E2E_TEST_USER`."""
+    return django_user_model.objects.get(username=settings.E2E_TEST_USER)
+
+
+@pytest.fixture
+def empty_current_workbasket(transactional_db, user):
+    """Yields an empty, unassigned workbasket that has been set as the user's
+    current workbasket."""
+    workbasket = factories.WorkBasketFactory.create(
+        title=get_unique_id(),
+        reason=f"End-to-end test by {user.get_displayname()}",
+        author=user,
+    )
+    workbasket.set_as_current(user)
+
+    yield workbasket
+
+    clear_and_delete_workbasket(workbasket)
+
+
+@pytest.fixture
+def current_workbasket(empty_current_workbasket):
+    """Returns an unassigned workbasket (containing a tracked model) that has
+    been set as the user's current workbasket."""
+    current_workbasket = empty_current_workbasket
+    factories.FootnoteDescriptionFactory.create(
+        sid=None,
+        described_footnote=Footnote.objects.as_at_today_and_beyond()
+        .order_by("?")
+        .first(),
+        transaction=current_workbasket.new_transaction(),
+    )
+    return current_workbasket
+
+
+@pytest.fixture
+def workbasket_ready_for_queue(current_workbasket):
+    """Returns an assigned workbasket that has passed a rule check and is ready
+    to proceed through packaging queue."""
+    create_tracked_model_checks(current_workbasket.tracked_models.all())
+    create_user_assignments(current_workbasket)
+    return current_workbasket
+
+
+@pytest.fixture
+def paused_queue(transactional_db, user):
+    """Ensures the packaging queue is paused."""
+    return OperationalStatus.pause_queue(user)
+
+
+@pytest.fixture
+def unpaused_queue(transactional_db, user):
+    """Ensures the packaging queue is unpaused."""
+    return OperationalStatus.unpause_queue(user)
+
+
+@pytest.fixture
+def commodity():
+    """Returns the latest version of a currently active `GoodsNomenclature` at
+    random."""
+    with override_current_transaction(Transaction.objects.last()):
+        return (
+            GoodsNomenclature.objects.filter(suffix=80)
+            .current()
+            .as_at_today_and_beyond()
+            .order_by("?")
+            .first()
+        )
+
+
+@pytest.fixture
+def footnote_type():
+    """Returns the latest version of a currently active `FootnoteType` at
+    random."""
+    with override_current_transaction(Transaction.objects.last()):
+        return (
+            FootnoteType.objects.current()
+            .as_at_today_and_beyond()
+            .order_by("?")
+            .first()
+        )
+
+
+@pytest.fixture
+def measure_type():
+    """Returns the latest version of a currently active `MeasureType` at
+    random."""
+    with override_current_transaction(Transaction.objects.last()):
+        return (
+            MeasureType.objects.filter(
+                measure_component_applicability_code=ApplicabilityCode.PERMITTED,
+                measure_explosion_level=MeasureExplosionLevel.TARIC,
+            )
+            .current()
+            .as_at_today_and_beyond()
+            .order_by("?")
+            .first()
+        )
+
+
+@pytest.fixture
+def regulation():
+    """Returns the latest version of a currently active `Regulation` at
+    random."""
+    with override_current_transaction(Transaction.objects.last()):
+        return (
+            Regulation.objects.current().as_at_today_and_beyond().order_by("?").first()
+        )
--- tests/e2e/utils.py
+++ tests/e2e/utils.py
@@ -1,4 +1,5 @@
 from importlib import import_module
+from time import time
 
 from django.conf import settings
 from django.contrib.auth import BACKEND_SESSION_KEY
@@ -8,7 +9,18 @@ from django.contrib.auth import SESSION_KEY
 SessionStore = import_module(settings.SESSION_ENGINE).SessionStore
 
 
+def get_unique_id() -> str:
+    """
+    Returns the current time in seconds since the Epoch as a string.
+
+    Useful for form fields in tests that require unique ID values.
+    """
+    return str(time()).replace(".", "")
+
+
 def login(page, user, base_url):
+    """Adds a Session cookie to a Playwright page's context for user
+    authentication."""
     session = SessionStore()
     session[SESSION_KEY] = user._meta.pk.value_to_string(user)
     session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
@@ -21,7 +33,6 @@ def login(page, user, base_url):
         "url": f"{base_url}/",
     }
 
-    page.goto(f"{base_url}")
     context = page.context
     context.add_cookies(
         [
