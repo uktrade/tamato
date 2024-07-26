d427b59733a8ff6a1c7d7766966a925d986f966a
Emile Swarts <emile.swarts@digital.trade.gov.uk>
2024-07-26 15:28:19 +0100

    Remove unused OTEL configuration

--- scripts/web-worker-entrypoint.sh
+++ scripts/web-worker-entrypoint.sh
@@ -2,10 +2,6 @@
 
 WORKER_COMMAND="gunicorn wsgi --bind 0.0.0.0:${PORT} --timeout 1000 --worker-class=gevent --worker-connections=1000 --workers 9"
 
-if [ -n "${COPILOT_ENVIRONMENT_NAME}" ]; then
-    WORKER_COMMAND="opentelemetry-instrument ${WORKER_COMMAND}"
-fi
-
 echo "Starting worker using the command: ${WORKER_COMMAND}"
 
 eval ${WORKER_COMMAND}
