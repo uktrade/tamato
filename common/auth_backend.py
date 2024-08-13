d8ff9d843a5de3132f51fa5f173af6946f6b8fa4
Matthew McKenzie <matthew.mckenzie3@gmail.com>
2024-08-13 12:22:39 +0100

    Update sso uuid for existing users

--- common/auth_backend.py
+++ common/auth_backend.py
@@ -1,6 +1,7 @@
 from authbroker_client.backends import AuthbrokerBackend
+from django.contrib.auth import get_user_model
 
-# from common.models import User
+UserModel = get_user_model()
 
 
 class CustomAuthbrokerBackend(AuthbrokerBackend):
@@ -11,3 +12,13 @@ class CustomAuthbrokerBackend(AuthbrokerBackend):
             "last_name": profile["last_name"],
             "sso_uuid": profile["user_id"],
         }
+
+    def confirm_sso_uuid(self, user, profile):
+        if not user.sso_uuid:
+            user.sso_uuid = profile["user_id"]
+            user.save()
+
+    def get_or_create_user(self, profile):
+        user = super().get_or_create_user(profile)
+        self.confirm_sso_uuid(user, profile)
+        return user
