bcfb454719235c2ac74dbc3755103093220b9820
Lauren Qurashi <lauren.qurashi@digital.trade.gov.uk>
2024-09-03 15:40:09 +0100

    Add MeasureSerializableWizardMixin to Create Measures

--- measures/views/wizard.py
+++ measures/views/wizard.py
@@ -28,6 +28,7 @@ from measures.constants import START
 from measures.constants import MeasureEditSteps
 from measures.creators import MeasuresCreator
 from measures.util import diff_components
+from measures.views.mixins import MeasureSerializableWizardMixin
 from workbaskets.models import WorkBasket
 from workbaskets.views.decorators import require_current_workbasket
 
@@ -313,6 +314,8 @@ class MeasureEditWizard(
 class MeasureCreateWizard(
     PermissionRequiredMixin,
     NamedUrlSessionWizardView,
+    MeasureSerializableWizardMixin
+
 ):
     """
     Multipart form wizard for creating a single measure.
@@ -435,24 +438,6 @@ class MeasureCreateWizard(
     boolean or boolean values that indicate whether a wizard step should be
     shown."""
 
-    def get_data_form_list(self) -> dict:
-        """
-        Returns a form list based on form_list, conditionally including only
-        those items as per condition_list and also appearing in data_form_list.
-
-        The list is generated dynamically because conditions in condition_list
-        may be dynamic.
-
-        Essentially, version of `WizardView.get_form_list()` filtering in only
-        those list items appearing in `data_form_list`.
-        """
-        data_form_keys = [key for key, form in self.data_form_list]
-        return {
-            form_key: form_class
-            for form_key, form_class in self.get_form_list().items()
-            if form_key in data_form_keys
-        }
-
     @property
     def workbasket(self) -> WorkBasket:
         return WorkBasket.current(self.request)
@@ -515,57 +500,6 @@ class MeasureCreateWizard(
             "measure-ui-create-confirm",
             expected_measures_count=measures_bulk_creator.expected_measures_count,
         )
-
-    def all_serializable_form_data(self) -> Dict:
-        """
-        Returns serializable data for all wizard steps.
-
-        This is a re-implementation of
-        MeasureCreateWizard.get_all_cleaned_data(), but using self.data after
-        is_valid() has been successfully run.
-        """
-
-        all_data = {}
-
-        for form_key in self.get_data_form_list().keys():
-            all_data[form_key] = self.serializable_form_data_for_step(form_key)
-
-        return all_data
-
-    def serializable_form_data_for_step(self, step) -> Dict:
-        """
-        Returns serializable data for a wizard step.
-
-        This is a re-implementation of WizardView.get_cleaned_data_for_step(),
-        returning the serializable version of data in place of the form's
-        regular cleaned_data.
-        """
-
-        form_obj = self.get_form(
-            step=step,
-            data=self.storage.get_step_data(step),
-            files=self.storage.get_step_files(step),
-        )
-
-        return form_obj.serializable_data(remove_key_prefix=step)
-
-    def all_serializable_form_kwargs(self) -> Dict:
-        """Returns serializable kwargs for all wizard steps."""
-
-        all_kwargs = {}
-
-        for form_key in self.get_data_form_list().keys():
-            all_kwargs[form_key] = self.serializable_form_kwargs_for_step(form_key)
-
-        return all_kwargs
-
-    def serializable_form_kwargs_for_step(self, step) -> Dict:
-        """Returns serializable kwargs for a wizard step."""
-
-        form_kwargs = self.get_form_kwargs(step)
-        form_class = self.form_list[step]
-
-        return form_class.serializable_init_kwargs(form_kwargs)
 
     def get_all_cleaned_data(self):
         """
