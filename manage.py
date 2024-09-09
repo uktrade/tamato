4f2b3609a23bca2cda2c91abb354d4d162beba0b
Paul Pepper <paul.pepper@digital.trade.gov.uk>
2024-09-09 17:04:13 +0100

    Add --env-info flag for platform diagnostics.

--- manage.py
+++ manage.py
@@ -6,14 +6,48 @@ import warnings
 
 import dotenv
 
+ENV_INFO_FLAG = "--env-info"
 
-def main():
+
+def output_env_info():
+    """Inspect and output environment diagnostics for help with platform /
+    environment debugging."""
+
+    from pathlib import Path
+
+    cwd = Path().resolve()
+    script_path = Path(__file__).resolve()
+    executable_path = Path(sys.executable).resolve()
+
+    print("Environment diagnostics")
+    print("----")
+    print(f" Current working directory: {cwd}")
+    print(f" Current script path: {script_path}")
+    print(f" Python executable path: {executable_path}")
+    print(f" PATH: {os.environ.get("PATH")}")
+    print("----")
+
+    # Remove the flag to avoid Django unknown command errors.
+    sys.argv = [arg for arg in sys.argv if arg != ENV_INFO_FLAG]
+
+
+def set_django_settings_module():
+    """Set the DJANGO_SETTINGS_MODULE env var with an appropriate value."""
+
     in_test = not {"pytest", "test"}.isdisjoint(sys.argv[1:])
     in_dev = in_test is False and "DEV" == str(os.environ.get("ENV")).upper()
     os.environ.setdefault(
         "DJANGO_SETTINGS_MODULE",
         "settings.test" if in_test else "settings.dev" if in_dev else "settings",
     )
+
+
+def main():
+    if ENV_INFO_FLAG in sys.argv:
+        output_env_info()
+
+    set_django_settings_module()
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
