b580f50ba9b620faca50df655541430fb6443e06
Dale Cannon <dale.cannon@digital.trade.gov.uk>
2024-09-10 15:09:14 +0100

    Register TaskProgressState in admin site

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from tasks.models import TaskCategory
+from tasks.models import TaskProgressState
 
 
 class TaskCategoryAdmin(admin.ModelAdmin):
@@ -8,4 +9,11 @@ class TaskCategoryAdmin(admin.ModelAdmin):
     search_fields = ["name"]
 
 
+class TaskProgressStateAdmin(admin.ModelAdmin):
+    ordering = ["name"]
+    search_fields = ["name"]
+
+
 admin.site.register(TaskCategory, TaskCategoryAdmin)
+
+admin.site.register(TaskProgressState, TaskProgressStateAdmin)
