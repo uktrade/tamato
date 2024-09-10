b978f393eeca9407637e6c780ee7bbcc554ae078
Dale Cannon <dale.cannon@digital.trade.gov.uk>
2024-09-10 15:20:17 +0100

    Register TaskAssignee in admin site

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from tasks.models import TaskAssignee
 from tasks.models import TaskCategory
 from tasks.models import TaskProgressState
 
@@ -14,6 +15,18 @@ class TaskProgressStateAdmin(admin.ModelAdmin):
     search_fields = ["name"]
 
 
+class TaskAssigneeAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "assignment_type", "task_title", "unassigned_at"]
+    search_fields = ["user", "assignment_type", "task"]
+
+    def task_title(self, obj):
+        if not obj.task:
+            return "Missing task!"
+        return obj.task.title
+
+
 admin.site.register(TaskCategory, TaskCategoryAdmin)
 
 admin.site.register(TaskProgressState, TaskProgressStateAdmin)
+
+admin.site.register(TaskAssignee, TaskAssigneeAdmin)
