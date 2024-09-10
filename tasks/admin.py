affb87eab21afc04164a31683b3da79dc77dd679
Dale Cannon <dale.cannon@digital.trade.gov.uk>
2024-09-10 16:19:02 +0100

    Register Task model in admin site

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,8 +1,54 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
+from tasks.models import Task
 from tasks.models import TaskAssignee
 from tasks.models import TaskCategory
 from tasks.models import TaskProgressState
+
+
+class TaskAdminMixin:
+    def link_to_task(self, task: Task):
+        task_url = reverse(
+            "admin:tasks_task_change",
+            args=(task.pk,),
+        )
+        return mark_safe(
+            f'<a href="{task_url}">{task.pk}</a>',
+        )
+
+    def link_to_workbasket(self, workbasket):
+        workbasket_url = reverse(
+            "admin:workbaskets_workbasket_change",
+            args=(workbasket.pk,),
+        )
+        return mark_safe(
+            f'<a href="{workbasket_url}">{workbasket.pk}</a>',
+        )
+
+
+class TaskAdmin(TaskAdminMixin, admin.ModelAdmin):
+    list_display = [
+        "id",
+        "title",
+        "description",
+        "category",
+        "progress_state",
+        "parent_task_id",
+        "workbasket_id",
+        "creator",
+    ]
+
+    def parent_task_id(self, obj):
+        if not obj.parent_task:
+            return "-"
+        return self.link_to_task(obj.parent_task)
+
+    def workbasket_id(self, obj):
+        if not obj.workbasket:
+            return "-"
+        return self.link_to_workbasket(obj.workbasket)
 
 
 class TaskCategoryAdmin(admin.ModelAdmin):
@@ -15,15 +61,17 @@ class TaskProgressStateAdmin(admin.ModelAdmin):
     search_fields = ["name"]
 
 
-class TaskAssigneeAdmin(admin.ModelAdmin):
-    list_display = ["id", "user", "assignment_type", "task_title", "unassigned_at"]
+class TaskAssigneeAdmin(TaskAdminMixin, admin.ModelAdmin):
+    list_display = ["id", "user", "assignment_type", "task_id", "unassigned_at"]
     search_fields = ["user", "assignment_type", "task"]
 
-    def task_title(self, obj):
+    def task_id(self, obj):
         if not obj.task:
             return "Missing task!"
-        return obj.task.title
+        return self.link_to_task(obj.task)
 
+
+admin.site.register(Task, TaskAdmin)
 
 admin.site.register(TaskCategory, TaskCategoryAdmin)
 
