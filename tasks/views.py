bc787f85156f3c263a68f41c4b4210ea24a5bc68
Lauren Qurashi <lauren.qurashi@digital.trade.gov.uk>
2024-11-18 14:58:29 +0000

    Fix double quotes lint error from create subtask form pr

--- tasks/views.py
+++ tasks/views.py
@@ -139,7 +139,7 @@ class SubTaskCreateView(PermissionRequiredMixin, CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["page_title"] = f"Create a subtask for task {self.kwargs["pk"]}"
+        context["page_title"] = f"Create a subtask for task {self.kwargs['pk']}"
         return context
 
     def form_valid(self, form):
