f2130d00996ff40a7e82ab02d24d94b0683ee9f9
Paul Pepper <paul.pepper@digital.trade.gov.uk>
2024-12-05 18:59:48 +0000

    Filter support for tasks and workflows list view

--- tasks/filters.py
+++ tasks/filters.py
@@ -1,8 +1,11 @@
+from django.db.models import TextChoices
 from django.forms import CheckboxSelectMultiple
 from django.urls import reverse_lazy
+from django_filters import ChoiceFilter
 from django_filters import ModelMultipleChoiceFilter
 
 from common.filters import TamatoFilter
+from common.widgets import RadioSelect
 from tasks.models import ProgressState
 from tasks.models import Task
 
@@ -57,7 +60,13 @@ class TaskWorkflowFilter(TamatoFilter):
         return qs.workflow_summary()
 
 
+class TasksAndWorkflowsChoices(TextChoices):
+    TASKS_ONLY = "TASKS_ONLY", "Tasks only"
+    WORKFLOWS_ONLY = "WORKFLOWS_ONLY", "Workflows only"
+
+
 class TaskAndWorkflowFilter(TamatoFilter):
+
     search_fields = (
         "id",
         "title",
@@ -65,11 +74,13 @@ class TaskAndWorkflowFilter(TamatoFilter):
     )
     clear_url = reverse_lazy("workflow:task-and-workflow-ui-list")
 
-    tasks_and_workflows = ModelMultipleChoiceFilter(
-        label="Tasks and Workflows",
-        help_text="Select all that apply",
-        queryset=ProgressState.objects.all(),
-        widget=CheckboxSelectMultiple,
+    tasks_and_workflows = ChoiceFilter(
+        choices=TasksAndWorkflowsChoices.choices,
+        method="filter_by_tasks_and_workflows",
+        widget=RadioSelect,
+        label="Tasks and workflows",
+        empty_label="Tasks and workflows",
+        help_text="Select the choice that applies",
     )
 
     progress_state = ModelMultipleChoiceFilter(
@@ -81,9 +92,16 @@ class TaskAndWorkflowFilter(TamatoFilter):
 
     class Meta:
         model = Task
-        fields = ["search", "category", "progress_state"]
+        fields = ["search", "tasks_and_workflows", "category", "progress_state"]
+
+    def filter_by_tasks_and_workflows(self, queryset, name, value):
+        if TasksAndWorkflowsChoices.TASKS_ONLY == value:
+            queryset = queryset.non_workflow()
+        elif TasksAndWorkflowsChoices.WORKFLOWS_ONLY == value:
+            queryset = queryset.workflow_summary()
+
+        return queryset
 
     @property
     def qs(self):
-        qs = super().qs
-        return qs.workflow_summary()
+        return super().qs.top_level()
