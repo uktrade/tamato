1655587d182ac12481d436c93c6b7eada0fb3ba3
marya <marya.shariq@digital.trade.gov.uk>
2024-12-17 13:32:17 +0000

    fixed datetime format inconsistencies by passing in settings.DATETIME_FORMAT as context variable

--- tasks/views.py
+++ tasks/views.py
@@ -323,6 +323,10 @@ class TaskWorkflowTemplateListView(
     filterset_class = WorkflowTemplateFilter
     sort_by_fields = ["created_at", "updated_at"]
 
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data["datetime_format"] = settings.DATETIME_FORMAT
+
     def get_queryset(self):
         queryset = TaskWorkflowTemplate.objects.all()
         ordering = self.get_ordering()
