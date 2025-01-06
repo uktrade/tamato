6e3e56241d213dcded31d6a7df9f683aa87e930d
Paul Pepper <paul.pepper@digital.trade.gov.uk>
2025-01-06 12:04:19 +0000

    List by default, add flags for async and sync publishing

--- publishing/management/commands/publish_to_api.py
+++ publishing/management/commands/publish_to_api.py
@@ -8,28 +8,40 @@ from publishing.tasks import publish_to_api
 
 
 class Command(BaseCommand):
-    help = "Upload unpublished envelopes to the Tariff API."
+    help = (
+        "Manage envelope uploads to Tariff API. Without arguments, this "
+        "management command lists unpublished envelopes."
+    )
 
     def add_arguments(self, parser):
         parser.add_argument(
-            "-l",
-            "--list",
-            dest="list",
+            "--publish-async",
             action="store_true",
-            help="List unpublished envelopes.",
+            help=(
+                "Asynchronously run (via a Celery task) the function to "
+                "upload unpublished envelopes to the tariffs-api service."
+            ),
+        )
+        parser.add_argument(
+            "--publish-now",
+            action="store_true",
+            help=(
+                "Immediately run (within the current terminal's process) the "
+                "function to upload unpublished envelopes to the tariffs-api "
+                "service."
+            ),
         )
 
     def get_incomplete_envelopes(self):
-        incomplete = CrownDependenciesEnvelope.objects.unpublished()
-        if not incomplete:
-            return None
-        return incomplete
+        return CrownDependenciesEnvelope.objects.unpublished()
 
     def get_unpublished_envelopes(self):
-        unpublished = PackagedWorkBasket.objects.get_unpublished_to_api()
-        if not unpublished:
-            return None
-        return unpublished
+        return PackagedWorkBasket.objects.get_unpublished_to_api()
+
+    def print_envelope_details(self, position, envelope):
+        self.stdout.write(
+            f"position={position}, pk={envelope.pk}, envelope_id={envelope.envelope_id}",
+        )
 
     def list_unpublished_envelopes(self):
         incomplete = self.get_incomplete_envelopes()
@@ -39,22 +51,33 @@ class Command(BaseCommand):
                 f"{incomplete.count()} envelope(s) not completed publishing:",
             )
             for i, crowndependencies in enumerate(incomplete, start=1):
-                self.stdout.write(
-                    f"{i}: {crowndependencies.packagedworkbaskets.last().envelope}",
+                self.print_envelope_details(
+                    position=i,
+                    envelope=crowndependencies.packagedworkbaskets.last(),
                 )
         if unpublished:
             self.stdout.write(
                 f"{unpublished.count()} envelope(s) ready to be published in the following order:",
             )
             for i, packaged_work_basket in enumerate(unpublished, start=1):
-                self.stdout.write(f"{i}: {packaged_work_basket.envelope}")
+                self.print_envelope_details(
+                    position=i,
+                    envelope=packaged_work_basket.envelope,
+                )
 
-    def handle(self, *args, **options):
-        if options["list"]:
-            self.list_unpublished_envelopes()
-            return
-
+    def publish(self, now: bool):
         if self.get_unpublished_envelopes() or self.get_incomplete_envelopes():
-            publish_to_api.apply()
+            if now:
+                self.stdout.write(f"Calling `publish_to_api()` now.")
+                publish_to_api()
+            else:
+                self.stdout.write(f"Calling `publish_to_api()` asynchronously.")
+                publish_to_api.apply()
         else:
             sys.exit("No unpublished envelopes")
+
+    def handle(self, *args, **options):
+        if options["publish_async"] or options["publish_now"]:
+            self.publish(now=options["publish_now"])
+        else:
+            self.list_unpublished_envelopes()
