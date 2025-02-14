8693b4d1cb0d786025e2ffc451275ae953af1f93
Matthew McKenzie <97194636+mattjamc@users.noreply.github.com>
2025-02-14 14:01:14 +0000

    TP2000-1633 Auto delete measures and associations (#1409)
    * Functionality to delete related measures and associations
    
    * Combine measures from deleted commodities into the measures queryset
    
    * Footnote association template
    
    * Add tests
    
    * Wrap deleted commodities in list()
    
    Forcing immediate evaluation of the queryset largely speeds up the view
    
    * Remove old comment
    
    * Add celery logging

--- workbaskets/tasks.py
+++ workbaskets/tasks.py
@@ -16,6 +16,7 @@ from commodities.models.orm import FootnoteAssociationGoodsNomenclature
 from commodities.models.orm import GoodsNomenclature
 from common.celery import app
 from common.models import Transaction
+from common.models.trackedmodel import TrackedModel
 from common.models.transactions import Transaction
 from common.util import TaricDateRange
 from common.validators import UpdateType
@@ -122,11 +123,13 @@ def end_objects(objects, workbasket):
             date.today(),
             commodity.valid_between.upper,
         ):
+            logger.info(f"Deleting object {type(object)} - {object.pk}")
             new_version = object.new_version(
                 workbasket=workbasket,
                 update_type=UpdateType.DELETE,
             )
         else:
+            logger.info(f"End dating object {type(object)} - {object.pk}")
             new_version = object.new_version(
                 workbasket=workbasket,
                 update_type=UpdateType.UPDATE,
@@ -138,16 +141,45 @@ def end_objects(objects, workbasket):
         promote_item_to_top(new_version.transaction, workbasket_transactions)
 
 
+@atomic
+def delete_objects(objects, workbasket):
+    """Iterate through a queryset of tracked models on deleted commodities and
+    delete them."""
+    for object in objects:
+        workbasket_transactions = Transaction.objects.filter(
+            workbasket=workbasket,
+            workbasket__status=WorkflowStatus.EDITING,
+        ).order_by("order")
+
+        logger.info(f"Deleting object {type(object)} - {object.pk}")
+        new_version = object.new_version(
+            workbasket=workbasket,
+            update_type=UpdateType.DELETE,
+        )
+
+        promote_item_to_top(new_version.transaction, workbasket_transactions)
+
+
 @app.task
-def call_end_measures(measure_pks, footnote_association_pks, workbasket_pk):
+def call_end_measures(
+    measure_pks,
+    footnote_association_pks,
+    objects_to_delete_pks,
+    workbasket_pk,
+):
     """Calls end_objects for measures and footnote associations."""
     workbasket = WorkBasket.objects.all().get(pk=workbasket_pk)
     measures = Measure.objects.all().filter(pk__in=measure_pks)
     footnote_associations = FootnoteAssociationGoodsNomenclature.objects.all().filter(
         pk__in=footnote_association_pks,
     )
+    objects_to_delete = TrackedModel.objects.all().filter(pk__in=objects_to_delete_pks)
+    logger.info(f"Calling end objects for measures")
     end_objects(measures, workbasket)
+    logger.info(f"Calling end objects for footnote associations")
     end_objects(footnote_associations, workbasket)
+    logger.info(f"Calling delete objects for measures and footnote associations")
+    delete_objects(objects_to_delete, workbasket)
 
 
 def check_comm_code_for_missing_measures(
