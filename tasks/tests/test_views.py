36704b312a74fee094dfd5ff545c6b1db7cd0e93
Dale Cannon <dale.cannon@digital.trade.gov.uk>
2025-03-25 12:53:04 +0000

    Amend step template deletion confirmation view

--- tasks/tests/test_views.py
+++ tasks/tests/test_views.py
@@ -532,7 +532,7 @@ def test_delete_task_template_view(
 
     assert confirmation_response.status_code == 200
     assert (
-        f"Task template ID: {task_template_pk}"
+        f"Step template ID: {task_template_pk}"
         in soup.select(".govuk-panel__title")[0].text
     )
 
