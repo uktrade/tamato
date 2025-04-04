27db3eeac647310f294f07f54570193aea30ab2f
Paul Pepper <paul.pepper@digital.trade.gov.uk>
2025-04-04 17:43:52 +0100

    Add note to docstring

--- task_automations.py
+++ task_automations.py
@@ -12,4 +12,7 @@ dotted path notation and accompanying description.
 The class name is used to identify and load the automation class, the
 description is presented to users through the UI when selecting and associating
 them with tasks.
+
+Note that changing the localtion of automation classes, or removing them
+altogther, will break any tasks that have been configured to use the automation.
 """
