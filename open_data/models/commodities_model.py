c7d88f0a6844f7a57afc6e702e8b176349a96fb3
Luisella Strona <36708790+Luisella21@users.noreply.github.com>
2025-04-25 15:11:30 +0100

    Models for open data simplified database (#1423)
    There is a desire to use TAP data in open data analysis. The data currently published contain all the historical information, and analysis are complex.
    
    This PR create a set of 'current' data that can be exported every day and used to create reports within TAP.
    
    It creates a shadow model of each tracked model in TAP. The shadow model contains only the approved and current version.
    Where feasible, models have been merged: for instance, descriptions are in the described model, not in a different one.
    
    The tables supporting the models are created in a different partition of the database, so they can easily be excluded from the daily back up of the database.
    
    There is a mechanism to populate the data : this will eventually happen daily, but it is not part of the current PR.
    Some raw sql is used to copy the data (to improve performance). Even so, the copy process on localhost takes a couple of hours. This may or may not be a problem, it is too early to know.
    
    Requires migrations: Yes
    Requires dependency updates - No

---
+++ open_data/models/commodities_model.py
@@ -0,0 +1,114 @@
+from django.db import models
+
+from commodities.models import FootnoteAssociationGoodsNomenclature
+from commodities.models import GoodsNomenclature
+from commodities.models import GoodsNomenclatureOrigin
+from commodities.models import GoodsNomenclatureSuccessor
+from commodities.models.code import CommodityCode
+from common.fields import TaricDateRangeField
+from open_data.models.utils import ReportModel
+
+
+class ReportGoodsNomenclature(ReportModel):
+    shadowed_model = GoodsNomenclature
+    extra_where = " AND valid_between @> CURRENT_DATE"
+
+    trackedmodel_ptr = models.OneToOneField(
+        shadowed_model,
+        models.DO_NOTHING,
+        primary_key=True,
+        db_column="trackedmodel_ptr_id",
+        related_name="+",
+    )
+    valid_between = TaricDateRangeField(db_index=True)
+    sid = models.IntegerField()
+    item_id = models.CharField(max_length=10)
+    suffix = models.CharField(max_length=2)
+    statistical = models.BooleanField()
+
+    indent = models.IntegerField(null=True)
+    description = models.TextField(blank=True, null=True)
+    parent_trackedmodel_ptr = models.ForeignKey(
+        "self",
+        models.DO_NOTHING,
+        null=True,
+    )
+
+    @property
+    def code(self) -> CommodityCode:
+        """Returns a CommodityCode instance for the good."""
+        return CommodityCode(code=self.item_id)
+
+    class Meta:
+        db_table = ReportModel.create_table_name(GoodsNomenclature)
+
+
+class ReportGoodsNomenclatureSuccessor(ReportModel):
+    shadowed_model = GoodsNomenclatureSuccessor
+    trackedmodel_ptr = models.OneToOneField(
+        shadowed_model,
+        models.DO_NOTHING,
+        primary_key=True,
+        db_column="trackedmodel_ptr_id",
+        related_name="+",
+    )
+
+    absorbed_into_goods_nomenclature = models.ForeignKey(
+        ReportGoodsNomenclature,
+        models.DO_NOTHING,
+    )
+    replaced_goods_nomenclature = models.ForeignKey(
+        ReportGoodsNomenclature,
+        models.DO_NOTHING,
+        related_name="commoditiesgoodsnomenclaturesuccessor_replaced_goods_nomenclature_set",
+    )
+
+    class Meta:
+        db_table = ReportModel.create_table_name(GoodsNomenclatureSuccessor)
+
+
+class ReportGoodsNomenclatureOrigin(ReportModel):
+    shadowed_model = GoodsNomenclatureOrigin
+
+    trackedmodel_ptr = models.OneToOneField(
+        shadowed_model,
+        models.DO_NOTHING,
+        primary_key=True,
+        db_column="trackedmodel_ptr_id",
+        related_name="+",
+    )
+
+    derived_from_goods_nomenclature = models.ForeignKey(
+        ReportGoodsNomenclature,
+        models.DO_NOTHING,
+    )
+    new_goods_nomenclature = models.ForeignKey(
+        ReportGoodsNomenclature,
+        models.DO_NOTHING,
+        related_name="commoditiesgoodsnomenclatureorigin_new_goods_nomenclature_set",
+    )
+
+    class Meta:
+        db_table = ReportModel.create_table_name(GoodsNomenclatureOrigin)
+
+
+class ReportFootnoteAssociationGoodsNomenclature(ReportModel):
+    shadowed_model = FootnoteAssociationGoodsNomenclature
+
+    trackedmodel_ptr = models.OneToOneField(
+        shadowed_model,
+        models.DO_NOTHING,
+        primary_key=True,
+        db_column="trackedmodel_ptr_id",
+        related_name="+",
+    )
+
+    valid_between = TaricDateRangeField(db_index=True)
+    associated_footnote = models.ForeignKey("ReportFootnote", models.DO_NOTHING)
+    goods_nomenclature = models.ForeignKey(
+        ReportGoodsNomenclature,
+        models.DO_NOTHING,
+    )
+
+    class Meta:
+        db_table = ReportModel.create_table_name(FootnoteAssociationGoodsNomenclature)
