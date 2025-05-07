544be155640f5f1af9d3f8abb9cc6487040007d4
Paul Pepper <paul.pepper@digital.trade.gov.uk>
2025-05-07 18:12:41 +0100

    Refactor and collapse unit tests using parametrize

--- importer/tests/task_automations/test_automation_models.py
+++ importer/tests/task_automations/test_automation_models.py
@@ -1,67 +1,49 @@
 import pytest
 from bs4 import BeautifulSoup
 
-from tasks.models import StateChoices
-
 pytestmark = pytest.mark.django_db
 
 
-def test_import_goods_automation_get_state_can_run(
-    import_goods_automation_state_is_CAN_RUN,
-):
-    """Test that an automation in CAN_RUN state renders correctly."""
-    assert import_goods_automation_state_is_CAN_RUN.get_state() == StateChoices.CAN_RUN
-    rendering = import_goods_automation_state_is_CAN_RUN.rendered_state()
-    soup = BeautifulSoup(rendering)
-    import_anchor = soup.select(".automation.state-can-run > a")
-    assert len(import_anchor) == 1
-    assert "Import EU TARIC file" in import_anchor[0]
-
-
-def test_import_goods_automation_get_state_running(
-    import_goods_automation_state_is_RUNNING,
-):
-    """Test that an automation in RUNNING state renders correctly."""
-    assert import_goods_automation_state_is_RUNNING.get_state() == StateChoices.RUNNING
-    rendering = import_goods_automation_state_is_RUNNING.rendered_state()
-    soup = BeautifulSoup(rendering)
-    headings = soup.select(".automation.state-running > h3")
-    assert len(headings) == 1
-    assert "File is being imported" in headings[0]
-
-
 @pytest.mark.parametrize(
-    ("import_goods_automation_done_name", "heading_text"),
+    ("automation_name", "css_path", "css_path_text"),
     (
         (
+            "import_goods_automation_state_is_CAN_RUN",
+            ".automation.state-can-run > a",
+            "Import EU TARIC file",
+        ),
+        (
+            "import_goods_automation_state_is_RUNNING",
+            ".automation.state-running > h3",
+            "File is being imported",
+        ),
+        (
             "import_goods_automation_state_is_DONE",
+            ".automation.state-done > h3",
             "File imported - changes detected",
         ),
         (
             "import_goods_automation_state_is_DONE_empty",
+            ".automation.state-done > h3",
             "File imported - empty",
+        ),
+        (
+            "import_goods_automation_state_is_ERRORED",
+            ".automation.state-errored > h3",
+            "There are problems",
         ),
     ),
 )
-def test_import_goods_automation_get_state_done(
-    import_goods_automation_done_name,
-    heading_text,
+def test_import_goods_automation(
+    automation_name,
+    css_path,
+    css_path_text,
     request,
 ):
-    """Test that an automation in DONE state renders correctly."""
-    import_goods_automation_done = request.getfixturevalue(
-        import_goods_automation_done_name,
-    )
-    assert import_goods_automation_done.get_state() == StateChoices.DONE
-    rendering = import_goods_automation_done.rendered_state()
-    soup = BeautifulSoup(rendering)
-    headings = soup.select(".automation.state-done > h3")
+    """Test that automation instances in various states renders correctly."""
+    automation = request.getfixturevalue(automation_name)
+    rendering = automation.rendered_state()
+    soup = BeautifulSoup(rendering, "html.parser")
+    headings = soup.select(css_path)
     assert len(headings) == 1
-    assert heading_text in headings[0]
-
-
-def test_import_goods_automation_get_state_errored(
-    import_goods_automation_state_is_ERRORED,
-):
-    """Test that an automation in ERRORED state renders correctly."""
-    """TODO."""
+    assert css_path_text in headings[0]
